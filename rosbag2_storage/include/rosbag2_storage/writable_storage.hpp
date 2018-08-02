ef2f8dbf7872263ba61d437029f7916bf4d18222
Alessandro Bottero <external.Alessandro.Bottero@bosch-si.com>
2018-08-02 10:42:10 +0200

    Add readable and writable storage interfaces

---
+++ rosbag2_storage/include/rosbag2_storage/writable_storage.hpp
@@ -0,0 +1,44 @@
+// Copyright 2018, Bosch Software Innovations GmbH.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef ROSBAG2_STORAGE__WRITABLE_STORAGE_HPP_
+#define ROSBAG2_STORAGE__WRITABLE_STORAGE_HPP_
+
+#include <string>
+
+namespace rosbag2_storage
+{
+
+class WritableStorage
+{
+public:
+  virtual ~WritableStorage();
+
+  /** Open the specified resource for writing.
+   * @param uri The identifier of the storage to be opened.
+   */
+  virtual void open_for_writing(const std::string & uri) = 0;
+
+  /**
+   * Write the data to storage.
+   * @param data Binary data to write.
+   * @param size Size of the data in bytes.
+   */
+  virtual void write(char * data, size_t size) = 0;
+};
+
+}  // namespace rosbag2_storage
+
+#endif  // ROSBAG2_STORAGE__WRITABLE_STORAGE_HPP_
