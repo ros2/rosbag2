9b7d701f352de6177a75ed0c8d0025bbd4d4b09d
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-08-20 14:21:26 +0200

    Add pragma for windows

--- rosbag2_storage/include/rosbag2_storage/storage_factory.hpp
+++ rosbag2_storage/include/rosbag2_storage/storage_factory.hpp
@@ -23,6 +23,14 @@
 #include "rosbag2_storage/storage_interfaces/read_write_interface.hpp"
 #include "rosbag2_storage/visibility_control.hpp"
 
+// This is necessary because of using stl types here. It is completely safe, because
+// a) the member is not accessible from the outside
+// b) there are no inline functions.
+#ifdef _WIN32
+# pragma warning(push)
+# pragma warning(disable:4251)
+#endif
+
 namespace rosbag2_storage
 {
 
@@ -47,5 +55,9 @@ private:
   std::unique_ptr<StorageFactoryImpl> impl_;
 };
 }  // namespace rosbag2_storage
+
+#ifdef _WIN32
+# pragma warning(pop)
+#endif
 
 #endif  // ROSBAG2_STORAGE__STORAGE_FACTORY_HPP_
