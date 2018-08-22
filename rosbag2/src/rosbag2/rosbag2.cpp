bc22b6343a6b57d5191b23092a7b8b937c046716
Alessandro Bottero <external.Alessandro.Bottero@bosch-si.com>
2018-08-22 15:53:59 +0200

    GH-54 find type name from topic

--- rosbag2/src/rosbag2/rosbag2.cpp
+++ rosbag2/src/rosbag2/rosbag2.cpp
@@ -18,6 +18,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rcl/graph.h"
 
 #include "rcutils/logging_macros.h"
 
@@ -44,34 +45,8 @@ void Rosbag2::record(
   auto storage = factory.open_read_write(file_name, "sqlite3");
 
   if (storage) {
-    std::string type_name = "String";
-    std::string package_name = "std_msgs";
 
-    auto library_path = get_typesupport_library(package_name);
-    std::shared_ptr<Poco::SharedLibrary> typesupport_library = nullptr;
-    const rosidl_message_type_support_t * ts = nullptr;
-
-    try {
-      typesupport_library = std::make_shared<Poco::SharedLibrary>(library_path);
-
-      auto symbol_name = std::string("rosidl_typesupport_c__get_message_type_support_handle__")
-        + package_name + "__msg__" + type_name;
-
-      if (!typesupport_library->hasSymbol(symbol_name)) {
-        std::cout << "\nerror 1\n";
-        return;
-      }
-
-      const rosidl_message_type_support_t * (*get_ts)(void) = nullptr;
-      get_ts = (decltype(get_ts)) typesupport_library->getSymbol(symbol_name);
-      ts = get_ts();
-      if (!ts) {
-        std::cout << "\nerror 2\n";
-        return;
-      }
-    } catch (Poco::LibraryLoadException &) {
-      std::cout << "\nerror 3\n";
-    }
+    RCUTILS_LOG_INFO_NAMED(ROS_PACKAGE_NAME, "Waiting for messages...");
 
     auto ret = RCL_RET_ERROR;
 
@@ -81,15 +56,73 @@ void Rosbag2::record(
     rcl_node_options_t node_options = rcl_node_get_default_options();
     ret = rcl_node_init(node_ptr, name, "", &node_options);
 
+    auto r_allocator = new rcl_allocator_t;
+    *r_allocator = rcutils_get_default_allocator();
+
+    bool topic_found = false;
+    std::string type;
+    while (!topic_found && rclcpp::ok()) {
+      auto names_and_types = new rmw_names_and_types_t;
+      *names_and_types = rmw_get_zero_initialized_names_and_types();
+
+      (void) rcl_get_topic_names_and_types(node_ptr, r_allocator, false, names_and_types);
+      size_t number_of_topics = names_and_types->names.size;
+      for (size_t i = 0; i < number_of_topics; i++) {
+        std::string complete_topic = "/" + topic_name;
+        if (complete_topic == std::string(names_and_types->names.data[i])) {
+          topic_found = true;
+          // TODO(botteroa-si): can a topic have multiple types? Here we asssume not.
+          type = std::string(names_and_types->types[i].data[0]);
+          break;
+        }
+      }
+      (void) rmw_names_and_types_fini(names_and_types);
+    }
+
+    // TODO(botteroa-si): this works if only one '/' is present.
+    char type_separator = '/';
+    auto sep_position = type.find(type_separator);
+
+    std::string type_name = type.substr(sep_position + 1);
+    std::string package_name = type.substr(0, sep_position);
+
+    auto library_path = get_typesupport_library(package_name);
+    std::shared_ptr<Poco::SharedLibrary> typesupport_library = nullptr;
+    const rosidl_message_type_support_t * ts = nullptr;
+
+    try {
+      typesupport_library = std::make_shared<Poco::SharedLibrary>(library_path);
+
+      auto symbol_name = std::string("rosidl_typesupport_c__get_message_type_support_handle__") +
+        package_name + "__msg__" + type_name;
+
+      if (!typesupport_library->hasSymbol(symbol_name)) {
+        std::cout << "\nerror 1\n";
+        return;
+      }
+
+      const rosidl_message_type_support_t * (* get_ts)(void) = nullptr;
+      get_ts = (decltype(get_ts))typesupport_library->getSymbol(symbol_name);
+      ts = get_ts();
+      if (!ts) {
+        std::cout << "\nerror 2\n";
+        return;
+      }
+    } catch (Poco::LibraryLoadException &) {
+      std::cout << "\nerror 3\n";
+    }
+
+
     auto subscription = rcl_get_zero_initialized_subscription();
     auto subscription_options = rcl_subscription_get_default_options();
-    ret =  rcl_subscription_init(
+    ret = rcl_subscription_init(
       &subscription, node_ptr, ts, topic_name.c_str(), &subscription_options);
 
     ret = RCL_RET_SUBSCRIPTION_TAKE_FAILED;
-    RCUTILS_LOG_INFO_NAMED(ROS_PACKAGE_NAME, "Waiting for messages...");
+
 
     while ((ret == RCL_RET_OK || ret == RCL_RET_SUBSCRIPTION_TAKE_FAILED) && rclcpp::ok()) {
+
       auto serialized_msg = new rcutils_char_array_t;
       *serialized_msg = rcutils_get_zero_initialized_char_array();
       auto allocator = new rcutils_allocator_t;
@@ -107,15 +140,15 @@ void Rosbag2::record(
       if (ret == RCL_RET_OK) {
         auto message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
         message->serialized_data = std::shared_ptr<rcutils_char_array_t>(serialized_msg,
-          [](rcutils_char_array_t * msg) {
-            int error = rcutils_char_array_fini(msg);
-            delete msg;
-            if (error != RCUTILS_RET_OK) {
-              RCUTILS_LOG_ERROR_NAMED(
-                "rosbag2_storage_default_plugins",
-                "Leaking memory. Error: %s", rcutils_get_error_string_safe());
-            }
-          });
+            [](rcutils_char_array_t * msg) {
+              int error = rcutils_char_array_fini(msg);
+              delete msg;
+              if (error != RCUTILS_RET_OK) {
+                RCUTILS_LOG_ERROR_NAMED(
+                  "rosbag2_storage_default_plugins",
+                  "Leaking memory. Error: %s", rcutils_get_error_string_safe());
+              }
+            });
         message->topic_name = topic_name;
         rcutils_time_point_value_t time_stamp;
         int error = rcutils_system_time_now(&time_stamp);
