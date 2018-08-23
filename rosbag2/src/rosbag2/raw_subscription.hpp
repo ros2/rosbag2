d2697dc07d63e225965dda8190246f76223ebe94
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-08-23 14:01:26 +0200

    GH-54: Extract subscription to correct class

--- rosbag2/src/rosbag2/raw_subscription.hpp
+++ rosbag2/src/rosbag2/raw_subscription.hpp
@@ -36,89 +36,31 @@ public:
     std::shared_ptr<rcl_node_t> node_handle,
     const rosidl_message_type_support_t & ts,
     const std::string & topic_name,
-    std::function<void(std::shared_ptr<rcutils_char_array_t>)> callback)
-  : SubscriptionBase(
-      node_handle,
-      ts,
-      topic_name,
-      rcl_subscription_get_default_options(),
-      true),
-    callback_(callback)
-  {}
+    std::function<void(std::shared_ptr<rcutils_char_array_t>)> callback);
 
-  std::shared_ptr<void> create_message() override
-  {
-    return borrow_serialized_message(0);
-  }
+  std::shared_ptr<void> create_message() override;
 
-  std::shared_ptr<rcl_serialized_message_t> create_serialized_message() override
-  {
-    return borrow_serialized_message(0);
-  }
+  std::shared_ptr<rcl_serialized_message_t> create_serialized_message() override;
 
 
   void handle_message(
-    std::shared_ptr<void> & message, const rmw_message_info_t & message_info) override
-  {
-    (void) message_info;
-    auto typed_message = std::static_pointer_cast<rcutils_char_array_t>(message);
-    callback_(typed_message);
-  }
+    std::shared_ptr<void> & message, const rmw_message_info_t & message_info) override;
 
-  void return_message(std::shared_ptr<void> & message) override
-  {
-    auto typed_message = std::static_pointer_cast<rcutils_char_array_t>(message);
-    message.reset();
-  }
+  void return_message(std::shared_ptr<void> & message) override;
 
-  void return_serialized_message(std::shared_ptr<rcl_serialized_message_t> & message) override
-  {
-    message.reset();
-  }
+  void return_serialized_message(std::shared_ptr<rcl_serialized_message_t> & message) override;
 
   void handle_intra_process_message(
     rcl_interfaces::msg::IntraProcessMessage & ipm,
-    const rmw_message_info_t & message_info) override
-  {
-    (void) ipm;
-    (void) message_info;
-    throw std::runtime_error("Intra process is not supported");
-  }
+    const rmw_message_info_t & message_info) override;
 
   const std::shared_ptr<rcl_subscription_t>
-  get_intra_process_subscription_handle() const override
-  {
-    return nullptr;
-  }
+  get_intra_process_subscription_handle() const override;
 
 private:
   RCLCPP_DISABLE_COPY(RawSubscription)
 
-  std::shared_ptr<rcl_serialized_message_t> borrow_serialized_message(size_t capacity)
-  {
-    auto message = new rcl_serialized_message_t;
-    *message = rmw_get_zero_initialized_serialized_message();
-    // TODO(Martin-Idel-SI): make class member
-    auto allocator = new rcutils_allocator_t;
-    *allocator = rcutils_get_default_allocator();
-    auto init_return = rmw_serialized_message_init(message, capacity, allocator);
-    if (init_return != RCL_RET_OK) {
-      rclcpp::exceptions::throw_from_rcl_error(init_return);
-    }
-
-    auto serialized_msg = std::shared_ptr<rcl_serialized_message_t>(message,
-        [](rmw_serialized_message_t * msg) {
-          auto fini_return = rmw_serialized_message_fini(msg);
-          delete msg;
-          if (fini_return != RCL_RET_OK) {
-            RCUTILS_LOG_ERROR_NAMED(
-              "rosbag2",
-              "failed to destroy serialized message: %s", rcl_get_error_string_safe());
-          }
-        });
-
-    return serialized_msg;
-  }
+  std::shared_ptr<rcl_serialized_message_t> borrow_serialized_message(size_t capacity);
 
   std::function<void(std::shared_ptr<rcutils_char_array_t>)> callback_;
 };
