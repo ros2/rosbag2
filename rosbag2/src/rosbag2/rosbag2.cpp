7f68b726f58e61635cf75500aafcddcf4f310020
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-08-27 09:12:48 +0200

    Add logging to false case

--- rosbag2/src/rosbag2/rosbag2.cpp
+++ rosbag2/src/rosbag2/rosbag2.cpp
@@ -102,6 +102,7 @@ void Rosbag2::record(
     std::string type = get_topic_type(topic_name, node);
 
     if (type.empty()) {
+      RCUTILS_LOG_INFO_NAMED(ROS_PACKAGE_NAME, "Topic could not be found. Abort.");
       return;
     }
 
