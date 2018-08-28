718bbdcb3a0bd3ca60c6b5d9faefe91e967ef408
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-08-28 12:22:49 +0200

    GH-62 Improve rosbag integration test

--- rosbag2/test/rosbag2/rosbag2_integration_test.cpp
+++ rosbag2/test/rosbag2/rosbag2_integration_test.cpp
@@ -36,14 +36,10 @@ public:
     rosbag2_ = rosbag2::Rosbag2();
   }
 
-  std::future<void> create_publisher(std::string topic)
+  void create_publisher(std::string topic)
   {
     auto publisher = publisher_node_->create_publisher<std_msgs::msg::String>(topic);
-    return std::async(std::launch::async, [publisher] {
-               auto message = std_msgs::msg::String();
-               message.data = "string message";
-               publisher->publish(message);
-             });
+    publishers_.push_back(publisher);
   }
 
   static void SetUpTestCase()
@@ -59,56 +55,52 @@ public:
   rosbag2::Rosbag2 rosbag2_;
   std::shared_ptr<rclcpp::Node> node_;
   std::shared_ptr<rclcpp::Node> publisher_node_;
+  std::vector<std::shared_ptr<rclcpp::PublisherBase>> publishers_;
 };
 
 TEST_F(RosBag2IntegrationFixture,
   get_topic_returns_with_topic_string_if_topic_is_specified_without_slash)
 {
-  auto future = create_publisher("string_topic");
+  create_publisher("string_topic");
 
   auto topics_and_types = rosbag2_.get_topics_with_types({"string_topic"}, node_);
 
-  EXPECT_THAT(topics_and_types, SizeIs(1));
+  ASSERT_THAT(topics_and_types, SizeIs(1));
   EXPECT_THAT(topics_and_types.begin()->second, StrEq("std_msgs/String"));
-  future.get();
 }
 
 TEST_F(RosBag2IntegrationFixture,
   get_topic_returns_with_topic_string_if_topic_is_specified_with_slash)
 {
-  auto future = create_publisher("string_topic");
+  create_publisher("string_topic");
 
   auto topics_and_types = rosbag2_.get_topics_with_types({"/string_topic"}, node_);
 
-  EXPECT_THAT(topics_and_types, SizeIs(1));
+  ASSERT_THAT(topics_and_types, SizeIs(1));
   EXPECT_THAT(topics_and_types.begin()->second, StrEq("std_msgs/String"));
-  future.get();
 }
 
-TEST_F(RosBag2IntegrationFixture,
-  returns_multiple_topics_for_multiple_inputs)
+TEST_F(RosBag2IntegrationFixture, returns_multiple_topics_for_multiple_inputs)
 {
   std::string first_topic("/string_topic");
   std::string second_topic("/other_topic");
   std::string third_topic("/wrong_topic");
 
-  auto future1 = create_publisher(first_topic);
-  auto future2 = create_publisher(second_topic);
-  auto future3 = create_publisher(third_topic);
+  create_publisher(first_topic);
+  create_publisher(second_topic);
+  create_publisher(third_topic);
 
   auto topics_and_types = rosbag2_.get_topics_with_types({first_topic, second_topic}, node_);
 
-  EXPECT_THAT(topics_and_types, SizeIs(2));
+  ASSERT_THAT(topics_and_types, SizeIs(2));
   EXPECT_THAT(topics_and_types.find(first_topic)->second, StrEq("std_msgs/String"));
   EXPECT_THAT(topics_and_types.find(second_topic)->second, StrEq("std_msgs/String"));
-  future1.get();
 }
 
 TEST_F(RosBag2IntegrationFixture, get_topic_returns_empty_if_topic_does_not_exist) {
-  auto future = create_publisher("string_topic");
+  create_publisher("string_topic");
 
   auto topics_and_types = rosbag2_.get_topics_with_types({"/wrong_topic"}, node_);
 
-  EXPECT_THAT(topics_and_types, IsEmpty());
-  future.get();
+  ASSERT_THAT(topics_and_types, IsEmpty());
 }
