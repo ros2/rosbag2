c246802033a3c163e3964a4e09aac04a9bb0764b
Andreas Holzner <external.Andreas.Holzner@bosch-si.com>
2018-08-28 11:33:12 +0200

    GH-61 enhance read integration test to check multiple topics

--- rosbag2/test/rosbag2/rosbag2_read_integration_test.cpp
+++ rosbag2/test/rosbag2/rosbag2_read_integration_test.cpp
@@ -14,12 +14,12 @@
 
 #include <gmock/gmock.h>
 
-#include <atomic>
+#include <chrono>
 #include <future>
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
-#include <utility>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rosbag2/rosbag2.hpp"
@@ -37,29 +37,51 @@ class RosBag2IntegrationTestFixture : public Rosbag2TestFixture
 public:
   RosBag2IntegrationTestFixture()
   : Rosbag2TestFixture(), messages_stored_counter_(0)
-  {}
-
-  std::vector<std::string> subscribe_messages(size_t expected_messages_number)
   {
-    std::vector<std::string> messages;
-    auto node = std::make_shared<rclcpp::Node>("subscriber_node");
-    auto subscription = node->create_subscription<std_msgs::msg::String>("string_topic",
-        [this, &messages](const std_msgs::msg::String::ConstSharedPtr message) {
-          messages.emplace_back(message->data);
-          messages_stored_counter_++;
-        }, 10);
+    rclcpp::init(0, nullptr);
+  }
 
-    while (messages_stored_counter_ < expected_messages_number) {
+  ~RosBag2IntegrationTestFixture() override
+  {
+    rclcpp::shutdown();
+  }
+
+  template<typename T>
+  auto subscribe_messages(
+    size_t expected_messages_number, std::string topic)
+  {
+    auto node = rclcpp::Node::make_shared("node_" + topic);
+    std::vector<typename T::_data_type> messages;
+    size_t messages_received = 0;
+    auto subscription = node->create_subscription<T>(topic,
+        [&messages, &messages_received](typename T::ConstSharedPtr message) {
+          messages.push_back(message->data);
+          ++messages_received;
+          std::cout << "received message: " << message->data << std::endl;
+        });
+    subscriptions_.push_back(subscription);
+
+    while (messages_received < expected_messages_number) {
       rclcpp::spin_some(node);
     }
     return messages;
   }
 
-  void launch_subscriber(size_t expected_messages_number)
+  template<typename MessageT>
+  auto launch_subscriber(size_t expected_messages_number, std::string topic)
   {
-    subscriber_future_ = std::async(std::launch::async, [this, expected_messages_number] {
-          return subscribe_messages(expected_messages_number);
-        });
+    return std::async(std::launch::async, [this, expected_messages_number, topic] {
+               return subscribe_messages<MessageT>(expected_messages_number, topic);
+             });
+  }
+
+  void wait_for_subscribers(size_t count)
+  {
+    std::async(std::launch::async, [this, count] {
+        while (subscriptions_.size() < count) {
+          std::this_thread::sleep_for(50ms);
+        }
+      }).get();
   }
 
   void play_bag(std::string database_name)
@@ -69,29 +91,38 @@ public:
   }
 
   std::atomic<size_t> messages_stored_counter_;
-  std::future<std::vector<std::string>> subscriber_future_;
+  std::vector<rclcpp::SubscriptionBase::SharedPtr> subscriptions_;
 };
 
-TEST_F(RosBag2IntegrationTestFixture, recorded_messages_are_played)
+TEST_F(RosBag2IntegrationTestFixture, recorded_messages_are_played_for_all_topics)
 {
-  rclcpp::init(0, nullptr);
-
+  auto topic_types = std::map<std::string, std::string>{
+    {"topic1", "std_msgs/String"},
+    {"topic2", "std_msgs/UInt8"},
+  };
   std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> messages =
-  {serialize_string_message("Hello World 1"),
-    serialize_string_message("Hello World 2"),
-    serialize_string_message("Hello World 2")};
+  {serialize_message<std_msgs::msg::String>("topic1", "Hello World 1"),
+    serialize_message<std_msgs::msg::String>("topic1", "Hello World 2"),
+    serialize_message<std_msgs::msg::String>("topic1", "Hello World 2"),
+    serialize_message<std_msgs::msg::UInt8>("topic2", 123),
+    serialize_message<std_msgs::msg::UInt8>("topic2", 127),
+    serialize_message<std_msgs::msg::UInt8>("topic2", 127)};
 
-  write_messages(database_name_, messages);
+  write_messages(database_name_, messages, topic_types);
 
   // Due to a problem related to the subscriber, we play many (3) messages but make the subscriber
   // node spin only until 2 have arrived. Hence the 2 as `launch_subscriber()` argument.
-  launch_subscriber(2);
+  auto string_subscriber_future = launch_subscriber<std_msgs::msg::String>(2, "topic1");
+  auto int_subscriber_future = launch_subscriber<std_msgs::msg::UInt8>(2, "topic2");
+  wait_for_subscribers(2);
   play_bag(database_name_);
 
-  auto replayed_messages = subscriber_future_.get();
-  ASSERT_THAT(replayed_messages, SizeIs(2));
-  ASSERT_THAT(replayed_messages[0], Eq("Hello World 1"));
-  ASSERT_THAT(replayed_messages[1], Eq("Hello World 2"));
-
-  rclcpp::shutdown();
+  auto replayed_string_messages = string_subscriber_future.get();
+  ASSERT_THAT(replayed_string_messages, SizeIs(2));
+  ASSERT_THAT(replayed_string_messages[0], Eq("Hello World 1"));
+  ASSERT_THAT(replayed_string_messages[1], Eq("Hello World 2"));
+  auto replayed_int_messages = int_subscriber_future.get();
+  ASSERT_THAT(replayed_int_messages, SizeIs(2));
+  ASSERT_THAT(replayed_int_messages[0], Eq(123));
+  ASSERT_THAT(replayed_int_messages[1], Eq(127));
 }
