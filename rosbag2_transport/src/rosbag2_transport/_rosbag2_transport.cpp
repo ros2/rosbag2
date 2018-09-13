b419300900dd2dfc77a56927841b0dc247631bb0
Andreas Greimel <external.andreas.greimel@bosch-si.com>
2018-09-13 11:17:17 +0200

    GH-21 Wire rosbag play into CLI

--- rosbag2_transport/src/rosbag2_transport/_rosbag2_transport.cpp
+++ rosbag2_transport/src/rosbag2_transport/_rosbag2_transport.cpp
@@ -49,11 +49,34 @@ rosbag2_transport_record_topics(PyObject * Py_UNUSED(self), PyObject * args)
   Py_RETURN_NONE;
 }
 
+static PyObject *
+rosbag2_transport_play(PyObject * Py_UNUSED(self), PyObject * args)
+{
+  char * file_name;
+  if (!PyArg_ParseTuple(args, "s", &file_name)) {
+    return nullptr;
+  }
+
+  rosbag2::Rosbag2PlayOptions options{};
+  options.queue_buffer_length_ = 1000;
+
+  rosbag2_transport::Rosbag2Transport transport;
+  transport.init();
+  transport.play(file_name, options);
+  transport.shutdown();
+
+  Py_RETURN_NONE;
+}
+
 /// Define the public methods of this module
 static PyMethodDef rosbag2_transport_methods[] = {
   {
     "record_topics", rosbag2_transport_record_topics, METH_VARARGS,
     "Record topics"
+  },
+  {
+    "play_bag", rosbag2_transport_play, METH_VARARGS,
+    "Play bag"
   },
   {nullptr, nullptr, 0, nullptr}  /* sentinel */
 };
