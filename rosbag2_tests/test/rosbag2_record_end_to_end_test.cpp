5514c48c1d1dde00d045f55584c4d6260ae0e3b4
Alessandro Bottero <external.Alessandro.Bottero@bosch-si.com>
2018-09-17 18:12:58 +0200

    GH-87 Separate record from play end-to-end test

--- rosbag2_tests/test/rosbag2_record_end_to_end_test.cpp
+++ rosbag2_tests/test/rosbag2_record_end_to_end_test.cpp
@@ -14,6 +14,7 @@
 
 #include <gmock/gmock.h>
 
+#include <atomic>
 #include <iostream>
 #include <memory>
 #include <future>
@@ -30,20 +31,21 @@
 #include "test_msgs/msg/primitives.hpp"
 #include "test_msgs/msg/static_array_primitives.hpp"
 #include "test_msgs/message_fixtures.hpp"
+#include "rosbag2_storage_default_plugins/sqlite/sqlite_storage.hpp"
 
 using namespace ::testing;  // NOLINT
+using namespace std::chrono_literals;  // NOLINT
 
 class EndToEndTestFixture : public Test
 {
 public:
-  EndToEndTestFixture()
-  : database_name_(std::string(UnitTest::GetInstance()->current_test_info()->name()) + ".db3")
+  EndToEndTestFixture() : publisher_is_done_(false)
   {
     std::string system_separator = "/";
 #ifdef _WIN32
     system_separator = "\\";
 #endif
-    database_name_ = temporary_dir_path_ + system_separator + database_name_;
+    database_name_ = temporary_dir_path_ + system_separator + "test.bag";
     std::cout << "Database name: " << database_name_ << std::endl;
     rclcpp::init(0, nullptr);
   }
@@ -108,7 +110,9 @@ public:
     AssignProcessToJobObject(h_job, process_info.hProcess);
     CloseHandle(process_info.hProcess);
     CloseHandle(process_info.hThread);
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    while (!publisher_is_done_) {
+      std::this_thread::sleep_for(50ms);
+    }
     CloseHandle(h_job);
 #else
     auto process_id = fork();
@@ -118,9 +122,11 @@ public:
       system("ros2 bag record -a");
     } else {
       // Here we wait to allow rosbag2 to record some messages before killing the process.
-      std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+      while (!publisher_is_done_) {
+        std::this_thread::sleep_for(50ms);
+      }
       kill(-process_id, SIGINT);
-      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+      std::this_thread::sleep_for(200ms);
       kill(-process_id, SIGTERM);
     }
 #endif
@@ -142,17 +148,24 @@ public:
 
   std::future<void> publish_test_message()
   {
-    return async(std::launch::async, []() {
-               auto publisher_node = std::make_shared<rclcpp::Node>("publisher_node");
-               auto publisher =
-               publisher_node->create_publisher<test_msgs::msg::Primitives>("test_topic");
-               auto message = get_messages_primitives()[0];
-               message->string_value = "test";
-               for (int i = 0; i < 10; i++) {
-                 publisher->publish(message);
-                 std::this_thread::sleep_for(std::chrono::milliseconds(200));
-               }
-             });
+    return async(
+      std::launch::async, [this]() {
+        std::string topic_name = "/test_topic";
+        auto publisher_node = std::make_shared<rclcpp::Node>("publisher_node");
+        auto publisher =
+        publisher_node->create_publisher<test_msgs::msg::Primitives>(topic_name);
+        auto message = get_messages_primitives()[0];
+        message->string_value = "test";
+
+        rosbag2_storage_plugins::SqliteWrapper
+          db(database_name_, rosbag2_storage::storage_interfaces::IOFlag::READ_ONLY);
+        while (rclcpp::ok() && count_stored_messages(db, topic_name) < 3) {
+          publisher->publish(message);
+          // rate limiting
+          std::this_thread::sleep_for(50ms);
+        }
+        publisher_is_done_ = true;
+      });
   }
 
   std::future<std::vector<std::string>> subscribe_and_wait_for_one_test_message()
@@ -175,21 +188,66 @@ public:
       });
   }
 
+  size_t
+  count_stored_messages(rosbag2_storage_plugins::SqliteWrapper & db, const std::string & topic_name)
+  {
+    // protect against concurrent writes (from recording) that may make the count query throw.
+    while (true) {
+      try {
+        return count_stored_messages_in_db(db, topic_name);
+      } catch (const rosbag2_storage_plugins::SqliteException & e) {
+        (void) e;
+      }
+      std::this_thread::sleep_for(50ms);
+    }
+  }
+
+  size_t count_stored_messages_in_db(
+    rosbag2_storage_plugins::SqliteWrapper & db, const std::string & topic_name)
+  {
+    auto row = db.prepare_statement(
+        "SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name = 'topics';")
+      ->execute_query<int>().get_single_line();
+    if (std::get<0>(row) == 0) {
+      return 0;
+    }
+    auto message_count = db.prepare_statement(
+      "SELECT COUNT(*) "
+      "FROM messages LEFT JOIN topics ON messages.topic_id = topics.id "
+      "WHERE topics.name = ?;")->bind(topic_name)->execute_query<int>().get_single_line();
+    return static_cast<size_t>(std::get<0>(message_count));
+  }
+
+  std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> get_messages()
+  {
+    std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> table_msgs;
+    auto storage = std::make_shared<rosbag2_storage_plugins::SqliteStorage>();
+    storage->open(database_name_, rosbag2_storage::storage_interfaces::IOFlag::READ_ONLY);
+
+    while (storage->has_next()) {
+      table_msgs.push_back(storage->read_next());
+    }
+
+    return table_msgs;
+  }
+
+
   std::string database_name_;
   static std::string temporary_dir_path_;
+  std::atomic<bool> publisher_is_done_;
 };
 
 std::string EndToEndTestFixture::temporary_dir_path_ = "";  // NOLINT
 
-TEST_F(EndToEndTestFixture, messages_are_recorder_and_replayed_correctly) {
+TEST_F(EndToEndTestFixture, record_end_to_end_test) {
+  auto record_future = std::async(std::launch::async, [this]() {record_all_topics();});
+  std::this_thread::sleep_for(5000ms);
   auto publisher_future = publish_test_message();
-  record_all_topics();
-  publisher_future.wait();
 
-  auto subscriber_future = subscribe_and_wait_for_one_test_message();
-  play_bag();
-  auto messages = subscriber_future.get();
+  record_future.get();
+  publisher_future.get();
 
-  ASSERT_THAT(messages, Not(IsEmpty()));
-  EXPECT_THAT(messages[0], Eq("test"));
+  auto recorded_messages = get_messages();
+  ASSERT_THAT(recorded_messages, SizeIs(Ge(3u)));
+  EXPECT_THAT(recorded_messages[0]->topic_name, Eq("/test_topic"));
 }
