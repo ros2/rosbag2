5bb79adfdc2eea90a5b07a2e7424c4d9903e56e6
Karsten Knese <karsten.knese@googlemail.com>
2018-09-19 17:56:26 -0700

    add license agreement

--- rosbag2_transport/include/rosbag2_transport/visibility_control.hpp
+++ rosbag2_transport/include/rosbag2_transport/visibility_control.hpp
@@ -1,3 +1,24 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* This header must be included by all rclcpp headers which declare symbols
+ * which are defined in the rclcpp library. When not building the rclcpp
+ * library, i.e. when using the headers in other package's code, the contents
+ * of this header change the visibility of certain symbols which the rclcpp
+ * library cannot have, but the consuming code must have inorder to link.
+ */
+
 #ifndef ROSBAG2_TRANSPORT__VISIBILITY_CONTROL_HPP_
 #define ROSBAG2_TRANSPORT__VISIBILITY_CONTROL_HPP_
 
