0bc4f6334340d2665cec4163bd1e6ae799826e1e
Alessandro Bottero <external.Alessandro.Bottero@bosch-si.com>
2018-10-09 14:52:12 +0200

    GH-101 Add converter interface

---
+++ rosbag2/include/rosbag2/format_converter_interface.hpp
@@ -0,0 +1,56 @@
+// Copyright 2018, Bosch Software Innovations GmbH.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ROSBAG2__FORMAT_CONVERTER_INTERFACE_HPP_
+#define ROSBAG2__FORMAT_CONVERTER_INTERFACE_HPP_
+
+#include <memory>
+#include <string>
+
+#include "rcutils/types.h"
+#include "rosbag2_storage/serialized_bag_message.hpp"
+#include "rosidl_typesupport_cpp/message_type_support.hpp"
+
+using SerializedBagMessage = rosbag2_storage::SerializedBagMessage;
+
+namespace rosbag2
+{
+
+struct Ros2Message
+{
+  void * message;
+  rcutils_char_array_t topic_name;
+  rcutils_time_point_value_t timestamp;
+  rcutils_allocator_t allocator;
+};
+
+class FormatConverterInterface
+{
+public:
+  virtual ~FormatConverterInterface() = default;
+
+  virtual void deserialize(
+    std::shared_ptr<Ros2Message> ros_message,
+    std::shared_ptr<const SerializedBagMessage> serialized_message,
+    const rosidl_message_type_support_t * type_support) = 0;
+
+  virtual void serialize(
+    std::shared_ptr<SerializedBagMessage> serialized_message,
+    std::shared_ptr<const Ros2Message> ros_message,
+    const rosidl_message_type_support_t * type_support) = 0;
+};
+
+}  // namespace rosbag2
+
+#endif  // ROSBAG2__FORMAT_CONVERTER_INTERFACE_HPP_
