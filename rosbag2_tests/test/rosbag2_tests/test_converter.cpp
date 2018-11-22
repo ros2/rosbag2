11ba7010b19a6582ca402925794e6a4b64edd2eb
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-11-22 15:32:38 +0100

    GH-137 It suffices to have only one converter test

--- rosbag2_tests/test/rosbag2_tests/test_converter.cpp
+++ rosbag2_tests/test/rosbag2_tests/test_converter.cpp
@@ -39,9 +39,8 @@ public:
     allocator_ = rcutils_get_default_allocator();
     cdr_converter_ = factory_->load_converter("cdr");
   }
-  std::shared_ptr<rosbag2_ros2_message_t>
 
-  allocate_empty_dynamic_array_message()
+  std::shared_ptr<rosbag2_ros2_message_t> allocate_empty_dynamic_array_message()
   {
     auto introspection_type_support = rosbag2::get_typesupport(
       "test_msgs/DynamicArrayNested", "rosidl_typesupport_introspection_cpp");
@@ -59,48 +58,29 @@ public:
 };
 
 TEST_F(ConverterTestFixture, serialize_converts_ros_message_into_cdr_for_dynamic_array_nested) {
-  auto ros_message = allocate_empty_dynamic_array_message();
+  auto initial_ros_message = allocate_empty_dynamic_array_message();
 
   auto correctly_typed_ros_message = reinterpret_cast<test_msgs::msg::DynamicArrayNested *>(
-    ros_message->message);
+    initial_ros_message->message);
   auto primitive_msgs = get_messages_primitives();
   for (const auto & primitive_msg : primitive_msgs) {
     correctly_typed_ros_message->primitive_values.push_back(*primitive_msg);
   }
-  // The message should be equivalent to the following message:
-  auto message = get_messages_dynamic_array_nested()[0];
-  EXPECT_THAT(*correctly_typed_ros_message, Eq(*message));
 
   auto serialized_message = std::make_shared<rosbag2::SerializedBagMessage>();
   serialized_message->serialized_data = memory_management_->make_initialized_message();
   auto type_support = rosbag2::get_typesupport(
     "test_msgs/DynamicArrayNested", "rosidl_typesupport_cpp");
 
-  cdr_converter_->serialize(ros_message, type_support, serialized_message);
+  cdr_converter_->serialize(initial_ros_message, type_support, serialized_message);
 
-  auto deserialized_msg = memory_management_->
-    deserialize_message<test_msgs::msg::DynamicArrayNested>(serialized_message->serialized_data);
-  EXPECT_THAT(*deserialized_msg, Eq(*message));
-  EXPECT_THAT(serialized_message->topic_name, StrEq(topic_name_));
-  EXPECT_THAT(serialized_message->time_stamp, Eq(ros_message->timestamp));
-}
+  auto final_roundtrip_ros_message = allocate_empty_dynamic_array_message();
 
-TEST_F(ConverterTestFixture, deserialize_converts_ros_message_into_cdr_for_dynamic_array_nested) {
-  auto ros_message = allocate_empty_dynamic_array_message();
+  cdr_converter_->deserialize(serialized_message, type_support, final_roundtrip_ros_message);
 
-  auto message = get_messages_dynamic_array_nested()[0];
-  auto bag_message = std::make_shared<rosbag2::SerializedBagMessage>();
-  bag_message->topic_name = topic_name_;
-  bag_message->time_stamp = 1;
-  bag_message->serialized_data = memory_management_->serialize_message(message);
-
-  auto type_support = rosbag2::get_typesupport(
-    "test_msgs/DynamicArrayNested", "rosidl_typesupport_cpp");
-
-  cdr_converter_->deserialize(bag_message, type_support, ros_message);
-
-  auto deserialized_message =
-    static_cast<test_msgs::msg::DynamicArrayNested *>(ros_message->message);
-  EXPECT_THAT(*deserialized_message, Eq(*message));
-  EXPECT_THAT(ros_message->topic_name, StrEq(topic_name_));
+  EXPECT_THAT(
+    *static_cast<test_msgs::msg::DynamicArrayNested *>(final_roundtrip_ros_message->message),
+    Eq(*static_cast<test_msgs::msg::DynamicArrayNested *>(initial_ros_message->message)));
+  EXPECT_THAT(final_roundtrip_ros_message->topic_name, StrEq(initial_ros_message->topic_name));
+  EXPECT_THAT(final_roundtrip_ros_message->timestamp, Eq(initial_ros_message->timestamp));
 }
