1f59781519e9b3614b3c729af4ef28955967c59d
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-12-14 09:10:06 +0100

    GH-157 Expose read ahead queue size property

--- ros2bag/ros2bag/verb/play.py
+++ ros2bag/ros2bag/verb/play.py
@@ -26,10 +26,28 @@ class PlayVerb(VerbExtension):
     def add_arguments(self, parser, cli_name):  # noqa: D102
         parser.add_argument(
             'bag_file', help='bag file to replay')
+        parser.add_argument(
+            '-s', '--storage', default='sqlite3',
+            help='storage identifier to be used, defaults to "sqlite3"')
+        parser.add_argument(
+            '-r', '--read-ahead-queue-size', default = 1000,
+            help='size of message queue rosbag tries to hold in memory to help deterministic '
+                 'playback. Larger size will result in larger memory needs but might prevent '
+                 'delay of message playback.')
 
     def main(self, *, args):  # noqa: D102
         bag_file = args.bag_file
         if not os.path.exists(bag_file):
             return "Error: bag file '{}' does not exist!".format(bag_file)
 
-        rosbag2_transport_py.play(uri=bag_file, storage_id='sqlite3')
+        read_ahead_queue_size = 1000
+        try:
+            read_ahead_queue_size = int(args.read_ahead_queue_size)
+        except:
+            print("[ERROR] [ros2bag] read ahead queue size must be an integer")
+            exit(0)
+
+        rosbag2_transport_py.play(
+            uri=bag_file,
+            storage_id=args.storage,
+            read_ahead_queue_size=read_ahead_queue_size)
