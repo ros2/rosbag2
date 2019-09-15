71c8cbc1ede6dcafd67f89500025d5cab54fa58c
ruffsl <roxfoxpox@gmail.com>
2019-09-14 21:08:13 -0700

    install package manifest
    Signed-off-by: ruffsl <roxfoxpox@gmail.com>

--- ros2bag/setup.py
+++ ros2bag/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+package_name = 'ros2bag'
+
 setup(
-    name='ros2bag',
+    name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['ros2cli'],
     zip_safe=True,
     author='Karsten Knese',
