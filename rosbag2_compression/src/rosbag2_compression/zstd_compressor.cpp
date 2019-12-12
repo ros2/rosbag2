3b54aec330de84b6dfe812a4491e9e1fc1cef0d0
Zachary Michaels <zmichaels11@gmail.com>
2019-12-12 15:03:11 -0800

    Alphasort headers
    Signed-off-by: Zachary Michaels <zmichaels11@gmail.com>

--- rosbag2_compression/src/rosbag2_compression/zstd_compressor.cpp
+++ rosbag2_compression/src/rosbag2_compression/zstd_compressor.cpp
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cstdio>
-
 #include <chrono>
+#include <cstdio>
 #include <memory>
 #include <string>
 #include <vector>
