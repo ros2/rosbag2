3bf6223531d5f64a64718b37f25fa79ebdc7163c
Zachary Michaels <zmichaels11@gmail.com>
2020-01-30 19:36:03 -0800

    Assert that bagfile split at least once on Windows
    Signed-off-by: Zachary Michaels <zmichaels11@gmail.com>

--- rosbag2_tests/test/rosbag2_tests/test_rosbag2_record_end_to_end.cpp
+++ rosbag2_tests/test/rosbag2_tests/test_rosbag2_record_end_to_end.cpp
@@ -333,8 +333,15 @@ TEST_F(RecordFixture, record_end_to_end_with_splitting_splits_bagfile) {
 
       const auto bag_path = rcpputils::fs::path(root_bag_path_) / bag_name.str();
 
-      metadata.relative_file_paths.push_back(bag_path.string());
+      // There is not any guarantee that the bagfile split expected_split times
+      // due to possible io sync delays. Instead, assert that the bagfile split
+      // at least once
+      if (rcpputils::fs::exists(bag_path)) {
+        metadata.relative_file_paths.push_back(bag_path.string());
+      }
     }
+
+    ASSERT_GT(metadata.relative_file_paths.size(), 1) << "Bagfile never split!";
     metadata_io.write_metadata(root_bag_path_, metadata);
   }
 #endif
