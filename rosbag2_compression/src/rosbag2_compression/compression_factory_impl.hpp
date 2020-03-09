cf68df2caf27620e6c45f759011d8b163ec3eec8
Anas Abou Allaban <allabana@amazon.com>
2020-03-09 14:25:54 -0700

    Add compression factory implementation (#313)
    * Add compression factory implementation
    
    Signed-off-by: Anas Abou Allaban <allabana@amazon.com>

--- rosbag2_compression/src/rosbag2_compression/compression_factory_impl.hpp
+++ rosbag2_compression/src/rosbag2_compression/compression_factory_impl.hpp
@@ -15,11 +15,34 @@
 #ifndef ROSBAG2_COMPRESSION__COMPRESSION_FACTORY_IMPL_HPP_
 #define ROSBAG2_COMPRESSION__COMPRESSION_FACTORY_IMPL_HPP_
 
+#include <algorithm>
 #include <memory>
 #include <stdexcept>
 #include <string>
 
+#include "logging.hpp"
 #include "rosbag2_compression/compression_factory.hpp"
+#include "rosbag2_compression/zstd_compressor.hpp"
+#include "rosbag2_compression/zstd_decompressor.hpp"
+
+namespace
+{
+
+constexpr const char kCompressionFormatZstd[] = "zstd";
+
+/// Verify whether two case-insensitive chars are equal
+bool compare_char(const char c1, const char c2)
+{
+  return c1 == c2 || std::tolower(c1) == std::tolower(c2);
+}
+
+/// Performs a case-insensitive comparison of two strings
+bool case_insensitive_compare(const std::string & str1, const std::string & str2) noexcept
+{
+  return (str1.size() == str2.size()) &&
+         std::equal(str1.begin(), str1.end(), str2.begin(), &compare_char);
+}
+}  // namespace
 
 namespace rosbag2_compression
 {
@@ -33,16 +56,30 @@ public:
 
   /// See CompressionFactory::create_compressor for documentation.
   std::unique_ptr<rosbag2_compression::BaseCompressorInterface>
-  create_compressor(const std::string &)
+  create_compressor(const std::string & compression_format)
   {
-    throw std::logic_error{"Not implemented"};
+    if (case_insensitive_compare(compression_format, kCompressionFormatZstd)) {
+      return std::make_unique<rosbag2_compression::ZstdCompressor>();
+    } else {
+      std::stringstream errmsg;
+      errmsg << "Compression format \"" << compression_format << "\" is not supported.";
+      ROSBAG2_COMPRESSION_LOG_ERROR_STREAM(errmsg.str());
+      throw std::invalid_argument{errmsg.str()};
+    }
   }
 
   /// See CompressionFactory::create_decompressor for documentation.
   std::unique_ptr<rosbag2_compression::BaseDecompressorInterface>
-  create_decompressor(const std::string &)
+  create_decompressor(const std::string & compression_format)
   {
-    throw std::logic_error{"Not implemented"};
+    if (case_insensitive_compare(compression_format, kCompressionFormatZstd)) {
+      return std::make_unique<rosbag2_compression::ZstdDecompressor>();
+    } else {
+      std::stringstream errmsg;
+      errmsg << "Compression format \"" << compression_format << "\" is not supported.";
+      ROSBAG2_COMPRESSION_LOG_ERROR_STREAM(errmsg.str());
+      throw std::invalid_argument{errmsg.str()};
+    }
   }
 };
 }  // namespace rosbag2_compression
---
+++ rosbag2_compression/test/rosbag2_compression/test_compression_factory.cpp
@@ -0,0 +1,91 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <algorithm>
+
+#include "rosbag2_compression/compression_factory.hpp"
+
+namespace
+{
+
+std::string to_lower(const std::string & text)
+{
+  std::string lowercase_text = text;
+  std::transform(lowercase_text.begin(), lowercase_text.end(), lowercase_text.begin(), ::tolower);
+  return lowercase_text;
+}
+}  // namespace
+
+class CompressionFactoryTest : public ::testing::Test
+{
+public:
+  rosbag2_compression::CompressionFactory factory{};
+};
+
+TEST_F(CompressionFactoryTest, creates_zstd_compressor) {
+  const auto compression_format = "zstd";
+  auto zstd_compressor = factory.create_compressor(compression_format);
+  ASSERT_EQ(compression_format, zstd_compressor->get_compression_identifier());
+}
+
+TEST_F(CompressionFactoryTest, creates_zstd_compressor_caseinsensitive)
+{
+  const auto compression_format = "ZsTd";
+  const auto lowercase_compression_format = to_lower(compression_format);
+  auto zstd_compressor = factory.create_compressor(compression_format);
+  ASSERT_EQ(lowercase_compression_format, zstd_compressor->get_compression_identifier());
+}
+
+TEST_F(CompressionFactoryTest, creates_zstd_compressor_uppercase)
+{
+  const auto compression_format = "ZSTD";
+  const auto lowercase_compression_format = to_lower(compression_format);
+  auto zstd_compressor = factory.create_compressor(compression_format);
+  ASSERT_EQ(lowercase_compression_format, zstd_compressor->get_compression_identifier());
+}
+
+TEST_F(CompressionFactoryTest, creates_zstd_decompressor) {
+  const auto compression_format = "zstd";
+  auto zstd_decompressor = factory.create_decompressor(compression_format);
+  ASSERT_EQ(compression_format, zstd_decompressor->get_decompression_identifier());
+}
+
+TEST_F(CompressionFactoryTest, creates_zstd_decompressor_caseinsensitive)
+{
+  const auto compression_format = "ZsTd";
+  const auto lowercase_compression_format = to_lower(compression_format);
+  auto zstd_compressor = factory.create_decompressor(compression_format);
+  ASSERT_EQ(lowercase_compression_format, zstd_compressor->get_decompression_identifier());
+}
+
+TEST_F(CompressionFactoryTest, creates_zstd_decompressor_uppercase)
+{
+  const auto compression_format = "ZSTD";
+  const auto lowercase_compression_format = to_lower(compression_format);
+  auto zstd_compressor = factory.create_decompressor(compression_format);
+  ASSERT_EQ(lowercase_compression_format, zstd_compressor->get_decompression_identifier());
+}
+
+TEST_F(CompressionFactoryTest, throws_on_bad_compressor_format) {
+  const auto compression_format = "foo";
+  ASSERT_THROW(factory.create_compressor(compression_format), std::invalid_argument);
+}
+
+TEST_F(CompressionFactoryTest, throws_on_bad_decompressor_format) {
+  const auto compression_format = "bar";
+  ASSERT_THROW(factory.create_decompressor(compression_format), std::invalid_argument);
+}
