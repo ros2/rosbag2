8affccf3d1e7940a24b6d88b10ad80a8cb88f4cf
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-03-30 19:13:39 -0700

    Add QoS to the GenericSubscription / GenericPublisher APIs
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rosbag2_transport/src/rosbag2_transport/generic_subscription.hpp
+++ rosbag2_transport/src/rosbag2_transport/generic_subscription.hpp
@@ -49,6 +49,7 @@ public:
     rclcpp::node_interfaces::NodeBaseInterface * node_base,
     const rosidl_message_type_support_t & ts,
     const std::string & topic_name,
+    const rclcpp::QoS & qos,
     std::function<void(std::shared_ptr<rmw_serialized_message_t>)> callback);
 
   // Same as create_serialized_message() as the subscription is to serialized_messages only
