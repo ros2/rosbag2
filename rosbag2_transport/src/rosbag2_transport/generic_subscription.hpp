0238751d4f86dff4a60e955a1805bf3f96f2ea1e
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-03-31 18:06:11 -0700

    Subscribe to topics using the common offered QoS, if there is one, with sufficient warning in the cases where we can't
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rosbag2_transport/src/rosbag2_transport/generic_subscription.hpp
+++ rosbag2_transport/src/rosbag2_transport/generic_subscription.hpp
@@ -68,12 +68,16 @@ public:
 
   void return_serialized_message(std::shared_ptr<rmw_serialized_message_t> & message) override;
 
+  // Provide a const reference to the QoS Profile used to create this subscription.
+  const rclcpp::QoS & qos_profile() const;
+
 private:
   RCLCPP_DISABLE_COPY(GenericSubscription)
 
   std::shared_ptr<rmw_serialized_message_t> borrow_serialized_message(size_t capacity);
   rcutils_allocator_t default_allocator_;
   std::function<void(std::shared_ptr<rmw_serialized_message_t>)> callback_;
+  const rclcpp::QoS qos_;
 };
 
 }  // namespace rosbag2_transport
