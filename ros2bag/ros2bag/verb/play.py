431a952a423eebe38c49a91d39d587ddc0857d0a
Anas Abou Allaban <aabouallaban@pm.me>
2020-04-08 14:37:52 -0400

    Add overrides to play verb
    Signed-off-by: Anas Abou Allaban <aabouallaban@pm.me>

--- ros2bag/ros2bag/verb/play.py
+++ ros2bag/ros2bag/verb/play.py
@@ -14,9 +14,11 @@
 
 from ros2bag.api import check_path_exists
 from ros2bag.api import check_positive_float
+from ros2bag.api import convert_yaml_to_qos_profile
 from ros2bag.api import print_error
 from ros2bag.verb import VerbExtension
 from ros2cli.node import NODE_NAME_PREFIX
+import yaml
 
 
 class PlayVerb(VerbExtension):
@@ -36,8 +38,20 @@ class PlayVerb(VerbExtension):
         parser.add_argument(
             '-r', '--rate', type=check_positive_float, default=1.0,
             help='rate at which to play back messages. Valid range > 0.0.')
+        parser.add_argument(
+            '--qos-profile-overrides-path', type=FileType('r'),
+            help='Path to a yaml file defining overrides of the QoS profile for specific topics.')
 
     def main(self, *, args):  # noqa: D102
+        qos_profile_overrides = {}  # Specify a valid default
+        if args.qos_profile_overrides_path:
+            qos_profile_dict = yaml.safe_load(args.qos_profile_overrides_path)
+            try:
+                qos_profile_overrides = convert_yaml_to_qos_profile(
+                    qos_profile_dict)
+            except (InvalidQoSProfileException, ValueError) as e:
+                return print_error(str(e))
+
         # NOTE(hidmic): in merged install workspaces on Windows, Python entrypoint lookups
         #               combined with constrained environments (as imposed by colcon test)
         #               may result in DLL loading failures when attempting to import a C
@@ -49,4 +63,5 @@ class PlayVerb(VerbExtension):
             storage_id=args.storage,
             node_prefix=NODE_NAME_PREFIX,
             read_ahead_queue_size=args.read_ahead_queue_size,
-            rate=args.rate)
+            rate=args.rate,
+            qos_profile_overrides=qos_profile_overrides)
---
+++ ros2bag/test/test_play_qos_profiles.py
@@ -0,0 +1,82 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import contextlib
+from pathlib import Path
+import re
+import unittest
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+import launch_testing
+import launch_testing.actions
+import launch_testing.asserts
+import launch_testing.markers
+import launch_testing.tools
+
+import pytest
+
+
+RESOURCES_PATH = Path(__file__).parent / 'resources'
+TEST_NODE = 'ros2bag_record_qos_profile_test_node'
+TEST_NAMESPACE = 'ros2bag_record_qos_profile'
+ERROR_STRING = r'\[ERROR] \[ros2bag]:'
+
+
+@pytest.mark.rostest
+@launch_testing.markers.keep_alive
+def generate_test_description():
+    return LaunchDescription([launch_testing.actions.ReadyToTest()])
+
+
+class TestRos2BagPlay(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls, launch_service, proc_info, proc_output):
+        @contextlib.contextmanager
+        def launch_bag_command(self, arguments, **kwargs):
+            pkg_command_action = ExecuteProcess(
+                cmd=['ros2', 'bag', *arguments],
+                additional_env={'PYTHONUNBUFFERED': '1'},
+                name='ros2bag-cli',
+                output='screen',
+                **kwargs
+            )
+            with launch_testing.tools.launch_process(
+                    launch_service, pkg_command_action, proc_info, proc_output
+            ) as pkg_command:
+                yield pkg_command
+        cls.launch_bag_command = launch_bag_command
+
+    def test_qos_simple(self):
+        profile_path = RESOURCES_PATH / 'qos_profile.yaml'
+        bag_path = RESOURCES_PATH / 'empty_bag'
+        arguments = ['play', '--qos-profile-overrides-path', profile_path.as_posix(),
+                     bag_path.as_posix()]
+        with self.launch_bag_command(arguments=arguments) as bag_command:
+            bag_command.wait_for_shutdown(timeout=5)
+        expected_string_regex = re.compile(ERROR_STRING)
+        matches = expected_string_regex.search(bag_command.output)
+        assert not matches, print('ros2bag CLI did not produce the expected output')
+
+    def test_qos_incomplete(self):
+        profile_path = RESOURCES_PATH / 'incomplete_qos_profile.yaml'
+        bag_path = RESOURCES_PATH / 'empty_bag'
+        arguments = ['play', '--qos-profile-overrides-path', profile_path.as_posix(),
+                     bag_path.as_posix()]
+        with self.launch_bag_command(arguments=arguments) as bag_command:
+            bag_command.wait_for_shutdown(timeout=5)
+        expected_string_regex = re.compile(ERROR_STRING)
+        matches = expected_string_regex.search(bag_command.output)
+        assert not matches, print('ros2bag CLI did not produce the expected output')
