c0a218834042ad6e7abe84c71eecb70df60e2f9f
Mabel Zhang <mabel@openrobotics.org>
2020-04-09 19:03:21 -0400

    add play end to end test for filter topic
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

--- rosbag2_tests/test/rosbag2_tests/test_rosbag2_play_end_to_end.cpp
+++ rosbag2_tests/test/rosbag2_tests/test_rosbag2_play_end_to_end.cpp
@@ -118,42 +118,89 @@ TEST_F(PlayEndToEndTestFixture, play_fails_gracefully_if_needed_coverter_plugin_
     error_output, HasSubstr("Requested converter for format 'wrong_format' does not exist"));
 }
 
-/* TODO implement this test after (re)set_filter() has been added to Reader and transport
 TEST_F(PlayEndToEndTestFixture, play_filters_by_topic) {
-  sub_->add_subscription<test_msgs::msg::Arrays>("/array_topic", 2);
+  // Play a specific topic
   sub_->add_subscription<test_msgs::msg::BasicTypes>("/test_topic", 3);
+  sub_->add_subscription<test_msgs::msg::Arrays>("/array_topic", 0);
 
   auto subscription_future = sub_->spin_subscriptions();
 
-  auto exit_code = execute_and_wait_until_completion("ros2 bag play cdr_test", database_path_);
+  auto exit_code = execute_and_wait_until_completion(
+    "ros2 bag play cdr_test --topics /test_topic",
+    database_path_);
 
   subscription_future.get();
 
-  auto topic1_messages = sub_->get_received_messages<test_msgs::msg::BasicTypes>("/test_topic");
-  auto topic2_messages = sub_->get_received_messages<test_msgs::msg::Arrays>(
+  auto primitive_messages = sub_->get_received_messages<test_msgs::msg::BasicTypes>("/test_topic");
+  auto array_messages = sub_->get_received_messages<test_msgs::msg::Arrays>(
     "/array_topic");
 
   EXPECT_THAT(exit_code, Eq(EXIT_SUCCESS));
 
-  EXPECT_THAT(topic1_messages, SizeIs(Ge(3u)));
-  EXPECT_THAT(
-    topic1_messages,
-    Each(Pointee(Field(&test_msgs::msg::BasicTypes::string_value, "test"))));
+  EXPECT_THAT(primitive_messages, SizeIs(Ge(3u)));
+  EXPECT_THAT(array_messages, SizeIs(Ge(0u)));
 
-  EXPECT_THAT(topic2_messages, SizeIs(Ge(2u)));
-  EXPECT_THAT(
-    topic2_messages,
-    Each(
-      Pointee(
-        Field(
-          &test_msgs::msg::Arrays::bool_values,
-          ElementsAre(true, false, true)))));
-  EXPECT_THAT(
-    topic2_messages,
-    Each(
-      Pointee(
-        Field(
-          &test_msgs::msg::Arrays::string_values,
-          ElementsAre("Complex Hello1", "Complex Hello2", "Complex Hello3")))));
+  // Play a different topic
+  sub_ = std::make_unique<SubscriptionManager>();
+  sub_->add_subscription<test_msgs::msg::BasicTypes>("/test_topic", 0);
+  sub_->add_subscription<test_msgs::msg::Arrays>("/array_topic", 2);
+
+  subscription_future = sub_->spin_subscriptions();
+
+  exit_code = execute_and_wait_until_completion("ros2 bag play --topics /array_topic -- cdr_test",
+      database_path_);
+
+  subscription_future.get();
+
+  primitive_messages = sub_->get_received_messages<test_msgs::msg::BasicTypes>("/test_topic");
+  array_messages = sub_->get_received_messages<test_msgs::msg::Arrays>(
+    "/array_topic");
+
+  EXPECT_THAT(exit_code, Eq(EXIT_SUCCESS));
+
+  EXPECT_THAT(primitive_messages, SizeIs(Ge(0u)));
+  EXPECT_THAT(array_messages, SizeIs(Ge(2u)));
+
+  // Play all topics
+  sub_ = std::make_unique<SubscriptionManager>();
+  sub_->add_subscription<test_msgs::msg::BasicTypes>("/test_topic", 3);
+  sub_->add_subscription<test_msgs::msg::Arrays>("/array_topic", 2);
+
+  subscription_future = sub_->spin_subscriptions();
+
+  exit_code = execute_and_wait_until_completion(
+    "ros2 bag play --topics /test_topic /array_topic -- cdr_test",
+    database_path_);
+
+  subscription_future.get();
+
+  primitive_messages = sub_->get_received_messages<test_msgs::msg::BasicTypes>("/test_topic");
+  array_messages = sub_->get_received_messages<test_msgs::msg::Arrays>(
+    "/array_topic");
+
+  EXPECT_THAT(exit_code, Eq(EXIT_SUCCESS));
+
+  EXPECT_THAT(primitive_messages, SizeIs(Ge(3u)));
+  EXPECT_THAT(array_messages, SizeIs(Ge(2u)));
+
+  // Play a non-existent topic
+  sub_ = std::make_unique<SubscriptionManager>();
+  sub_->add_subscription<test_msgs::msg::BasicTypes>("/test_topic", 0);
+  sub_->add_subscription<test_msgs::msg::Arrays>("/array_topic", 0);
+
+  subscription_future = sub_->spin_subscriptions();
+
+  exit_code = execute_and_wait_until_completion(
+    "ros2 bag play --topics /nonexistent_topic -- cdr_test", database_path_);
+
+  subscription_future.get();
+
+  primitive_messages = sub_->get_received_messages<test_msgs::msg::BasicTypes>("/test_topic");
+  array_messages = sub_->get_received_messages<test_msgs::msg::Arrays>(
+    "/array_topic");
+
+  EXPECT_THAT(exit_code, Eq(EXIT_SUCCESS));
+
+  EXPECT_THAT(primitive_messages, SizeIs(Ge(0u)));
+  EXPECT_THAT(array_messages, SizeIs(Ge(0u)));
 }
-*/
