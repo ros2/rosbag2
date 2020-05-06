2d60b00d2ea44f1c1149d054c6f087cb00baac65
Knese Karsten <karsten@openrobotics.org>
2020-05-06 14:28:44 -0700

    correct library bookkeeping
    Signed-off-by: Knese Karsten <karsten@openrobotics.org>

--- rosbag2_cpp/test/rosbag2_cpp/types/test_ros2_message.cpp
+++ rosbag2_cpp/test/rosbag2_cpp/types/test_ros2_message.cpp
@@ -46,14 +46,15 @@ public:
 
   auto get_allocated_message(const std::string & message_type)
   {
-    auto library = rosbag2_cpp::get_typesupport_library(
+    library_ = rosbag2_cpp::get_typesupport_library(
       message_type, "rosidl_typesupport_introspection_cpp");
     auto introspection_ts = rosbag2_cpp::get_typesupport_handle(
-      message_type, "rosidl_typesupport_introspection_cpp", library);
+      message_type, "rosidl_typesupport_introspection_cpp", library_);
 
     return rosbag2_cpp::allocate_introspection_message(introspection_ts, &allocator_);
   }
 
+  std::shared_ptr<rcpputils::SharedLibrary> library_;
   rcutils_allocator_t allocator_;
 };
 
