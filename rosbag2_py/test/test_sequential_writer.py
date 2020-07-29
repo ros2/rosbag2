ebf3a0190788055cf37530b5c57b3c22846f6ada
Mabel Zhang <mabel@openrobotics.org>
2020-07-29 02:16:34 -0400

    fix import error
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

--- rosbag2_py/test/test_sequential_writer.py
+++ rosbag2_py/test/test_sequential_writer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import os
+import os
 
 from common import get_rosbag_options
 from rclpy.serialization import deserialize_message, serialize_message
