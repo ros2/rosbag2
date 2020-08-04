9fe17cb12e9d12cabbf60a8f49fc599dab901617
Jesse Ikawa <jikawa@amazon.com>
2020-08-04 11:07:29 -0700

    modify style
    Signed-off-by: Jesse Ikawa <jikawa@amazon.com>

--- ros2bag/ros2bag/api/__init__.py
+++ ros2bag/ros2bag/api/__init__.py
@@ -44,7 +44,8 @@ def dict_to_duration(time_dict: Optional[Dict[str, int]]) -> Duration:
     """Convert a QoS duration profile from YAML into an rclpy Duration."""
     if time_dict:
         try:
-            if Duration(seconds=time_dict['sec'], nanoseconds=time_dict['nsec']) < Duration(seconds=0):
+            if (Duration(seconds=time_dict['sec'], nanoseconds=time_dict['nsec']) < 
+                    Duration(seconds=0)):
                 raise ValueError('Time duration may not be a negative value.')
             return Duration(seconds=time_dict['sec'], nanoseconds=time_dict['nsec'])
         except KeyError:
