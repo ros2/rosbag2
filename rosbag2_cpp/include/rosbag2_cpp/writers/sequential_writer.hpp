4af4676ca6d90a8ea599eeb6befdad5adc5fe5bf
Jaison Titus <jaisontj@amazon.com>
2020-10-01 18:30:02 -0700

    Remove some code duplication between SequentialWriter and SequentialCompressionWriter (#527)
    Remove code duplication between SequentialWriter and SequentialCompressionWriter
    
    Signed-off-by: Jaison Titus <jaisontj@amazon.com>

--- rosbag2_cpp/include/rosbag2_cpp/writers/sequential_writer.hpp
+++ rosbag2_cpp/include/rosbag2_cpp/writers/sequential_writer.hpp
@@ -102,7 +102,7 @@ public:
    */
   void write(std::shared_ptr<rosbag2_storage::SerializedBagMessage> message) override;
 
-private:
+protected:
   std::string base_folder_;
   std::unique_ptr<rosbag2_storage::StorageFactoryInterface> storage_factory_;
   std::shared_ptr<SerializationFormatConverterFactoryInterface> converter_factory_;
@@ -128,16 +128,23 @@ private:
   rosbag2_storage::BagMetadata metadata_;
 
   // Closes the current backed storage and opens the next bagfile.
-  void split_bagfile();
+  virtual void split_bagfile();
 
   // Checks if the current recording bagfile needs to be split and rolled over to a new file.
   bool should_split_bagfile() const;
 
   // Prepares the metadata by setting initial values.
-  void init_metadata();
+  virtual void init_metadata();
 
   // Record TopicInformation into metadata
   void finalize_metadata();
+
+  // Helper method used by write to get the message in a format that is ready to be written.
+  // Common use cases include converting the message using the converter or
+  // performing other operations like compression on it
+  virtual std::shared_ptr<rosbag2_storage::SerializedBagMessage>
+  get_writeable_message(
+    std::shared_ptr<rosbag2_storage::SerializedBagMessage> message);
 };
 
 }  // namespace writers
