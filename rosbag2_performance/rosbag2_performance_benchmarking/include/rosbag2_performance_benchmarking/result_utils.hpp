4b722646dae01babbe38fa8bba83d77afb5a313a
Adam Dabrowski <adam.dabrowski@robotec.ai>
2021-01-12 15:14:46 +0100

    linters
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>

--- rosbag2_performance/rosbag2_performance_benchmarking/include/rosbag2_performance_benchmarking/result_utils.hpp
+++ rosbag2_performance/rosbag2_performance_benchmarking/include/rosbag2_performance_benchmarking/result_utils.hpp
@@ -19,6 +19,7 @@
 #include <fstream>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "rosbag2_storage/storage_options.hpp"
 #include "rosbag2_storage/metadata_io.hpp"
@@ -41,64 +42,66 @@
 
 namespace result_utils
 {
-  int get_message_count_from_metadata(const std::string & uri)
-  {
-    int total_recorded_count = 0;
-    std::string metadata_filename(rosbag2_storage::MetadataIo::metadata_filename);
-    std::string metadata_path = uri + "/" + metadata_filename;
-    try {
-      YAML::Node yaml_file = YAML::LoadFile(metadata_path);
-      total_recorded_count = yaml_file["rosbag2_bagfile_information"]["message_count"].as<int>();
-    } catch (const YAML::Exception & ex) {
-      throw std::runtime_error(
-              std::string("Exception on parsing metadata file to get total message count: ") +
-              metadata_path + " " +
-              ex.what());
+
+int get_message_count_from_metadata(const std::string & uri)
+{
+  int total_recorded_count = 0;
+  std::string metadata_filename(rosbag2_storage::MetadataIo::metadata_filename);
+  std::string metadata_path = uri + "/" + metadata_filename;
+  try {
+    YAML::Node yaml_file = YAML::LoadFile(metadata_path);
+    total_recorded_count = yaml_file["rosbag2_bagfile_information"]["message_count"].as<int>();
+  } catch (const YAML::Exception & ex) {
+    throw std::runtime_error(
+            std::string("Exception on parsing metadata file to get total message count: ") +
+            metadata_path + " " +
+            ex.what());
+  }
+  return total_recorded_count;
+}
+
+void write_benchmark_results(
+  const std::vector<PublisherGroupConfig> & publisher_groups_config,
+  const BagConfig & bag_config,
+  const std::string & results_file)
+{
+  bool new_file = false;
+  { // test if file exists - we want to write a csv header after creation if not
+    // use std::filesystem when switching to C++17
+    std::ifstream test_existence(results_file);
+    if (!test_existence) {
+      new_file = true;
     }
-    return total_recorded_count;
   }
 
-  void write_benchmark_results(
-    const std::vector<PublisherGroupConfig> & publisher_groups_config,
-    const BagConfig & bag_config,
-    const std::string & results_file)
-  {
-    bool new_file = false;
-    { // test if file exists - we want to write a csv header after creation if not
-      // use std::filesystem when switching to C++17
-      std::ifstream test_existence(results_file);
-      if (!test_existence) {
-        new_file = true;
-      }
-    }
+  // append, we want to accumulate results from multiple runs
+  std::ofstream output_file(results_file, std::ios_base::app);
+  if (!output_file.is_open()) {
+    throw std::runtime_error(std::string("Could not open file: ") + results_file);
+  }
 
-    // append, we want to accumulate results from multiple runs
-    std::ofstream output_file(results_file, std::ios_base::app);
-    if (!output_file.is_open()) {
-      throw std::runtime_error(std::string("Could not open file: ") + results_file);
-    }
+  if (new_file) {
+    output_file << "instances frequency message_size total_messages_sent cache_size compression ";
+    output_file << "total_produced total_recorded_count\n";
+  }
 
-    if (new_file) {
-      output_file << "instances frequency message_size total_messages_sent cache_size compression ";
-      output_file << "total_produced total_recorded_count\n";
-    }
+  int total_recorded_count = get_message_count_from_metadata(bag_config.storage_options.uri);
 
-    int total_recorded_count = get_message_count_from_metadata(bag_config.storage_options.uri);
+  for (const auto & c : publisher_groups_config) {
+    output_file << c.count << " ";
+    output_file << c.producer_config.frequency << " ";
+    output_file << c.producer_config.message_size << " ";
+    output_file << c.producer_config.max_count << " ";
+    output_file << bag_config.storage_options.max_cache_size << " ";
+    output_file << bag_config.compression_format << " ";
 
-    for (const auto & c : publisher_groups_config) {
-      output_file << c.count << " ";
-      output_file << c.producer_config.frequency << " ";
-      output_file << c.producer_config.message_size << " ";
-      output_file << c.producer_config.max_count << " ";
-      output_file << bag_config.storage_options.max_cache_size << " ";
-      output_file << bag_config.compression_format << " ";
-
-      // TODO(adamdbrw) - this is a result for the entire group, but we don't yet have per-group stats
-      // For now, these need to be summed for each group
-      auto total_messages_produced = c.producer_config.max_count * c.count;
-      output_file << total_messages_produced << " ";
-      output_file << total_recorded_count << std::endl;
-    }
+    // TODO(adamdbrw) - this is a result for the entire group,
+    // but we don't yet have per-group stats.
+    // For now, these need to be summed for each group
+    auto total_messages_produced = c.producer_config.max_count * c.count;
+    output_file << total_messages_produced << " ";
+    output_file << total_recorded_count << std::endl;
+  }
 }
 
 }  // namespace result_utils
