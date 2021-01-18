0444bd41a857cc6ff9b86e2018b742b8f8e90d60
Jacob Hassold <jhassold@dcscorp.com>
2021-01-18 08:54:35 -0500

    Test reordering license header Distro A, OPSEC #4584
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- rosbag2_tests/test/rosbag2_tests/test_rosbag2_reindex_end_to_end.cpp
+++ rosbag2_tests/test/rosbag2_tests/test_rosbag2_reindex_end_to_end.cpp
@@ -1,6 +1,3 @@
-// DISTRIBUTION A. Approved for public release; distribution unlimited.
-// OPSEC #4584.
-//
 // Copyright (c) 2020 DCS Corporation
 // All Rights Reserved
 //
@@ -11,6 +8,9 @@
 //    Caution-http://www.apache.org/licenses/LICENSE-2.0
 //
 // The software/firmware is provided to you on an As-Is basis.
+//
+// DISTRIBUTION A. Approved for public release; distribution unlimited.
+// OPSEC #4584.
 //
 // Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
 // Part 252.227-7013 or 7014 (Feb 2014).
