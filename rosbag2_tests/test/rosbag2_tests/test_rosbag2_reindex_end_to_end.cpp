94acad2f566ec7fb96f14d3844e09d0d8b8832e3
Jacob Hassold <jhassold@dcscorp.com>
2021-01-18 08:54:34 -0500

    FIXED USING WRONG LICENSE Distro A, OPSEC #4584
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- rosbag2_tests/test/rosbag2_tests/test_rosbag2_reindex_end_to_end.cpp
+++ rosbag2_tests/test/rosbag2_tests/test_rosbag2_reindex_end_to_end.cpp
@@ -1,16 +1,21 @@
-// Copyright 2018, Bosch Software Innovations GmbH.
+// DISTRIBUTION A. Approved for public release; distribution unlimited.
+// OPSEC #4584.
+//
+// Copyright (c) 2020 DCS Corporation
+// All Rights Reserved
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//    Caution-http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// The software/firmware is provided to you on an As-Is basis.
+//
+// Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
+// Part 252.227-7013 or 7014 (Feb 2014).
+//
+// This notice must appear in all copies of this file and its derivatives.
 
 #include <gmock/gmock.h>
 
