cbfee13f64fdac92ec5ea59340b1754ed037922d
Jacob Hassold <jhassold@dcscorp.com>
2021-02-12 08:03:49 -0500

    First working version of unit tests Distro A, OPSEC #4584
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- rosbag2_cpp/test/rosbag2_cpp/test_reindexer.cpp
+++ rosbag2_cpp/test/rosbag2_cpp/test_reindexer.cpp
@@ -22,11 +22,13 @@
 
 #include <gmock/gmock.h>
 
+#include <algorithm>
 #include <memory>
 #include <string>
 #include <utility>
 #include <vector>
 
+#include "rcpputils/asserts.hpp"
 #include "rcpputils/filesystem_helper.hpp"
 
 #include "rosbag2_cpp/readers/sequential_reader.hpp"
@@ -50,6 +52,7 @@ public:
   ReindexTestFixture()
   {
     database_path = _SRC_REINDEX_DIR_PATH;
+    target_dir = database_path + "/target_metadata";
   }
 
   // std::shared_ptr<NiceMock<MockStorage>> storage_;
@@ -57,16 +60,75 @@ public:
   // std::unique_ptr<rosbag2_cpp::Reader> reader_;
   // std::string storage_serialization_format_;
   std::string database_path;
+  std::string target_dir;
   // rosbag2_storage::StorageOptions default_storage_options_;
 };
 
 TEST_F(ReindexTestFixture, test_multiple_files) {
+  auto bag_dir = database_path + "/multiple_files";
   std::unique_ptr<rosbag2_cpp::reindexers::Reindexer> reindexer =
     std::make_unique<rosbag2_cpp::reindexers::Reindexer>();
 
   rosbag2_storage::StorageOptions so = rosbag2_storage::StorageOptions();
-  so.uri = database_path + "/multiple_files";
+  so.uri = bag_dir;
   so.storage_id = "sqlite3";
 
   reindexer->reindex(so);
+
+  // We should have a metadata file at this point
+  auto generated_file = rcpputils::fs::path(bag_dir + "/metadata.yaml");
+  EXPECT_TRUE(generated_file.exists());
+
+  // We made it here. Excellent. Truly excellent.
+  // Read the generated and target metadata
+  auto metadata_io = std::make_unique<rosbag2_storage::MetadataIo>();
+  auto generated_metadata = metadata_io->read_metadata(bag_dir);
+  auto target_metadata = metadata_io->read_metadata(target_dir + "/multiple_files");
+
+  // Compare versions
+  EXPECT_EQ(generated_metadata.version, target_metadata.version);
+
+  // Compare relative file paths
+  for (const auto & gen_rel_path : generated_metadata.relative_file_paths) {
+    EXPECT_TRUE(
+      std::find(
+        target_metadata.relative_file_paths.begin(),
+        target_metadata.relative_file_paths.end(),
+        gen_rel_path) != target_metadata.relative_file_paths.end());
+  }
+
+  // Compare starting times
+  // Disabled for now, since it may not be possible to 100% recreate
+  //   original starting time from metadata
+  // EXPECT_EQ(generated_metadata.starting_time, target_metadata.starting_time);
+
+  // Compare durations
+  // Disabled for now, since I'm not sure how duraction is created, and if it's correct (jhdcs)
+  // EXPECT_EQ(generated_metadata.duration, target_metadata.duration);
+
+  // Compare message count
+  EXPECT_EQ(generated_metadata.message_count, target_metadata.message_count);
+
+  // Compare topic information
+  // Reindexer can only reconstruct topics that had messages, so not all topics may exist
+  for (const auto & gen_topic : generated_metadata.topics_with_message_count) {
+    EXPECT_TRUE(
+      std::find_if(
+        target_metadata.topics_with_message_count.begin(),
+        target_metadata.topics_with_message_count.end(),
+        [&gen_topic](rosbag2_storage::TopicInformation & t) {
+          return (t.topic_metadata.name == gen_topic.topic_metadata.name) &&
+          (t.message_count == gen_topic.message_count) &&
+          (t.topic_metadata.offered_qos_profiles ==
+          gen_topic.topic_metadata.offered_qos_profiles) &&
+          (t.topic_metadata.type == gen_topic.topic_metadata.type);
+        }
+      ) != target_metadata.topics_with_message_count.end()
+    );
+  }
+
+  // Clean up generated file
+  if (generated_file.exists()) {
+    remove(generated_file);
+  }
 }
