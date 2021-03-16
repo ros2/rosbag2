29850b0ce146474ee2258a381dcef8026d144cd3
Michael Orlov <morlovmr@gmail.com>
2021-03-15 23:11:36 -0700

    Rename playback_time to the playback_duartion
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>

--- rosbag2_transport/test/rosbag2_transport/test_play_timing.cpp
+++ rosbag2_transport/test/rosbag2_transport/test_play_timing.cpp
@@ -67,7 +67,7 @@ public:
   }
 
   const std::chrono::nanoseconds message_time_difference_ = 250ms;
-  const std::chrono::nanoseconds original_playback_time_ = 2 * message_time_difference_;
+  const std::chrono::nanoseconds original_playback_duartion_ = 2 * message_time_difference_;
   //  2 corresponds to the number of messages - 1
 
   // There are additional delay after playback in Player::play(..) method which we need to take
@@ -83,9 +83,9 @@ TEST_F(Rosbag2PlayTestTimingFixture, playing_respects_relative_timing_of_stored_
   Rosbag2Transport rosbag2_transport(reader_, writer_);
   auto start = std::chrono::steady_clock::now();
   rosbag2_transport.play(storage_options_, play_options_);
-  auto replay_time = std::chrono::steady_clock::now() - start;
+  auto replay_duration = std::chrono::steady_clock::now() - start;
 
-  ASSERT_THAT(replay_time, Gt(original_playback_time_ + delay_after_playback_));
+  ASSERT_THAT(replay_duration, Gt(original_playback_duartion_ + delay_after_playback_));
 }
 
 TEST_F(Rosbag2PlayTestTimingFixture, playing_respects_rate)
@@ -95,51 +95,49 @@ TEST_F(Rosbag2PlayTestTimingFixture, playing_respects_rate)
   Rosbag2Transport rosbag2_transport(reader_, writer_);
   auto start = std::chrono::steady_clock::now();
   rosbag2_transport.play(storage_options_, play_options_);
-  auto replay_time = std::chrono::steady_clock::now() - start;
+  auto replay_duration = std::chrono::steady_clock::now() - start;
 
-  auto expected_lower_bound_time =
-    std::chrono::duration_cast<std::chrono::nanoseconds>(
-    (1 / play_options_.rate) *
-    original_playback_time_) + delay_after_playback_;
+  auto expected_lower_bound_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+    (1 / play_options_.rate) * original_playback_duartion_) + delay_after_playback_;
 
-  EXPECT_THAT(replay_time, Gt(expected_lower_bound_time));
-  EXPECT_THAT(replay_time, Lt(original_playback_time_ + delay_after_playback_));
+  EXPECT_THAT(replay_duration, Gt(expected_lower_bound_time));
+  EXPECT_THAT(replay_duration, Lt(original_playback_duartion_ + delay_after_playback_));
 
   // Play at 1x speed
   play_options_.rate = 1.0;
   rosbag2_transport = Rosbag2Transport(reader_, writer_);
   start = std::chrono::steady_clock::now();
   rosbag2_transport.play(storage_options_, play_options_);
-  replay_time = std::chrono::steady_clock::now() - start;
+  replay_duration = std::chrono::steady_clock::now() - start;
 
-  EXPECT_THAT(replay_time, Gt(original_playback_time_ + delay_after_playback_));
+  EXPECT_THAT(replay_duration, Gt(original_playback_duartion_ + delay_after_playback_));
 
   // Play at half speed
   play_options_.rate = 0.5;
   rosbag2_transport = Rosbag2Transport(reader_, writer_);
   start = std::chrono::steady_clock::now();
   rosbag2_transport.play(storage_options_, play_options_);
-  replay_time = std::chrono::steady_clock::now() - start;
+  replay_duration = std::chrono::steady_clock::now() - start;
 
-  EXPECT_THAT(replay_time, Gt(2 * original_playback_time_ + delay_after_playback_));
+  EXPECT_THAT(replay_duration, Gt(2 * original_playback_duartion_ + delay_after_playback_));
 
   // Invalid value should result in playing at default rate 1.0
   play_options_.rate = 0.0;
   rosbag2_transport = Rosbag2Transport(reader_, writer_);
   start = std::chrono::steady_clock::now();
   rosbag2_transport.play(storage_options_, play_options_);
-  replay_time = std::chrono::steady_clock::now() - start;
+  replay_duration = std::chrono::steady_clock::now() - start;
 
-  EXPECT_THAT(replay_time, Gt(original_playback_time_ + delay_after_playback_));
+  EXPECT_THAT(replay_duration, Gt(original_playback_duartion_ + delay_after_playback_));
 
   // Invalid value should result in playing at default rate 1.0
   play_options_.rate = -1.23f;
   rosbag2_transport = Rosbag2Transport(reader_, writer_);
   start = std::chrono::steady_clock::now();
   rosbag2_transport.play(storage_options_, play_options_);
-  replay_time = std::chrono::steady_clock::now() - start;
+  replay_duration = std::chrono::steady_clock::now() - start;
 
-  EXPECT_THAT(replay_time, Gt(original_playback_time_ + delay_after_playback_));
+  EXPECT_THAT(replay_duration, Gt(original_playback_duartion_ + delay_after_playback_));
 }
 
 TEST_F(Rosbag2PlayTestTimingFixture, play_pause_resume_respect_play_time) {
@@ -168,9 +166,9 @@ TEST_F(Rosbag2PlayTestTimingFixture, play_pause_resume_respect_play_time) {
   // Play first two messages in pause mode.
   EXPECT_TRUE(player.play_next());
   EXPECT_TRUE(player.play_next());
-  // After that, expected playback time without pause is going to be "original_playback_time_ / 2".
-  // Player shall take in to account messages played in pause mode and adjust playback timeouts
-  // for following messages.
+  // After that, expected playback time without pause is going to be
+  // "original_playback_duartion_ / 2". Player shall take in to account messages played in pause
+  // mode and adjust playback timeouts for following messages.
 
   std::this_thread::sleep_for(35ms);
 
@@ -181,18 +179,17 @@ TEST_F(Rosbag2PlayTestTimingFixture, play_pause_resume_respect_play_time) {
   auto time_elapsed_in_pause = std::chrono::steady_clock::now() - start_in_pause;
 
   play_future_result.wait();
-  auto replay_time = play_future_result.get();
+  auto replay_duration = play_future_result.get();
 
   EXPECT_FALSE(player.play_next());
   EXPECT_FLOAT_EQ(2.0f, player.get_playback_rate());
 
-  auto expected_lower_bound_time =
-    std::chrono::duration_cast<std::chrono::nanoseconds>(
-    (1 / player.get_playback_rate()) *
-    original_playback_time_ / 2) + time_elapsed_in_pause + delay_after_playback_;
+  auto expected_lower_bound_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+    (1 / player.get_playback_rate()) * original_playback_duartion_ / 2) +
+    time_elapsed_in_pause + delay_after_playback_;
 
-  EXPECT_THAT(replay_time, Gt(expected_lower_bound_time));
-  EXPECT_THAT(replay_time, Lt(original_playback_time_ + delay_after_playback_));
+  EXPECT_THAT(replay_duration, Gt(expected_lower_bound_time));
+  EXPECT_THAT(replay_duration, Lt(original_playback_duartion_ + delay_after_playback_));
 
   // Make sure that we can replay one more time with the same player object after resetting reader.
   reader_->open(storage_options_, {"", rmw_get_serialization_format()});
@@ -200,6 +197,6 @@ TEST_F(Rosbag2PlayTestTimingFixture, play_pause_resume_respect_play_time) {
   play_options_.rate = 1.0f;
   auto start = std::chrono::steady_clock::now();
   player.play(play_options_);
-  replay_time = std::chrono::steady_clock::now() - start;
-  EXPECT_THAT(replay_time, Gt(original_playback_time_ + delay_after_playback_));
+  replay_duration = std::chrono::steady_clock::now() - start;
+  EXPECT_THAT(replay_duration, Gt(original_playback_duartion_ + delay_after_playback_));
 }
