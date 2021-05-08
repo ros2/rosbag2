de7c2bc47e135b05d270e06692b5924da6b14a00
Michael Orlov <morlovmr@gmail.com>
2021-05-07 19:37:18 -0700

    Make mocked read function to block until command from unit test to address race conditions in tests.
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>

--- keyboard_handler/test/keyboard_handler_unix_tests.cpp
+++ keyboard_handler/test/keyboard_handler_unix_tests.cpp
@@ -16,6 +16,8 @@
 #include <string>
 #include <memory>
 #include <algorithm>
+#include <utility>
+#include <condition_variable>
 #include "gmock/gmock.h"
 #include "fake_recorder.hpp"
 #include "fake_player.hpp"
@@ -27,8 +29,6 @@ using ::testing::AtLeast;
 using ::testing::_;
 using ::testing::NiceMock;
 
-using namespace std::chrono_literals;
-
 namespace
 {
 int isatty_mock(int fd) {return 1;}
@@ -38,21 +38,88 @@ int tcgetattr_mock(int fd, struct termios * termios_p) {return 0;}
 int tcsetattr_mock(int fd, int optional_actions, const struct termios * termios_p) {return 0;}
 }  // namespace
 
-ACTION_P(SetArg1FromCharPtr, param)
+// Mock the public system calls APIs. read() function become the stub function.
+class MockSystemCalls
 {
-  strncpy(static_cast<char *>(arg1), param, arg2);  // NOLINT
-}
+public:
+  ssize_t read(int fd, void * buff_ptr, size_t n_bytes)
+  {
+    std::unique_lock<std::mutex> lk(read_fn_mutex_);
+    if (wait_on_read_) {
+      cv_read_.wait(lk, [this]() {return unblock_read_;});
+      unblock_read_ = false;
+    }
+    strncpy(static_cast<char *>(buff_ptr), read_returning_str_value_.c_str(), n_bytes);
+    return read_returning_str_value_.length();
+  }
+
+  void read_will_return_once(const std::string & str)
+  {
+    {
+      std::lock_guard<std::mutex> lk(read_fn_mutex_);
+      read_returning_str_value_ = str;
+      wait_on_read_ = true;
+      unblock_read_ = true;
+    }
+    cv_read_.notify_all();
+  }
+
+  void unblock_read()
+  {
+    {
+      std::lock_guard<std::mutex> lk(read_fn_mutex_);
+      unblock_read_ = true;
+      wait_on_read_ = false;
+    }
+    cv_read_.notify_all();
+  }
+
+  void read_will_repeatedly_return(const std::string & str)
+  {
+    {
+      std::lock_guard<std::mutex> lk(read_fn_mutex_);
+      read_returning_str_value_ = str;
+      unblock_read_ = true;
+      wait_on_read_ = false;
+    }
+    cv_read_.notify_all();
+  }
+
+private:
+  std::mutex read_fn_mutex_;
+  std::condition_variable cv_read_;
+  // By default read will block and wait
+  bool wait_on_read_{true};
+  bool unblock_read_{false};
+  std::string read_returning_str_value_{};
+};
+
+std::shared_ptr<MockSystemCalls> g_system_calls_stub;
 
 class MockKeyboardHandler : public KeyboardHandlerUnixImpl
 {
 public:
-  explicit MockKeyboardHandler(const readFunction & read_fn)
-  : KeyboardHandlerUnixImpl(read_fn, isatty_mock, tcgetattr_mock, tcsetattr_mock) {}
+  explicit MockKeyboardHandler(
+    const readFunction & read_fn,
+    std::weak_ptr<MockSystemCalls> system_calls_stub = g_system_calls_stub)
+  : KeyboardHandlerUnixImpl(read_fn, isatty_mock, tcgetattr_mock, tcsetattr_mock),
+    system_calls_stub_(std::move(system_calls_stub)) {}
 
+  ~MockKeyboardHandler() override
+  {
+    auto sys_calls_stub = system_calls_stub_.lock();
+    if (sys_calls_stub) {
+      // unlock read to let inner worker thread to finish
+      sys_calls_stub->unblock_read();
+    }
+  }
   size_t get_number_of_registered_callbacks() const
   {
     return callbacks_.size();
   }
+
+private:
+  std::weak_ptr<MockSystemCalls> system_calls_stub_;
 };
 
 class MockPlayer : public FakePlayer
@@ -61,28 +128,15 @@ public:
   MOCK_METHOD(void, callback_func, (KeyboardHandler::KeyCode key_code));
 };
 
-// Mock the public system calls APIs. read() function become the stub function.
-class MockSystemCalls
-{
-public:
-  MockSystemCalls()
-  {
-    ON_CALL(*this, read(_, _, _)).WillByDefault(Return(0));
-  }
-  MOCK_METHOD(ssize_t, read, (int fd, void * buf_ptr, size_t n_bytes));
-};
-
-std::unique_ptr<NiceMock<MockSystemCalls>> system_calls_stub;
-
 class KeyboardHandlerUnixTest : public ::testing::Test
 {
 public:
   KeyboardHandlerUnixTest()
   {
-    system_calls_stub = std::make_unique<NiceMock<MockSystemCalls>>();
+    g_system_calls_stub = std::make_shared<MockSystemCalls>();
     read_fn_ = [](int fd, void * buf_ptr, size_t n_bytes) -> ssize_t {
-        if (system_calls_stub) {
-          return system_calls_stub->read(fd, buf_ptr, n_bytes);
+        if (g_system_calls_stub) {
+          return g_system_calls_stub->read(fd, buf_ptr, n_bytes);
         } else {
           std::cerr << "Call for non existing unique_ptr" << std::endl;
           return 0;
@@ -92,7 +146,7 @@ public:
 
   ~KeyboardHandlerUnixTest() override
   {
-    system_calls_stub.reset();
+    g_system_calls_stub.reset();
   }
 
 protected:
@@ -100,8 +154,6 @@ protected:
 };
 
 TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_null_callback) {
-  EXPECT_CALL(*system_calls_stub, read(_, _, _)).WillRepeatedly(Return(0));
-
   testing::MockFunction<void(KeyboardHandler::KeyCode keycode)> mock_global_callback;
   EXPECT_CALL(mock_global_callback, Call(Eq(KeyboardHandler::KeyCode::UNKNOWN))).Times(0);
 
@@ -119,11 +171,6 @@ TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_lambdas_in_callbacks) {
     MockKeyboardHandler keyboard_handler(read_fn_);
     const std::string terminal_seq =
       keyboard_handler.get_terminal_sequence(KeyboardHandler::KeyCode::CURSOR_UP);
-
-    EXPECT_CALL(*system_calls_stub, read(_, _, _)).WillRepeatedly(
-      testing::DoAll(
-        SetArg1FromCharPtr(terminal_seq.c_str()), Return(terminal_seq.length())));
-
     ASSERT_EQ(keyboard_handler.get_number_of_registered_callbacks(), 0U);
 
     // Capture std::cout to verify at the end of the test that callbacks was correctly processed
@@ -134,6 +181,7 @@ TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_lambdas_in_callbacks) {
 
     player_shared_ptr->register_callbacks(keyboard_handler);
     ASSERT_EQ(keyboard_handler.get_number_of_registered_callbacks(), 2U);
+    g_system_calls_stub->read_will_return_once(terminal_seq);
   }
   // Check that callbacks was called with proper key code.
   std::string test_output = testing::internal::GetCapturedStdout();
@@ -148,11 +196,6 @@ TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_lambdas_in_callbacks_and_d
     MockKeyboardHandler keyboard_handler(read_fn_);
     const std::string terminal_seq =
       keyboard_handler.get_terminal_sequence(KeyboardHandler::KeyCode::CURSOR_UP);
-
-    EXPECT_CALL(*system_calls_stub, read(_, _, _)).WillRepeatedly(
-      testing::DoAll(
-        SetArg1FromCharPtr(terminal_seq.c_str()), Return(terminal_seq.length())));
-
     ASSERT_EQ(keyboard_handler.get_number_of_registered_callbacks(), 0U);
     {
       auto recorder = FakeRecorder::create();
@@ -167,8 +210,7 @@ TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_lambdas_in_callbacks_and_d
       player_shared_ptr->register_callbacks(keyboard_handler);
       ASSERT_EQ(keyboard_handler.get_number_of_registered_callbacks(), 2U);
     }
-    // Yield CPU resources to give keyboard_handler a chance to process deleted callbacks.
-    std::this_thread::sleep_for(1ms);
+    g_system_calls_stub->read_will_return_once(terminal_seq);
   }
   // Check that callbacks was called for deleted objects and processed properly
   std::string test_output = testing::internal::GetCapturedStdout();
@@ -189,15 +231,12 @@ TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_global_callback) {
   const std::string terminal_seq =
     keyboard_handler.get_terminal_sequence(KeyboardHandler::KeyCode::CAPITAL_E);
 
-  EXPECT_CALL(*system_calls_stub, read(_, _, _)).WillRepeatedly(
-    testing::DoAll(
-      SetArg1FromCharPtr(terminal_seq.c_str()), Return(terminal_seq.length())));
-
   ASSERT_EQ(keyboard_handler.get_number_of_registered_callbacks(), 0U);
   EXPECT_TRUE(
     keyboard_handler.add_key_press_callback(
       mock_global_callback.AsStdFunction(), KeyboardHandler::KeyCode::CAPITAL_E));
   EXPECT_EQ(keyboard_handler.get_number_of_registered_callbacks(), 1U);
+  g_system_calls_stub->read_will_return_once(terminal_seq);
 }
 
 TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_class_member_in_callback) {
@@ -205,20 +244,21 @@ TEST_F(KeyboardHandlerUnixTest, keyboard_handler_with_class_member_in_callback) 
   const std::string terminal_seq =
     keyboard_handler.get_terminal_sequence(KeyboardHandler::KeyCode::CURSOR_DOWN);
 
-  EXPECT_CALL(*system_calls_stub, read(_, _, _)).WillRepeatedly(
-    testing::DoAll(
-      SetArg1FromCharPtr(terminal_seq.c_str()), Return(terminal_seq.length())));
-
-  std::shared_ptr<NiceMock<MockPlayer>> mock_player_sptr(new NiceMock<MockPlayer>());
+  std::shared_ptr<NiceMock<MockPlayer>> mock_player_shared_ptr(new NiceMock<MockPlayer>());
   EXPECT_CALL(
-    *mock_player_sptr,
+    *mock_player_shared_ptr,
     callback_func(Eq(KeyboardHandler::KeyCode::CURSOR_DOWN))).Times(AtLeast(1));
-  EXPECT_CALL(*mock_player_sptr, callback_func(Eq(KeyboardHandler::KeyCode::CURSOR_UP))).Times(0);
+  EXPECT_CALL(
+    *mock_player_shared_ptr,
+    callback_func(Eq(KeyboardHandler::KeyCode::CURSOR_UP))).Times(0);
 
-  auto callback = std::bind(&MockPlayer::callback_func, mock_player_sptr, std::placeholders::_1);
+  auto callback = std::bind(
+    &MockPlayer::callback_func, mock_player_shared_ptr,
+    std::placeholders::_1);
   EXPECT_TRUE(
     keyboard_handler.add_key_press_callback(callback, KeyboardHandler::KeyCode::CURSOR_UP));
   EXPECT_TRUE(
     keyboard_handler.add_key_press_callback(callback, KeyboardHandler::KeyCode::CURSOR_DOWN));
+  g_system_calls_stub->read_will_return_once(terminal_seq);
 }
 #endif  // #ifndef _WIN32
