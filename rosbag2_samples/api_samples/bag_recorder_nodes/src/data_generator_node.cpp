754a3d30440a3df8023757ff1fb701c30978be80
Geoffrey Biggs <gbiggs@killbots.net>
2021-06-30 15:18:54 +0900

    Add code samples for writing bag files
    Signed-off-by: Geoffrey Biggs <gbiggs@killbots.net>

---
+++ rosbag2_samples/api_samples/bag_recorder_nodes/src/data_generator_node.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+
+#include <chrono>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/serialization.hpp>
+#include <example_interfaces/msg/int32.hpp>
+
+#include <rosbag2_cpp/writer.hpp>
+#include <rosbag2_cpp/writers/sequential_writer.hpp>
+#include <rosbag2_storage/serialized_bag_message.hpp>
+
+using namespace std::chrono_literals;
+
+class DataGenerator : public rclcpp::Node
+{
+public:
+  DataGenerator()
+  : Node("data_generator")
+  {
+    data.data = 0;
+    const rosbag2_cpp::StorageOptions storage_options({"timed_synthetic_bag", "sqlite3"});
+    const rosbag2_cpp::ConverterOptions converter_options(
+      {rmw_get_serialization_format(),
+       rmw_get_serialization_format()});
+    writer_ = std::make_unique<rosbag2_cpp::writers::SequentialWriter>();
+
+    writer_->open(storage_options, converter_options);
+
+    writer_->create_topic(
+      {"synthetic",
+       "example_interfaces/msg/Int32",
+       rmw_get_serialization_format(),
+       ""});
+
+    timer_ = create_wall_timer(1s, std::bind(&DataGenerator::timer_callback, this));
+  }
+
+private:
+  void timer_callback()
+  {
+    auto serializer = rclcpp::Serialization<example_interfaces::msg::Int32>();
+    auto serialized_message = rclcpp::SerializedMessage();
+    serializer.serialize_message(&data, &serialized_message);
+
+    auto bag_message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
+
+    bag_message->serialized_data = std::shared_ptr<rcutils_uint8_array_t>(
+      new rcutils_uint8_array_t,
+      [this](rcutils_uint8_array_t *msg) {
+        auto fini_return = rcutils_uint8_array_fini(msg);
+        delete msg;
+        if (fini_return != RCUTILS_RET_OK) {
+          RCLCPP_ERROR(get_logger(),
+            "Failed to destroy serialized message %s", rcutils_get_error_string().str);
+        }
+      });
+    *bag_message->serialized_data = serialized_message.release_rcl_serialized_message();
+
+    bag_message->topic_name = "synthetic";
+    if (rcutils_system_time_now(&bag_message->time_stamp) != RCUTILS_RET_OK) {
+      RCLCPP_ERROR(get_logger(), "Error getting current time: %s",
+        rcutils_get_error_string().str);
+    }
+
+    writer_->write(bag_message);
+    ++data.data;
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::unique_ptr<rosbag2_cpp::writers::SequentialWriter> writer_;
+  example_interfaces::msg::Int32 data;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DataGenerator>());
+  rclcpp::shutdown();
+  return 0;
+}
+
---
+++ rosbag2_samples/api_samples/bag_recorder_nodes/src/simple_bag_recorder.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+#include <rosbag2_cpp/typesupport_helpers.hpp>
+#include <rosbag2_cpp/writer.hpp>
+#include <rosbag2_cpp/writers/sequential_writer.hpp>
+#include <rosbag2_storage/serialized_bag_message.hpp>
+
+using std::placeholders::_1;
+
+class SimpleBagRecorder : public rclcpp::Node
+{
+public:
+  SimpleBagRecorder()
+  : Node("simple_bag_recorder")
+  {
+    const rosbag2_cpp::StorageOptions storage_options({"my_bag", "sqlite3"});
+    const rosbag2_cpp::ConverterOptions converter_options(
+      {rmw_get_serialization_format(),
+       rmw_get_serialization_format()});
+    writer_ = std::make_unique<rosbag2_cpp::writers::SequentialWriter>();
+
+    writer_->open(storage_options, converter_options);
+
+    writer_->create_topic(
+      {"chatter",
+       "std_msgs/msg/String",
+       rmw_get_serialization_format(),
+       ""});
+
+    subscription_ = create_subscription<std_msgs::msg::String>(
+      "chatter", 10, std::bind(&SimpleBagRecorder::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(std::shared_ptr<rclcpp::SerializedMessage> msg) const
+  {
+    auto bag_message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
+
+    bag_message->serialized_data = std::shared_ptr<rcutils_uint8_array_t>(
+      new rcutils_uint8_array_t,
+      [this](rcutils_uint8_array_t *msg) {
+        auto fini_return = rcutils_uint8_array_fini(msg);
+        delete msg;
+        if (fini_return != RCUTILS_RET_OK) {
+          RCLCPP_ERROR(get_logger(),
+            "Failed to destroy serialized message %s", rcutils_get_error_string().str);
+        }
+      });
+    *bag_message->serialized_data = msg->release_rcl_serialized_message();
+
+    bag_message->topic_name = "chatter";
+    if (rcutils_system_time_now(&bag_message->time_stamp) != RCUTILS_RET_OK) {
+      RCLCPP_ERROR(get_logger(), "Error getting current time: %s",
+        rcutils_get_error_string().str);
+    }
+
+    writer_->write(bag_message);
+  }
+
+  rclcpp::Subscription<rclcpp::SerializedMessage>::SharedPtr subscription_;
+  std::unique_ptr<rosbag2_cpp::writers::SequentialWriter> writer_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SimpleBagRecorder>());
+  rclcpp::shutdown();
+  return 0;
+}
+
