414c4a4704f658b40e6811bb7c45db9cda11e39f
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2021-11-29 17:57:58 -0300

    [ros2bag_transport] Add test case
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

---
+++ rosbag2_transport/test/rosbag2_transport/test_record_all_ignore_leaf_topics.cpp
@@ -0,0 +1,89 @@
+// Copyright 2018, Bosch Software Innovations GmbH.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gmock/gmock.h>
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "test_msgs/msg/arrays.hpp"
+#include "test_msgs/msg/basic_types.hpp"
+#include "test_msgs/message_fixtures.hpp"
+
+#include "rosbag2_test_common/publication_manager.hpp"
+#include "rosbag2_test_common/wait_for.hpp"
+
+#include "rosbag2_transport/recorder.hpp"
+
+#include "record_integration_fixture.hpp"
+
+using namespace std::chrono_literals;  // NOLINT
+
+TEST_F(RecordIntegrationTestFixture, published_messages_from_two_topics_ignore_leaf_topics)
+{
+  auto node = std::make_shared<rclcpp::Node>("test_string_msg_listener_node");
+  auto string_msgs_sub = node->create_subscription<test_msgs::msg::Strings>(
+    "/string_topic", 10, [](test_msgs::msg::Strings::SharedPtr) {});
+
+  auto array_message = get_messages_arrays()[0];
+  array_message->float32_values = {{40.0f, 2.0f, 0.0f}};
+  array_message->bool_values = {{true, false, true}};
+  std::string array_topic = "/array_topic";
+
+  auto string_message = get_messages_strings()[0];
+  string_message->string_value = "Hello World";
+  std::string string_topic = "/string_topic";
+
+  rosbag2_test_common::PublicationManager pub_manager;
+  pub_manager.setup_publisher(array_topic, array_message, 2);
+  pub_manager.setup_publisher(string_topic, string_message, 2);
+
+  rosbag2_transport::RecordOptions record_options = {true, false, {}, "rmw_format", 100ms};
+  record_options.ignore_leaf_topics = true;
+  auto recorder = std::make_shared<rosbag2_transport::Recorder>(
+    std::move(writer_), storage_options_, record_options);
+  recorder->record();
+
+  start_async_spin(recorder);
+
+  ASSERT_TRUE(pub_manager.wait_for_matched(string_topic.c_str()));
+
+  pub_manager.run_publishers();
+
+  auto & writer = recorder->get_writer_handle();
+  MockSequentialWriter & mock_writer =
+    static_cast<MockSequentialWriter &>(writer.get_implementation_handle());
+
+  size_t expected_messages = 2;
+  auto ret = rosbag2_test_common::wait_until_shutdown(
+    std::chrono::seconds(5),
+    [&mock_writer, &expected_messages]() {
+      return mock_writer.get_messages().size() >= expected_messages;
+    });
+  auto recorded_messages = mock_writer.get_messages();
+  // We may receive additional messages from rosout, it doesn't matter,
+  // as long as we have received at least as many total messages as we expect
+  EXPECT_TRUE(ret) << "failed to capture expected messages in time";
+  EXPECT_THAT(recorded_messages, SizeIs(Ge(expected_messages)));
+
+  auto string_messages = filter_messages<test_msgs::msg::Strings>(
+    recorded_messages, string_topic);
+  auto array_messages = filter_messages<test_msgs::msg::Arrays>(
+    recorded_messages, array_topic);
+  ASSERT_THAT(string_messages, SizeIs(2));
+  ASSERT_THAT(array_messages, SizeIs(0));
+  EXPECT_THAT(string_messages[0]->string_value, Eq("Hello World"));
+}
