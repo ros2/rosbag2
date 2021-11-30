b83dec5b58febd35e168043ea9e152ca8ea4e7ba
William Woodall <william@osrfoundation.org>
2021-11-30 15:43:58 -0600

    use expect no throw
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rosbag2_storage_default_plugins/test/rosbag2_storage_default_plugins/sqlite/test_sqlite_storage.cpp
+++ rosbag2_storage_default_plugins/test/rosbag2_storage_default_plugins/sqlite/test_sqlite_storage.cpp
@@ -388,8 +388,10 @@ TEST_F(StorageTestFixture, does_not_throw_on_message_too_big) {
 
   // This should produce a warning, but not an exception.
   std::string msg(sqlite_limit + 1, '\0');
-  this->write_messages_to_sqlite(
-  {
-    {msg, 0, "/too_big_message", "some_type", "some_rmw"}
-  }, writable_storage);
+  EXPECT_NO_THROW({
+    this->write_messages_to_sqlite(
+    {
+      {msg, 0, "/too_big_message", "some_type", "some_rmw"}
+    }, writable_storage);
+  });
 }
