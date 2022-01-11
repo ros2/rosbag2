595dc7036dfd42d6e87965546be20ec4b7ff0856
Jacob Perron <jacob@openrobotics.org>
2022-01-11 12:54:11 -0800

    Fix relative path syntax for cpplint
    Relates to https://github.com/ament/ament_lint/pull/324
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- rosbag2_cpp/src/rosbag2_cpp/rmw_implemented_serialization_format_converter.cpp
+++ rosbag2_cpp/src/rosbag2_cpp/rmw_implemented_serialization_format_converter.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "./rmw_implemented_serialization_format_converter.hpp"
+#include "rmw_implemented_serialization_format_converter.hpp"
 
 #include <memory>
 #include <sstream>
