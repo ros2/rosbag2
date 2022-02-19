c8c17dbc6351b437c555bf1d5863fdf77de11f07
Akash Munagala <akash.munagala@gmail.com>
2022-02-19 11:17:37 -0600

    Remove YAML_CPP_DLL define
    Signed-off-by: Akash Munagala <akash.munagala@gmail.com>

--- rosbag2_storage/include/rosbag2_storage/yaml.hpp
+++ rosbag2_storage/include/rosbag2_storage/yaml.hpp
@@ -17,8 +17,6 @@
 #include <string>
 
 #ifdef _WIN32
-// This is necessary because of a bug in yaml-cpp's cmake
-#define YAML_CPP_DLL
 // This is necessary because yaml-cpp does not always use dllimport/dllexport consistently
 # pragma warning(push)
 # pragma warning(disable:4251)
