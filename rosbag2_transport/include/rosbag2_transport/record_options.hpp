e1d00ceafc6cae8702799e3dba51262b17ad541e
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2022-05-12 12:01:01 +0900

    Add use_sim_time option to record verb (#994)
    * add use_sim_time option to record verb
    
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
    
    * add default argument
    
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
    
    * fix record option
    
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
    
    * Update rosbag2_transport/test/rosbag2_transport/test_record_all_use_sim_time.cpp
    
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
    
    Co-authored-by: Michael Orlov <morlovmr@gmail.com>
    
    * Update rosbag2_transport/test/rosbag2_transport/test_record_all_use_sim_time.cpp
    
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
    
    Co-authored-by: Michael Orlov <morlovmr@gmail.com>
    
    * change clock message
    
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
    
    * fix test
    
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
    
    Co-authored-by: Michael Orlov <morlovmr@gmail.com>

--- rosbag2_transport/include/rosbag2_transport/record_options.hpp
+++ rosbag2_transport/include/rosbag2_transport/record_options.hpp
@@ -47,6 +47,7 @@ public:
   bool include_unpublished_topics = false;
   bool ignore_leaf_topics = false;
   bool start_paused = false;
+  bool use_sim_time = false;
 };
 
 }  // namespace rosbag2_transport
