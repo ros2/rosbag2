7b030fa5d848e41af57efe50dfd198eb85dce7ea
Michael Orlov <michael.orlov@apex.ai>
2022-05-16 13:09:13 -0700

    Fix for windows build
    Add redefinition for __PRETTY_FUNCTION__ to __FUNCSIG__ if it is not
    defined and not gcc compiler.
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>

--- rosbag2_transport/test/rosbag2_transport/test_play_services.cpp
+++ rosbag2_transport/test/rosbag2_transport/test_play_services.cpp
@@ -31,6 +31,10 @@
 
 #include "rosbag2_play_test_fixture.hpp"
 
+#if !defined(__PRETTY_FUNCTION__) && !defined(__GNUC__)
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#endif
+
 using namespace ::testing;  // NOLINT
 
 class PlaySrvsTest : public RosBag2PlayTestFixture
