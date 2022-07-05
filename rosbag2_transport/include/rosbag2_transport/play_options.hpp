2f15b242cbe74f17a7eb7de174b750c57bd34b52
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-07-05 23:40:31 +0200

    Added support for filtering topics via regular expressions (#1034)
    * Added support for filtering topics via regular expressions
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>
    
    * Added application function for evaluating regular expressions from within sqlite3
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>
    
    * Use nullptr for null pointers
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>
    
    * Added tests for topics-regex API
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>

--- rosbag2_transport/include/rosbag2_transport/play_options.hpp
+++ rosbag2_transport/include/rosbag2_transport/play_options.hpp
@@ -39,6 +39,11 @@ public:
   // If list is empty, the filter is ignored and all messages are played.
   std::vector<std::string> topics_to_filter = {};
 
+  // Regular expressions of topic names to whitelist when playing a bag.
+  // Only messages matching these specified topics will be played.
+  // If list is empty, the filter is ignored and all messages are played.
+  std::vector<std::string> topics_to_filter_regex = {};
+
   std::unordered_map<std::string, rclcpp::QoS> topic_qos_profile_overrides = {};
   bool loop = false;
   std::vector<std::string> topic_remapping_options = {};
