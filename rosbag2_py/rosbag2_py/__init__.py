c611a8010a0aaaba8bff47c42846aae2374fe81f
james-rms <james@foxglove.dev>
2022-11-10 10:23:27 +1100

    rosbag2_storage: expose default storage ID as method (#1146)
    * rosbag2_storage: expose default storage ID as method
    
    Signed-off-by: James Smith <james@foxglove.dev>

--- rosbag2_py/rosbag2_py/__init__.py
+++ rosbag2_py/rosbag2_py/__init__.py
@@ -40,6 +40,7 @@ with add_dll_directories_from_env('PATH'):
         get_registered_writers,
         get_registered_compressors,
         get_registered_serializers,
+        get_default_storage_id,
     )
     from rosbag2_py._info import (
         Info,
@@ -59,6 +60,7 @@ __all__ = [
     'bag_rewrite',
     'ConverterOptions',
     'FileInformation',
+    'get_default_storage_id',
     'get_registered_readers',
     'get_registered_writers',
     'get_registered_compressors',
