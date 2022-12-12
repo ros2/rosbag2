8f0ebb6381d149c61cb7b9c153ef7d346622f73c
James Smith <james@foxglove.dev>
2022-12-12 12:11:23 +1100

    rosbag2_transport: parametrize test_play_seek
    Signed-off-by: James Smith <james@foxglove.dev>

--- rosbag2_transport/test/rosbag2_transport/test_play_seek.cpp
+++ rosbag2_transport/test/rosbag2_transport/test_play_seek.cpp
@@ -25,6 +25,7 @@
 #include "rosbag2_cpp/writers/sequential_writer.hpp"
 #include "rosbag2_play_test_fixture.hpp"
 #include "rosbag2_storage/storage_options.hpp"
+#include "rosbag2_test_common/tested_storage_ids.hpp"
 #include "test_msgs/message_fixtures.hpp"
 #include "test_msgs/msg/basic_types.hpp"
 
@@ -32,7 +33,9 @@ using namespace ::testing;  // NOLINT
 using namespace rosbag2_transport;  // NOLINT
 using namespace rosbag2_test_common;  // NOLINT
 
-class RosBag2PlaySeekTestFixture : public RosBag2PlayTestFixture
+class RosBag2PlaySeekTestFixture
+  : public RosBag2PlayTestFixture,
+  public WithParamInterface<std::string>
 {
 public:
   RosBag2PlaySeekTestFixture()
@@ -42,7 +45,7 @@ public:
       {"topic1", "test_msgs/BasicTypes", rmw_get_serialization_format(), ""}};
 
     const rcpputils::fs::path base{_SRC_RESOURCES_DIR_PATH};
-    const rcpputils::fs::path bag_path = base / "test_bag_for_seek";
+    const rcpputils::fs::path bag_path = base / GetParam() / "test_bag_for_seek";
 
     storage_options_ = rosbag2_storage::StorageOptions({bag_path.string(), "", 0, 0, 0});
     play_options_.read_ahead_queue_size = 2;
@@ -96,7 +99,7 @@ public:
   std::unique_ptr<rosbag2_cpp::Reader> reader_;
 };
 
-TEST_F(RosBag2PlaySeekTestFixture, seek_back_in_time) {
+TEST_P(RosBag2PlaySeekTestFixture, seek_back_in_time) {
   const size_t expected_number_of_messages = num_msgs_in_bag_ + 2;
   auto player = std::make_shared<MockPlayer>(std::move(reader_), storage_options_, play_options_);
 
@@ -143,7 +146,7 @@ TEST_F(RosBag2PlaySeekTestFixture, seek_back_in_time) {
   }
 }
 
-TEST_F(RosBag2PlaySeekTestFixture, seek_with_timestamp_later_than_in_last_message) {
+TEST_P(RosBag2PlaySeekTestFixture, seek_with_timestamp_later_than_in_last_message) {
   const size_t expected_number_of_messages = 0;
   auto player = std::make_shared<MockPlayer>(std::move(reader_), storage_options_, play_options_);
 
@@ -177,7 +180,7 @@ TEST_F(RosBag2PlaySeekTestFixture, seek_with_timestamp_later_than_in_last_messag
   ASSERT_THAT(replayed_topic1, SizeIs(expected_number_of_messages));
 }
 
-TEST_F(RosBag2PlaySeekTestFixture, seek_forward) {
+TEST_P(RosBag2PlaySeekTestFixture, seek_forward) {
   const size_t expected_number_of_messages = num_msgs_in_bag_ - 1;
   auto player = std::make_shared<MockPlayer>(std::move(reader_), storage_options_, play_options_);
 
@@ -215,7 +218,7 @@ TEST_F(RosBag2PlaySeekTestFixture, seek_forward) {
   }
 }
 
-TEST_F(RosBag2PlaySeekTestFixture, seek_back_in_time_from_the_end_of_the_bag) {
+TEST_P(RosBag2PlaySeekTestFixture, seek_back_in_time_from_the_end_of_the_bag) {
   const size_t expected_number_of_messages = num_msgs_in_bag_ + num_msgs_in_bag_ - 2;
   auto player = std::make_shared<MockPlayer>(std::move(reader_), storage_options_, play_options_);
 
@@ -261,7 +264,7 @@ TEST_F(RosBag2PlaySeekTestFixture, seek_back_in_time_from_the_end_of_the_bag) {
   }
 }
 
-TEST_F(RosBag2PlaySeekTestFixture, seek_forward_from_the_end_of_the_bag) {
+TEST_P(RosBag2PlaySeekTestFixture, seek_forward_from_the_end_of_the_bag) {
   const size_t expected_number_of_messages = num_msgs_in_bag_;
   auto player = std::make_shared<MockPlayer>(std::move(reader_), storage_options_, play_options_);
 
@@ -300,3 +303,9 @@ TEST_F(RosBag2PlaySeekTestFixture, seek_forward_from_the_end_of_the_bag) {
     EXPECT_EQ(replayed_topic1[i]->int32_value, static_cast<int32_t>(i + 1)) << "i=" << i;
   }
 }
+
+INSTANTIATE_TEST_SUITE_P(
+  ParametrizedPlaySeekTests,
+  RosBag2PlaySeekTestFixture,
+  ValuesIn(rosbag2_test_common::kTestedStorageIDs)
+);
