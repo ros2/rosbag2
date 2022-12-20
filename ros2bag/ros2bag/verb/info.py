c8db9fbb6753402623cb6f81e1510e71715a4bb3
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2022-12-20 16:16:48 +0900

    revert standard_reader_args
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>

--- ros2bag/ros2bag/verb/info.py
+++ ros2bag/ros2bag/verb/info.py
@@ -21,6 +21,7 @@ class InfoVerb(VerbExtension):
     """Print information about a bag to the screen."""
 
     def add_arguments(self, parser, cli_name):  # noqa: D102
+        add_standard_reader_args(parser)
         parser.add_argument(
             '-q', '--quiet', action='store_true',
             help='Only display topic names.'
