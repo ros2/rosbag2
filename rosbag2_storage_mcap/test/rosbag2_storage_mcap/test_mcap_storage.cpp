c745425df3eb12f0595b7ac30146d566179f9197
James Smith <james@foxglove.dev>
2023-04-13 13:48:55 +1000

    add topic information equality operator
    Signed-off-by: James Smith <james@foxglove.dev>

--- rosbag2_storage_mcap/test/rosbag2_storage_mcap/test_mcap_storage.cpp
+++ rosbag2_storage_mcap/test/rosbag2_storage_mcap/test_mcap_storage.cpp
@@ -30,6 +30,16 @@
 using namespace ::testing;  // NOLINT
 using TemporaryDirectoryFixture = rosbag2_test_common::TemporaryDirectoryFixture;
 
+namespace rosbag2_storage
+{
+
+bool operator==(const TopicInformation & lhs, const TopicInformation & rhs)
+{
+  return lhs.topic_metadata == rhs.topic_metadata && lhs.message_count == rhs.message_count;
+}
+
+}  // namespace rosbag2_storage
+
 TEST_F(TemporaryDirectoryFixture, can_write_and_read_basic_mcap_file)
 {
   auto uri = rcpputils::fs::path(temporary_dir_path_) / "bag";
