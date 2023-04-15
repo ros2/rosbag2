56e34873c55b7b8efee89f6a3657d2f6ab36b537
Michael Orlov <michael.orlov@apex.ai>
2023-04-15 13:40:02 -0700

    Move routines from recorder's destructor to RecorderImpl::stop()
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>

--- rosbag2_transport/src/rosbag2_transport/recorder.cpp
+++ rosbag2_transport/src/rosbag2_transport/recorder.cpp
@@ -182,12 +182,19 @@ RecorderImpl::RecorderImpl(
 RecorderImpl::~RecorderImpl()
 {
   keyboard_handler_->delete_key_press_callback(toggle_paused_key_callback_handle_);
+  stop();
+}
+
+
+void RecorderImpl::stop()
+{
   stop_discovery_ = true;
   if (discovery_future_.valid()) {
     discovery_future_.wait();
   }
-
+  paused_ = true;
   subscriptions_.clear();
+  writer_->close();  // Call writer->close() to finalize current bag file and write metadata
 
   {
     std::lock_guard<std::mutex> lock(event_publisher_thread_mutex_);
@@ -199,13 +206,9 @@ RecorderImpl::~RecorderImpl()
   }
 }
 
-void RecorderImpl::stop()
-{
-  writer_->close();
-}
-
 void RecorderImpl::record()
 {
+  paused_ = record_options_.start_paused;
   topic_qos_profile_overrides_ = record_options_.topic_qos_profile_overrides;
   if (record_options_.rmw_serialization_format.empty()) {
     throw std::runtime_error("No serialization format specified!");
