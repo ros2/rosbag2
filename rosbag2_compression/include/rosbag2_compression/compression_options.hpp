84bb8d079badacaa71347b7347ec8a7d40bfcd63
Chris Lalancette <clalancette@gmail.com>
2023-05-04 13:54:45 -0400

    Add in a missing cstdint include. (#1321)
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>
    (cherry picked from commit 65c889e1fa55dd85a148b27b8c27dadc73238e67)

--- rosbag2_compression/include/rosbag2_compression/compression_options.hpp
+++ rosbag2_compression/include/rosbag2_compression/compression_options.hpp
@@ -15,6 +15,7 @@
 #ifndef ROSBAG2_COMPRESSION__COMPRESSION_OPTIONS_HPP_
 #define ROSBAG2_COMPRESSION__COMPRESSION_OPTIONS_HPP_
 
+#include <cstdint>
 #include <string>
 
 #include "visibility_control.hpp"
