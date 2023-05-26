c8539c89d8c81d997a8453ac9eb640ead310b24c
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2023-05-26 11:41:54 -0700

    [foxy] Fix YAML_CPP_DLL warnings on Windows (#1353)
    * [foxy] Fix YAML_CPP_DLL warnings on Windows
    
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rosbag2_transport/src/rosbag2_transport/qos.hpp
+++ rosbag2_transport/src/rosbag2_transport/qos.hpp
@@ -23,7 +23,9 @@
 
 #ifdef _WIN32
 // This is necessary because of a bug in yaml-cpp's cmake
+#ifndef YAML_CPP_DLL
 #define YAML_CPP_DLL
+#endif
 // This is necessary because yaml-cpp does not always use dllimport/dllexport consistently
 # pragma warning(push)
 # pragma warning(disable:4251)
