33edccf8636e97e6c2a2637ff560afef3553d59c
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2023-06-12 16:55:12 -0700

    [iron] Add ROS_DISTRO metadata record to mcap file when opening for writing (#1371)
    * Iron: Add ROS_DISTRO metadata record to mcap file when opening for writing
    
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rosbag2_storage_mcap/src/mcap_storage.cpp
+++ rosbag2_storage_mcap/src/mcap_storage.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "rcpputils/env.hpp"
 #include "rcutils/logging_macros.h"
 #include "rosbag2_storage/metadata_io.hpp"
 #include "rosbag2_storage/ros_helper.hpp"
@@ -237,6 +238,7 @@ private:
   bool enqueued_message_is_already_read();
   bool message_indexes_present();
   void ensure_summary_read();
+  void ensure_rosdistro_metadata_added();
 
   std::optional<rosbag2_storage::storage_interfaces::IOFlag> opened_as_;
   std::string relative_path_;
@@ -259,6 +261,7 @@ private:
   std::unique_ptr<mcap::McapWriter> mcap_writer_;
 
   bool has_read_summary_ = false;
+  bool has_added_ros_distro_metadata_ = false;
   rcutils_time_point_value_t last_read_time_point_ = 0;
   std::optional<mcap::RecordOffset> last_read_message_offset_;
   std::optional<mcap::RecordOffset> last_enqueued_message_offset_;
@@ -363,6 +366,7 @@ void MCAPStorage::open_impl(const std::string & uri, const std::string & preset_
       if (!status.ok()) {
         throw std::runtime_error(status.message);
       }
+      ensure_rosdistro_metadata_added();
       break;
     }
   }
@@ -803,8 +807,23 @@ void MCAPStorage::update_metadata(const rosbag2_storage::BagMetadata & bag_metad
       "MCAP storage plugin does not support message compression, "
       "consider using chunk compression by setting `compression: 'Zstd'` in storage config");
   }
+  ensure_rosdistro_metadata_added();
 }
 #endif
+
+void MCAPStorage::ensure_rosdistro_metadata_added()
+{
+  if (!has_added_ros_distro_metadata_) {
+    mcap::Metadata metadata;
+    metadata.name = "rosbag2";
+    metadata.metadata = {{"ROS_DISTRO", rcpputils::get_env_var("ROS_DISTRO")}};
+    mcap::Status status = mcap_writer_->write(metadata);
+    if (!status.ok()) {
+      OnProblem(status);
+    }
+  }
+  has_added_ros_distro_metadata_ = true;
+}
 
 }  // namespace rosbag2_storage_plugins
 
