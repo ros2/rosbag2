7f467a926fca69082e32db456fdd052a8739925d
Patrick Roncagliolo <ronca.pat@gmail.com>
2023-07-17 09:57:36 +0200

    Move param declaration out of constructor

--- rosbag2_storage/include/rosbag2_storage/storage_options.hpp
+++ rosbag2_storage/include/rosbag2_storage/storage_options.hpp
@@ -16,9 +16,14 @@
 #define ROSBAG2_STORAGE__STORAGE_OPTIONS_HPP_
 
 #include <cstdint>
+#include <limits>
+#include <memory>
 #include <string>
 #include <unordered_map>
 
+#include "rcl_interfaces/msg/parameter_descriptor.hpp"
+#include "rcl_interfaces/msg/integer_range.hpp"
+#include "rcl_interfaces/msg/floating_point_range.hpp"
 #include "rosbag2_storage/visibility_control.hpp"
 #include "rosbag2_storage/yaml.hpp"
 
@@ -60,6 +65,97 @@ public:
   std::unordered_map<std::string, std::string> custom_data{};
 };
 
+namespace
+{
+rcl_interfaces::msg::ParameterDescriptor int_param_description(
+  std::string description, int64_t min,
+  int64_t max)
+{
+  rcl_interfaces::msg::ParameterDescriptor d{};
+  rcl_interfaces::msg::IntegerRange r{};
+  d.description = description;
+  r.from_value = min;
+  r.to_value = max;
+  d.integer_range.push_back(r);
+  return d;
+}
+
+rcl_interfaces::msg::ParameterDescriptor float_param_description(
+  std::string description, float min,
+  float max)
+{
+  rcl_interfaces::msg::ParameterDescriptor d{};
+  rcl_interfaces::msg::FloatingPointRange r{};
+  d.description = description;
+  r.from_value = min;
+  r.to_value = max;
+  d.floating_point_range.push_back(r);
+  return d;
+}
+}  // namespace
+
+void declare_storage_options_r_params(std::shared_ptr<rclcpp::Node> nh, Storageoptions & so)
+{
+  // TODO(roncapat): check if file can be read
+  so.uri = nh->declare_parameter<std::string>(
+    "storage.uri",
+    "");
+
+  so.storage_id = nh->declare_parameter<std::string>(
+    "storage.storage_id",
+    "");
+
+  // TODO(roncapat): check if file can be read
+  so.storage_config_uri = nh->declare_parameter<std::string>(
+    "storage.config_uri",
+    "");
+}
+
+void declare_storage_options_rw_params(std::shared_ptr<rclcpp::Node> nh, Storageoptions & so)
+{
+  auto desc_mbs = int_param_description(
+    "Max bagfile size (bytes)",
+    1,
+    std::numeric_limits<int64_t>::max());
+  auto max_bagfile_size_ = nh->declare_parameter<int64_t>(
+    "storage.max_bagfile_size",
+    0,
+    desc_mbs);
+  so.max_bagfile_size = static_cast<uint64_t>(max_bagfile_size_);
+
+  auto desc_mbd = int_param_description(
+    "Max bagfile duration (nanoseconds)",
+    1,
+    std::numeric_limits<int64_t>::max());
+  auto max_bagfile_duration_ = nh->declare_parameter<int64_t>(
+    "storage.max_bagfile_duration",
+    0,
+    desc_mbd);
+  so.max_bagfile_duration = static_cast<uint64_t>(max_bagfile_duration_);
+
+  auto desc_mcs = int_param_description(
+    "Max chache size (messages)",
+    1,
+    std::numeric_limits<int64_t>::max());
+  auto max_cache_size_ = nh->declare_parameter<int64_t>(
+    "storage.max_cache_size",
+    0,
+    desc_mcs);
+  so.max_cache_size = static_cast<uint64_t>(max_cache_size_);
+
+  so.storage_preset_profile = nh->declare_parameter<std::string>(
+    "storage.preset_profile",
+    "");
+
+  so.snapshot_mode = nh->declare_parameter<bool>(
+    "storage.snapshot_mode",
+    false);
+
+  // FIXME(roncapat)
+  // std::unordered_map<std::string, std::string> custom_data{};
+  declare_storage_options_r_params(nh, so);
+}
+
 }  // namespace rosbag2_storage
 
 namespace YAML
@@ -67,8 +163,8 @@ namespace YAML
 template<>
 struct ROSBAG2_STORAGE_PUBLIC convert<rosbag2_storage::StorageOptions>
 {
-  static Node encode(const rosbag2_storage::StorageOptions & storage_options);
-  static bool decode(const Node & node, rosbag2_storage::StorageOptions & storage_options);
+  static Node encode(const rosbag2_storage::StorageOptions & so);
+  static bool decode(const Node & node, rosbag2_storage::StorageOptions & so);
 };
 }  // namespace YAML
 
--- rosbag2_transport/include/rosbag2_transport/play_options.hpp
+++ rosbag2_transport/include/rosbag2_transport/play_options.hpp
@@ -16,10 +16,15 @@
 #define ROSBAG2_TRANSPORT__PLAY_OPTIONS_HPP_
 
 #include <cstddef>
+#include <limits>
+#include <memory>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
+#include "rcl_interfaces/msg/parameter_descriptor.hpp"
+#include "rcl_interfaces/msg/integer_range.hpp"
+#include "rcl_interfaces/msg/floating_point_range.hpp"
 #include "keyboard_handler/keyboard_handler.hpp"
 #include "rclcpp/duration.hpp"
 #include "rclcpp/time.hpp"
@@ -100,6 +105,133 @@ public:
   // Disable to publish as loaned message
   bool disable_loan_message = false;
 };
+
+namespace
+{
+rcl_interfaces::msg::ParameterDescriptor int_param_description(
+  std::string description, int64_t min,
+  int64_t max)
+{
+  rcl_interfaces::msg::ParameterDescriptor d{};
+  rcl_interfaces::msg::IntegerRange r{};
+  d.description = description;
+  r.from_value = min;
+  r.to_value = max;
+  d.integer_range.push_back(r);
+  return d;
+}
+
+rcl_interfaces::msg::ParameterDescriptor float_param_description(
+  std::string description, float min,
+  float max)
+{
+  rcl_interfaces::msg::ParameterDescriptor d{};
+  rcl_interfaces::msg::FloatingPointRange r{};
+  d.description = description;
+  r.from_value = min;
+  r.to_value = max;
+  d.floating_point_range.push_back(r);
+  return d;
+}
+}  // namespace
+
+void declare_play_options_rw_params(std::shared_ptr<rclcpp::Node> nh, PlayOptions & po)
+{
+  static const std::vector<std::string> empty_str_list;
+
+  auto desc_raqs = int_param_description(
+    "Read ahead queue size (messages)",
+    1,
+    std::numeric_limits<int64_t>::max());
+  auto read_ahead_queue_size_ = nh->declare_parameter<int64_t>(
+    "play.read_ahead_queue_size",
+    1000,
+    desc_raqs);
+  po.read_ahead_queue_size = static_cast<uint64_t>(read_ahead_queue_size_);
+
+  po.node_prefix = nh->declare_parameter<std::string>(
+    "play.node_prefix",
+    "");
+
+  auto desc_rate = float_param_description(
+    "Playback rate (hz)",
+    0.000001,
+    std::numeric_limits<float>::max());
+  po.rate = nh->declare_parameter<float>(
+    "play.rate",
+    1.0,
+    desc_rate);
+
+  po.topics_to_filter = nh->declare_parameter<std::vector<std::string>>(
+    "play.topics_to_filter",
+    empty_str_list);
+
+  po.topics_regex_to_filter = nh->declare_parameter<std::string>(
+    "play.topics_regex_to_filter",
+    "");
+
+  po.topics_regex_to_exclude = nh->declare_parameter<std::string>(
+    "play.topics_regex_to_exclude",
+    "");
+
+  // TODO(roncapat)
+  // std::unordered_map<std::string, rclcpp::QoS> topic_qos_profile_overrides = {};
+
+  po.loop = nh->declare_parameter<bool>(
+    "play.loop",
+    false);
+
+  // TODO(roncapat): but I think it's worth to use classic CLI/launchfile remap instead
+  // po.topic_remapping_options =
+  //    nh->declare_parameter<std::vector<std::string>>("play.topic_remapping_options", empty_str_list);
+
+  po.clock_publish_frequency = nh->declare_parameter<double>(
+    "play.clock_publish_frequency",
+    0.0);
+
+  po.clock_publish_on_topic_publish = nh->declare_parameter<bool>(
+    "play.clock_publish_on_topic_publish",
+    false);
+
+  po.clock_trigger_topics = nh->declare_parameter<std::vector<std::string>>(
+    "play.clock_trigger_topics",
+    empty_str_list);
+
+  auto playback_duration_ = nh->declare_parameter<double>(
+    "play.playback_duration",
+    -1);
+  po.playback_duration = rclcpp::Duration::from_seconds(playback_duration_);
+
+  po.playback_until_timestamp = nh->declare_parameter<int64_t>(
+    "play.playback_until_timestamp",
+    -1);
+
+  po.start_paused = nh->declare_parameter<bool>(
+    "play.start_paused",
+    false);
+
+  po.start_offset = nh->declare_parameter<int64_t>(
+    "play.start_offset",
+    0);
+
+  po.disable_keyboard_controls = nh->declare_parameter<bool>(
+    "play.disable_keyboard_controls",
+    false);
+
+  // TODO(roncapat): but these are never set in ros2 bag play verb. May we skip also here?
+  // KeyboardHandler::KeyCode pause_resume_toggle_key = KeyboardHandler::KeyCode::SPACE;
+  // KeyboardHandler::KeyCode play_next_key = KeyboardHandler::KeyCode::CURSOR_RIGHT;
+  // KeyboardHandler::KeyCode increase_rate_key = KeyboardHandler::KeyCode::CURSOR_UP;
+  // KeyboardHandler::KeyCode decrease_rate_key = KeyboardHandler::KeyCode::CURSOR_DOWN;
+
+  po.wait_acked_timeout = nh->declare_parameter<int64_t>(
+    "play.wait_acked_timeout",
+    -1);
+
+  po.disable_loan_message = nh->declare_parameter<bool>(
+    "play.disable_loan_message",
+    false);
+}
 
 }  // namespace rosbag2_transport
 
