b4939876899b5f1fe4f09e110bc95e2dcabb3953
Michael Orlov <michael.orlov@apex.ai>
2023-11-20 20:33:30 -0800

    Cleanup in functions which are getting values from node parameters
    - Address issues in duration and integer parameters conversion
    - Introduce `declare_integer_node_params(..)` and
    `get_duration_from_node_param(..)` helper functions
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>

--- rosbag2_transport/src/rosbag2_transport/config_options_from_node_params.cpp
+++ rosbag2_transport/src/rosbag2_transport/config_options_from_node_params.cpp
@@ -50,35 +50,60 @@ rcl_interfaces::msg::ParameterDescriptor float_param_description(
   d.floating_point_range.push_back(r);
   return d;
 }
-}  // namespace param_utils
 
+template<typename T>
+typename std::enable_if<std::numeric_limits<T>::is_integer, T>::type
+declare_integer_node_params(
+  rclcpp::Node & node,
+  const std::string & description,
+  int64_t min = std::numeric_limits<int64_t>::min(),
+  int64_t max = std::numeric_limits<int64_t>::max(),
+  T default_value = 0)
+{
+  auto param_desc = param_utils::int_param_description(description, min, max);
+  return static_cast<T>(node.declare_parameter<int64_t>(description, default_value, param_desc));
+}
+
+rclcpp::Duration get_duration_from_node_param(
+  rclcpp::Node & node,
+  const std::string & node_name,
+  int32_t default_value_sec = 0,
+  int64_t default_value_nsec = 0)
+{
+  constexpr uint32_t max_limits = std::numeric_limits<uint32_t>::max();
+  constexpr int64_t min_limits = -1 * static_cast<int64_t>(max_limits) - 1;
+
+  const auto sec = declare_integer_node_params<int64_t>(
+    node, node_name + ".sec", min_limits, max_limits, default_value_sec);
+  const auto nsec = declare_integer_node_params<int64_t>(
+    node, node_name + ".nsec", min_limits, max_limits, default_value_nsec);
+
+  auto total_nanoseconds =
+    static_cast<int64_t>(std::abs(sec)) * (1000LL * 1000LL * 1000LL) + std::abs(nsec);
+  if (sec < 0 || nsec < 0) {
+    total_nanoseconds *= -1;
+  }
+  return rclcpp::Duration::from_nanoseconds(total_nanoseconds);
+}
+
+}  // namespace param_utils
 
 PlayOptions get_play_options_from_node_params(rclcpp::Node & node)
 {
   PlayOptions play_options{};
-  auto desc_raqs = param_utils::int_param_description(
-    "Read ahead queue size (messages)",
-    1,
-    std::numeric_limits<int64_t>::max());
-  auto read_ahead_queue_size = node.declare_parameter<int64_t>(
-    "read_ahead_queue_size",
-    1000,
-    desc_raqs);
-  play_options.read_ahead_queue_size = static_cast<uint64_t>(read_ahead_queue_size);
+  play_options.read_ahead_queue_size = param_utils::declare_integer_node_params<size_t>(
+    node, "read_ahead_queue_size", 1, std::numeric_limits<int64_t>::max(), 1000);
 
-  play_options.node_prefix = node.declare_parameter<std::string>(
-    "node_prefix",
-    "");
+  play_options.node_prefix = node.declare_parameter<std::string>("node_prefix", "");
 
   auto desc_rate = param_utils::float_param_description(
     "Playback rate (hz)",
     0.000001,
     std::numeric_limits<float>::max());
-  play_options.rate = node.declare_parameter<float>("rate", 1.0, desc_rate);
+  play_options.rate = static_cast<float>(node.declare_parameter<float>("rate", 1.0, desc_rate));
 
   play_options.topics_to_filter = node.declare_parameter<std::vector<std::string>>(
-    "topics_to_filter",
-    std::vector<std::string>());
+    "topics_to_filter", std::vector<std::string>());
 
   play_options.topics_regex_to_filter =
     node.declare_parameter<std::string>("topics_regex_to_filter", "");
@@ -125,39 +150,24 @@ PlayOptions get_play_options_from_node_params(rclcpp::Node & node)
   play_options.clock_trigger_topics = node.declare_parameter<std::vector<std::string>>(
     "clock_trigger_topics", std::vector<std::string>());
 
-  auto delay_sec = node.declare_parameter<int32_t>("delay.sec", 0.0);
-  auto delay_nsec = node.declare_parameter<int32_t>("delay.nsec", 0.0);
-  play_options.delay = rclcpp::Duration(delay_sec, delay_nsec);
+  play_options.delay = param_utils::get_duration_from_node_param(node, "delay", 0, 0);
 
-  auto playback_duration_sec = node.declare_parameter<int32_t>("playback_duration.sec", -1.0);
-  auto playback_duration_nsec = node.declare_parameter<int32_t>("playback_duration.nsec", 0.0);
-  play_options.playback_duration = rclcpp::Duration(playback_duration_sec, playback_duration_nsec);
+  play_options.playback_duration = param_utils::get_duration_from_node_param(
+    node, "playback_duration", -1, 0);
 
-  auto playback_until_timestamp_sec =
-    node.declare_parameter<int32_t>("playback_until_timestamp.sec", -1.0);
-
-  auto playback_until_timestamp_nsec =
-    node.declare_parameter<int32_t>("playback_until_timestamp.nsec", 0.0);
-
-  play_options.playback_until_timestamp =
-    rclcpp::Duration(playback_until_timestamp_sec, playback_until_timestamp_nsec).nanoseconds();
+  play_options.playback_until_timestamp = param_utils::get_duration_from_node_param(
+    node, "playback_until_timestamp", 0, -1).nanoseconds();
 
   play_options.start_paused = node.declare_parameter<bool>("start_paused", false);
 
-  auto start_offset_sec = node.declare_parameter<int32_t>("start_offset.sec", -1.0);
-  auto start_offset_nsec = node.declare_parameter<int32_t>("start_offset.nsec", 0.0);
-  play_options.start_offset = rclcpp::Duration(start_offset_sec, start_offset_nsec).nanoseconds();
+  play_options.start_offset = param_utils::get_duration_from_node_param(
+    node, "start_offset", 0, 0).nanoseconds();
 
   play_options.disable_keyboard_controls =
     node.declare_parameter<bool>("disable_keyboard_controls", false);
 
-  auto wait_acked_timeout_sec =
-    node.declare_parameter<int32_t>("wait_acked_timeout.sec", -1.0);
-  auto wait_acked_timeout_nsec =
-    node.declare_parameter<int32_t>("wait_acked_timeout.nsec", 0.0);
-
-  play_options.wait_acked_timeout =
-    rclcpp::Duration(wait_acked_timeout_sec, wait_acked_timeout_nsec).nanoseconds();
+  play_options.wait_acked_timeout = param_utils::get_duration_from_node_param(
+    node, "wait_acked_timeout", 0, -1).nanoseconds();
 
   play_options.disable_loan_message =
     node.declare_parameter<bool>("disable_loan_message", false);
@@ -179,14 +189,9 @@ RecordOptions get_record_options_from_node_params(rclcpp::Node & node)
   record_options.rmw_serialization_format =
     node.declare_parameter<std::string>("rmw_serialization_format", "");
 
-  auto topic_polling_interval_sec = node.declare_parameter<int32_t>(
-    "topic_polling_interval.sec", 0.0);
-  auto topic_polling_interval_nsec = node.declare_parameter<int32_t>(
-    "topic_polling_interval.nsec", 1000000.0);
-
-  record_options.topic_polling_interval = rclcpp::Duration(
-    topic_polling_interval_sec,
-    topic_polling_interval_nsec).to_chrono<std::chrono::milliseconds>();
+  record_options.topic_polling_interval = param_utils::get_duration_from_node_param(
+    node, "topic_polling_interval",
+    0, 1000000).to_chrono<std::chrono::milliseconds>();
 
   record_options.regex = node.declare_parameter<std::string>("regex", "");
   record_options.exclude = node.declare_parameter<std::string>("exclude", "");
@@ -194,17 +199,12 @@ RecordOptions get_record_options_from_node_params(rclcpp::Node & node)
   record_options.compression_mode = node.declare_parameter<std::string>("compression_mode", "");
   record_options.compression_format = node.declare_parameter<std::string>("compression_format", "");
 
-  auto desc_cqs = param_utils::int_param_description(
-    "Compression queue size (messages)", 1, std::numeric_limits<int64_t>::max());
-  auto compression_queue_size = node.declare_parameter<int64_t>(
-    "compression_queue_size", 1, desc_cqs);
-  record_options.compression_queue_size = static_cast<uint64_t>(compression_queue_size);
+  record_options.compression_queue_size = param_utils::declare_integer_node_params<uint64_t>(
+    node, "compression_queue_size", 0, std::numeric_limits<int64_t>::max(), 1);
 
-  auto desc_cts = param_utils::int_param_description(
-    "Compression threads", 0, std::numeric_limits<int64_t>::max());
-  auto compression_threads = node.declare_parameter<int64_t>(
-    "compression_threads", 0, desc_cts);
-  record_options.compression_threads = static_cast<uint64_t>(compression_threads);
+  record_options.compression_threads = param_utils::declare_integer_node_params<uint64_t>(
+    node, "compression_threads", 0, std::numeric_limits<int64_t>::max(),
+    record_options.compression_threads);
 
   std::string qos_profile_overrides_path =
     node.declare_parameter<std::string>("qos_profile_overrides_path", "");
@@ -237,7 +237,6 @@ RecordOptions get_record_options_from_node_params(rclcpp::Node & node)
 
   record_options.use_sim_time = node.get_parameter("use_sim_time").get_value<bool>();
 
-
   if (record_options.use_sim_time && record_options.is_discovery_disabled) {
     throw std::invalid_argument(
             "'use_sim_time' and 'is_discovery_disabled' both set, but are incompatible settings. "
@@ -258,35 +257,17 @@ get_storage_options_from_node_params(rclcpp::Node & node)
   storage_options.storage_config_uri =
     node.declare_parameter<std::string>("storage_config_uri", "");
 
-  auto desc_mbs = param_utils::int_param_description(
-    "Max bagfile size (bytes)",
-    0,
-    std::numeric_limits<int64_t>::max());
-  auto max_bagfile_size = node.declare_parameter<int64_t>(
-    "max_bagfile_size",
-    0,
-    desc_mbs);
-  storage_options.max_bagfile_size = static_cast<uint64_t>(max_bagfile_size);
+  storage_options.max_bagfile_size = param_utils::declare_integer_node_params<uint64_t>(
+    node, "max_bagfile_size", 0,
+    std::numeric_limits<int64_t>::max(), storage_options.max_bagfile_size);
 
-  auto desc_mbd = param_utils::int_param_description(
-    "Max bagfile duration (nanoseconds)",
-    0,
-    std::numeric_limits<int64_t>::max());
-  auto max_bagfile_duration = node.declare_parameter<int64_t>(
-    "max_bagfile_duration",
-    0,
-    desc_mbd);
-  storage_options.max_bagfile_duration = static_cast<uint64_t>(max_bagfile_duration);
+  storage_options.max_bagfile_duration = param_utils::declare_integer_node_params<uint64_t>(
+    node, "max_bagfile_duration", 0,
+    std::numeric_limits<int64_t>::max(), storage_options.max_bagfile_duration);
 
-  auto desc_mcs = param_utils::int_param_description(
-    "Max cache size (bytes per each buffer for double-buffer cache)",
-    1,
-    std::numeric_limits<int64_t>::max());
-  auto max_cache_size = node.declare_parameter<int64_t>(
-    "max_cache_size",
-    100 * 1024 * 1024,
-    desc_mcs);
-  storage_options.max_cache_size = static_cast<uint64_t>(max_cache_size);
+  storage_options.max_cache_size = param_utils::declare_integer_node_params<uint64_t>(
+    node, "max_cache_size", 0,
+    std::numeric_limits<int64_t>::max(), 100 * 1024 * 1024);
 
   storage_options.storage_preset_profile =
     node.declare_parameter<std::string>("storage_preset_profile", "");
@@ -309,8 +290,13 @@ get_storage_options_from_node_params(rclcpp::Node & node)
     storage_options.custom_data[key_string] = value_string;
   }
 
-  storage_options.start_time_ns = node.declare_parameter<int64_t>("start_time_ns", -1);
-  storage_options.end_time_ns = node.declare_parameter<int64_t>("end_time_ns", -1);
+  storage_options.start_time_ns = param_utils::declare_integer_node_params<int64_t>(
+    node, "start_time_ns", std::numeric_limits<int64_t>::min(),
+    std::numeric_limits<int64_t>::max(), storage_options.start_time_ns);
+
+  storage_options.end_time_ns = param_utils::declare_integer_node_params<int64_t>(
+    node, "end_time_ns", std::numeric_limits<int64_t>::min(),
+    std::numeric_limits<int64_t>::max(), storage_options.end_time_ns);
 
   return storage_options;
 }
