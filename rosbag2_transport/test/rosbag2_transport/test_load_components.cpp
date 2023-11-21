e588657e1f24646ce1dd78f89303f11be4b1b26e
Patrick Roncagliolo <ronca.pat@gmail.com>
2023-11-21 12:48:17 +0100

    Move component manager in fixture
    Signed-off-by: Patrick Roncagliolo <ronca.pat@gmail.com>

--- rosbag2_transport/test/rosbag2_transport/test_load_components.cpp
+++ rosbag2_transport/test/rosbag2_transport/test_load_components.cpp
@@ -29,84 +29,88 @@ using namespace std::chrono_literals;
 class TestComponentManager : public ::testing::Test
 {
 protected:
-  static void SetUpTestCase()
+  std::shared_ptr<rclcpp::Node> node;
+  std::shared_ptr<rclcpp::executors::SingleThreadedExecutor> exec;
+  std::shared_ptr<rclcpp::Client<composition_interfaces::srv::LoadNode>> composition_client;
+  std::shared_ptr<rclcpp_components::ComponentManager> manager;
+  void SetUp() override
   {
     rclcpp::init(0, nullptr);
+    exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+    node = rclcpp::Node::make_shared("test_component_manager");
+    using ComponentManagerIsolated =
+      rclcpp_components::ComponentManagerIsolated<rclcpp::executors::SingleThreadedExecutor>;
+    manager = std::make_shared<ComponentManagerIsolated>(exec);
+
+    exec->add_node(manager);
+    exec->add_node(node);
+
+    composition_client = node->create_client<composition_interfaces::srv::LoadNode>(
+      "/ComponentManager/_container/load_node");
+
+    if (!composition_client->wait_for_service(20s)) {
+      ASSERT_TRUE(false) << "service not available after waiting";
+    }
+  }
+  void TearDown() override{
+    rclcpp::shutdown();
   }
 };
 
-TEST_F(TestComponentManager, test_load_components)
+TEST_F(TestComponentManager, test_load_play_component)
 {
-  auto exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-  auto node = rclcpp::Node::make_shared("test_component_manager");
-  std::shared_ptr<rclcpp_components::ComponentManager> manager;
-  using ComponentManagerIsolated =
-    rclcpp_components::ComponentManagerIsolated<rclcpp::executors::SingleThreadedExecutor>;
-  manager = std::make_shared<ComponentManagerIsolated>(exec);
-
-  exec->add_node(manager);
-  exec->add_node(node);
-
-  auto composition_client = node->create_client<composition_interfaces::srv::LoadNode>(
-    "/ComponentManager/_container/load_node");
-
-  if (!composition_client->wait_for_service(20s)) {
-    ASSERT_TRUE(false) << "service not available after waiting";
+  std::string path{_SRC_RESOURCES_DIR_PATH "/player_node_params.yaml"};
+  auto pm = rclcpp::parameter_map_from_yaml_file(path);
+  auto pl = rclcpp::parameters_from_map(pm, "/player_params_node");
+  auto request = std::make_shared<composition_interfaces::srv::LoadNode::Request>();
+  request->package_name = "rosbag2_transport";
+  request->plugin_name = "rosbag2_transport::Player";
+  for (auto p : pl) {
+    request->parameters.push_back(p.to_parameter_msg());
   }
 
-  {
-    std::string path{_SRC_RESOURCES_DIR_PATH "/player_node_params.yaml"};
-    auto pm = rclcpp::parameter_map_from_yaml_file(path);
-    auto pl = rclcpp::parameters_from_map(pm, "/player_params_node");
-    auto request = std::make_shared<composition_interfaces::srv::LoadNode::Request>();
-    request->package_name = "rosbag2_transport";
-    request->plugin_name = "rosbag2_transport::Player";
-    for (auto p : pl) {
-      request->parameters.push_back(p.to_parameter_msg());
-    }
+  rclcpp::Parameter qos_profile_overrides_path("qos_profile_overrides_path",
+    rclcpp::ParameterValue(_SRC_RESOURCES_DIR_PATH "/qos_profile_overrides.yaml"));
 
-    rclcpp::Parameter qos_profile_overrides_path("qos_profile_overrides_path",
-      rclcpp::ParameterValue(_SRC_RESOURCES_DIR_PATH "/qos_profile_overrides.yaml"));
+  rclcpp::Parameter uri("uri",
+    rclcpp::ParameterValue(_SRC_RESOURCES_DIR_PATH "//sqlite3/test_bag_for_seek"));
 
-    rclcpp::Parameter uri("uri",
-      rclcpp::ParameterValue(_SRC_RESOURCES_DIR_PATH "//sqlite3/test_bag_for_seek"));
+  request->parameters.push_back(qos_profile_overrides_path.to_parameter_msg());
+  request->parameters.push_back(uri.to_parameter_msg());
 
-    request->parameters.push_back(qos_profile_overrides_path.to_parameter_msg());
-    request->parameters.push_back(uri.to_parameter_msg());
+  auto future = composition_client->async_send_request(request);
+  auto ret = exec->spin_until_future_complete(future, 5s);  // Wait for the result.
+  auto result = future.get();
+  EXPECT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
+  EXPECT_EQ(result->success, true);
+  EXPECT_EQ(result->error_message, "");
+  EXPECT_EQ(result->full_node_name, "/rosbag2_player");
+  EXPECT_EQ(result->unique_id, 1u);
+}
 
-    auto future = composition_client->async_send_request(request);
-    auto ret = exec->spin_until_future_complete(future, 5s);  // Wait for the result.
-    auto result = future.get();
-    EXPECT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
-    EXPECT_EQ(result->success, true);
-    EXPECT_EQ(result->error_message, "");
-    EXPECT_EQ(result->full_node_name, "/rosbag2_player");
-    EXPECT_EQ(result->unique_id, 1u);
+TEST_F(TestComponentManager, test_load_record_component)
+{
+  std::string path{_SRC_RESOURCES_DIR_PATH "/recorder_node_params.yaml"};
+  auto pm = rclcpp::parameter_map_from_yaml_file(path);
+  auto pl = rclcpp::parameters_from_map(pm, "/recorder_params_node");
+  auto request = std::make_shared<composition_interfaces::srv::LoadNode::Request>();
+  request->package_name = "rosbag2_transport";
+  request->plugin_name = "rosbag2_transport::Recorder";
+  for (auto p : pl) {
+    request->parameters.push_back(p.to_parameter_msg());
   }
 
-  {
-    std::string path{_SRC_RESOURCES_DIR_PATH "/recorder_node_params.yaml"};
-    auto pm = rclcpp::parameter_map_from_yaml_file(path);
-    auto pl = rclcpp::parameters_from_map(pm, "/recorder_params_node");
-    auto request = std::make_shared<composition_interfaces::srv::LoadNode::Request>();
-    request->package_name = "rosbag2_transport";
-    request->plugin_name = "rosbag2_transport::Recorder";
-    for (auto p : pl) {
-      request->parameters.push_back(p.to_parameter_msg());
-    }
+  rclcpp::Parameter qos_profile_overrides_path("qos_profile_overrides_path",
+    rclcpp::ParameterValue(_SRC_RESOURCES_DIR_PATH "/qos_profile_overrides.yaml"));
 
-    rclcpp::Parameter qos_profile_overrides_path("qos_profile_overrides_path",
-      rclcpp::ParameterValue(_SRC_RESOURCES_DIR_PATH "/qos_profile_overrides.yaml"));
+  request->parameters.push_back(qos_profile_overrides_path.to_parameter_msg());
 
-    request->parameters.push_back(qos_profile_overrides_path.to_parameter_msg());
-
-    auto future = composition_client->async_send_request(request);
-    auto ret = exec->spin_until_future_complete(future, 5s);  // Wait for the result.
-    auto result = future.get();
-    EXPECT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
-    EXPECT_EQ(result->success, true);
-    EXPECT_EQ(result->error_message, "");
-    EXPECT_EQ(result->full_node_name, "/rosbag2_recorder");
-    EXPECT_EQ(result->unique_id, 2u);
-  }
+  auto future = composition_client->async_send_request(request);
+  auto ret = exec->spin_until_future_complete(future, 5s);  // Wait for the result.
+  auto result = future.get();
+  EXPECT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
+  EXPECT_EQ(result->success, true);
+  EXPECT_EQ(result->error_message, "");
+  EXPECT_EQ(result->full_node_name, "/rosbag2_recorder");
+  EXPECT_EQ(result->unique_id, 1u);
 }
