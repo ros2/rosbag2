c4c2c3097069faa016dc0385472796b2a5e44b10
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-01-12 09:44:30 +0000

    Add proper message for --start-paused
    Signed-off-by: Christoph Froehlich <christoph.froehlich@ait.ac.at>

--- rosbag2_transport/src/rosbag2_transport/recorder.cpp
+++ rosbag2_transport/src/rosbag2_transport/recorder.cpp
@@ -343,7 +343,13 @@ void RecorderImpl::record()
     discovery_future_ =
       std::async(std::launch::async, std::bind(&RecorderImpl::topics_discovery, this));
   }
-  RCLCPP_INFO(node->get_logger(), "Recording...");
+  if (record_options_.start_paused) {
+    RCLCPP_INFO(
+      node->get_logger(), "Wait for recording: Press %s to start.",
+      enum_key_code_to_str(Recorder::kPauseResumeToggleKey).c_str());
+  } else {
+    RCLCPP_INFO(node->get_logger(), "Recording...");
+  }
 }
 
 void RecorderImpl::event_publisher_thread_main()
