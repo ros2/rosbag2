eb4e65ab6f157053a57ef4a8cc1e2442e47b0284
Roman <rsokolkov@gmail.com>
2024-02-02 21:29:02 +0300

    Remove rcpputils::fs dependencies from rosbag2_storages (#1558)
    * Remove rcpputils::fs dependencies from rosbag2_storages
    
    Original author: Kenta Yonekura <yoneken@ieee.org>
    
    Signed-off-by: Roman Sokolkov <rsokolkov@gmail.com>
    
    * Use path::generic_string() for Windows compatibility
    
    Co-authored with Kenta Yonekura <yoneken@ieee.org>
    
    Signed-off-by: Roman Sokolkov <rsokolkov@gmail.com>
    
    * Fix linter errors in osbag2_storage_sqlite3
    
    Signed-off-by: Roman Sokolkov <rsokolkov@gmail.com>
    
    ---------
    
    Signed-off-by: Roman Sokolkov <rsokolkov@gmail.com>
    Co-authored-by: Kenta Yonekura <yoneken@ieee.org>
    (cherry picked from commit edda3769ffe0986d75cabe4464597a516f7aa969)
    
    # Conflicts:
    #	rosbag2_storage_mcap/test/rosbag2_storage_mcap/test_mcap_storage.cpp
    #	rosbag2_storage_sqlite3/src/rosbag2_storage_sqlite3/sqlite_storage.cpp
    #	rosbag2_storage_sqlite3/test/rosbag2_storage_sqlite3/test_sqlite_storage.cpp

--- rosbag2_storage_sqlite3/src/rosbag2_storage_sqlite3/sqlite_storage.cpp
+++ rosbag2_storage_sqlite3/src/rosbag2_storage_sqlite3/sqlite_storage.cpp
@@ -19,8 +19,9 @@
 #include <atomic>
 #include <chrono>
 #include <cstring>
-#include <iostream>
+#include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <memory>
 #include <string>
 #include <unordered_map>
@@ -28,7 +29,11 @@
 #include <vector>
 
 #include "rcpputils/env.hpp"
+<<<<<<< HEAD
 #include "rcpputils/filesystem_helper.hpp"
+=======
+#include "rcpputils/scope_exit.hpp"
+>>>>>>> edda376 (Remove rcpputils::fs dependencies from rosbag2_storages (#1558))
 
 #include "rosbag2_storage/metadata_io.hpp"
 #include "rosbag2_storage/serialized_bag_message.hpp"
@@ -192,7 +197,7 @@ void SqliteStorage::open(
     relative_path_ = storage_options.uri + FILE_EXTENSION;
 
     // READ_WRITE requires the DB to not exist.
-    if (rcpputils::fs::path(relative_path_).exists()) {
+    if (std::filesystem::exists(std::filesystem::path(relative_path_))) {
       throw std::runtime_error(
               "Failed to create bag: File '" + relative_path_ + "' already exists!");
     }
@@ -200,7 +205,7 @@ void SqliteStorage::open(
     relative_path_ = storage_options.uri;
 
     // APPEND and READ_ONLY require the DB to exist
-    if (!rcpputils::fs::path(relative_path_).exists()) {
+    if (!std::filesystem::exists(std::filesystem::path(relative_path_))) {
       throw std::runtime_error(
               "Failed to read from bag: File '" + relative_path_ + "' does not exist!");
     }
@@ -401,9 +406,20 @@ void SqliteStorage::get_all_message_definitions(
 
 uint64_t SqliteStorage::get_bagfile_size() const
 {
+<<<<<<< HEAD
   const auto bag_path = rcpputils::fs::path{get_relative_file_path()};
 
   return bag_path.exists() ? bag_path.file_size() : 0u;
+=======
+  if (!database_ || !page_count_statement_) {
+    // Trying to call get_bagfile_size when SqliteStorage::open was not called or db
+    // failed to open. Fallback to the filesystem call.
+    const auto bag_path = std::filesystem::path{get_relative_file_path()};
+    return std::filesystem::exists(bag_path) ? std::filesystem::file_size(bag_path) : 0u;
+  } else {
+    return db_file_size_;
+  }
+>>>>>>> edda376 (Remove rcpputils::fs dependencies from rosbag2_storages (#1558))
 }
 
 void SqliteStorage::initialize()
