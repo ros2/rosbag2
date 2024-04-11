5ff8f942682868b299d684c5809f9efc25c83e44
Michael Orlov <michael.orlov@apex.ai>
2024-04-10 18:57:52 -0700

    Gracefully handle SIGINT and SIGTERM signals for play and burst CLI (#1557)
    * Gracefully handle SIGINT and SIGTERM signals for play and burst CLI
    
    - Intercept signals to call Player::stop() instead of relying on the
    rclcpp::shutdown() in default signal handlers.
    - Also added static Player::Cancel() method.
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>
    
    * Add test_play_cancel to the test_transport.py
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>
    
    * Add missing imports in test_transport.py
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>
    
    * Regenerate Python stub files (.pyi) after altering API
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>
    
    * Add call for original deferred signal handler for Player and Recorder
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>
    
    ---------
    
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>
    (cherry picked from commit 32bd5c3a9b5e4ff690682f04621d42f3151ba467)
    
    # Conflicts:
    #	rosbag2_py/rosbag2_py/_transport.pyi
    #	rosbag2_py/src/rosbag2_py/_transport.cpp
    #	rosbag2_py/test/test_transport.py
    #	rosbag2_transport/src/rosbag2_transport/player.cpp

--- rosbag2_py/test/test_transport.py
+++ rosbag2_py/test/test_transport.py
@@ -13,8 +13,13 @@
 # limitations under the License.
 
 import datetime
+<<<<<<< HEAD
 import os
 import sys
+=======
+from pathlib import Path
+import re
+>>>>>>> 32bd5c3a (Gracefully handle SIGINT and SIGTERM signals for play and burst CLI (#1557))
 import threading
 
 
@@ -31,6 +36,10 @@ if os.environ.get('ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL', None) is not None:
     #
     # For the fun RTTI ABI details, see https://whatofhow.wordpress.com/2015/03/17/odr-rtti-dso/.
     sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
+
+
+RESOURCES_PATH = Path(__file__).parent / 'resources'
+PLAYBACK_UNTIL_TIMESTAMP_REGEX_STRING = r'\[rosbag2_player]: Playback until timestamp: -1'
 
 
 def test_options_qos_conversion():
@@ -106,3 +115,47 @@ def test_record_cancel(tmp_path):
     assert wait_for(lambda: metadata_path.is_file() and db3_path.is_file(),
                     timeout=rclpy.duration.Duration(seconds=3))
     record_thread.join()
+<<<<<<< HEAD
+=======
+
+    metadata = metadata_io.read_metadata(str(bag_path))
+    assert len(metadata.relative_file_paths)
+    storage_path = bag_path / metadata.relative_file_paths[0]
+    assert wait_for(lambda: storage_path.is_file(),
+                    timeout=rclpy.duration.Duration(seconds=3))
+
+
+@pytest.mark.parametrize('storage_id', TESTED_STORAGE_IDS)
+def test_play_cancel(storage_id, capfd):
+    bag_path = str(RESOURCES_PATH / storage_id / 'talker')
+    storage_options, converter_options = get_rosbag_options(bag_path, storage_id)
+
+    player = rosbag2_py.Player()
+
+    play_options = rosbag2_py.PlayOptions()
+    play_options.loop = True
+    play_options.start_paused = True
+
+    player_thread = threading.Thread(
+        target=player.play,
+        args=(storage_options, play_options),
+        daemon=True)
+    player_thread.start()
+
+    def check_playback_start_output(cumulative_out, cumulative_err):
+        out, err = capfd.readouterr()
+        cumulative_err += err
+        cumulative_out += out
+        expected_string_regex = re.compile(PLAYBACK_UNTIL_TIMESTAMP_REGEX_STRING)
+        matches = expected_string_regex.search(cumulative_err)
+        return matches is not None
+
+    captured_out = ''
+    captured_err = ''
+    assert wait_for(lambda: check_playback_start_output(captured_out, captured_err),
+                    timeout=rclpy.duration.Duration(seconds=3))
+
+    player.cancel()
+    player_thread.join(3)
+    assert not player_thread.is_alive()
+>>>>>>> 32bd5c3a (Gracefully handle SIGINT and SIGTERM signals for play and burst CLI (#1557))
