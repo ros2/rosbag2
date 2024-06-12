e7e60057bf5c43f843af6856e09628cabd1010c1
Kosuke Takeuchi <kosuke.tnp@gmail.com>
2024-06-12 11:31:02 +0900

    Exclude recorded /clock topic when --clock option is specified (#1646)
    * Fix double clock publish
    
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>
    
    * add option help
    
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>
    
    ---------
    
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- ros2bag/ros2bag/verb/play.py
+++ ros2bag/ros2bag/verb/play.py
@@ -87,7 +87,8 @@ class PlayVerb(VerbExtension):
         clock_args_group.add_argument(
             '--clock', type=positive_float, nargs='?', const=40, default=0,
             help='Publish to /clock at a specific frequency in Hz, to act as a ROS Time Source. '
-                 'Value must be positive. Defaults to not publishing.')
+                 'Value must be positive. Defaults to not publishing.'
+                 'If specified, /clock topic in the bag file is excluded to publish.')
         clock_args_group.add_argument(
             '--clock-topics', type=str, default=[], nargs='+',
             help='List of topics separated by spaces that will trigger a /clock update '
@@ -212,8 +213,6 @@ class PlayVerb(VerbExtension):
 
         play_options.exclude_regex_to_filter = args.exclude_regex
 
-        play_options.exclude_topics_to_filter = args.exclude_topics if args.exclude_topics else []
-
         play_options.exclude_service_events_to_filter = \
             convert_service_to_service_event_topic(args.exclude_services)
 
@@ -221,6 +220,10 @@ class PlayVerb(VerbExtension):
         play_options.loop = args.loop
         play_options.topic_remapping_options = topic_remapping
         play_options.clock_publish_frequency = args.clock
+        exclude_topics = args.exclude_topics if args.exclude_topics else []
+        if play_options.clock_publish_frequency > 0:
+            exclude_topics.append('/clock')
+        play_options.exclude_topics_to_filter = exclude_topics
         if args.clock_topics_all or len(args.clock_topics) > 0:
             play_options.clock_publish_on_topic_publish = True
         play_options.clock_topics = args.clock_topics
