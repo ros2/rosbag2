ros2/rosbag2

commit 14ae5c7e9bb72ea0a274e5139142b0c4e79c81d0

[humble] Expose py Reader metadata, improve `rosbag2_py.BagMetadata` usability (backport #1082) (#1404)
* expose py Reader metadata, improve `rosbag2_py.BagMetadata` usability (#1082)

* expose metadata for reader, fix metadata fields to yield rclpy.Time and rclpy.Duration values instead of datetime
* Provide default constructor arguments for rosbag2_py.BagMetadata
* Add tests for rosbag2_py Storage struct constructors, fixing found issues
* Expose all StorageFilter fields in py

Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>
(cherry picked from commit c58d73ce1391d814a2331da5ea65b27bc2931765)

* Fix compilation errors

Signed-off-by: Jannik Jose <jannik.jose@hs-osnabrueck.de>

---------

Signed-off-by: Jannik Jose <jannik.jose@hs-osnabrueck.de>
Co-authored-by: Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
Co-authored-by: Jannik Jose <jannik.jose@hs-osnabrueck.de>

rosbag2_py/src/rosbag2_py/_reader.cpp
@@ -45,7 +45,7 @@ public:
 
   void open(
     rosbag2_storage::StorageOptions & storage_options,
-    rosbag2_cpp::ConverterOptions & converter_options)
+    rosbag2_cpp::ConverterOptions & converter_options = rosbag2_cpp::ConverterOptions())
   {
     reader_->open(storage_options, converter_options);
   }
@@ -71,6 +71,11 @@ public:
   std::vector<rosbag2_storage::TopicMetadata> get_all_topics_and_types()
   {
     return reader_->get_all_topics_and_types();
+  }
+
+  const rosbag2_storage::BagMetadata & get_metadata() const
+  {
+    return reader_->get_metadata();
   }
 
   void set_filter(const rosbag2_storage::StorageFilter & storage_filter)
@@ -108,44 +113,33 @@ std::unordered_set<std::string> get_registered_readers()
 
 }  // namespace rosbag2_py
 
+using PyReader = rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>;
+using PyCompressionReader = rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>;
+
 PYBIND11_MODULE(_reader, m) {
   m.doc() = "Python wrapper of the rosbag2_cpp reader API";
 
-  pybind11::class_<rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>>(
-    m, "SequentialReader")
+  pybind11::class_<PyReader>(m, "SequentialReader")
   .def(pybind11::init())
-  .def("open", &rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>::open)
-  .def("read_next", &rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>::read_next)
-  .def("has_next", &rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>::has_next)
-  .def(
-    "get_all_topics_and_types",
-    &rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>::get_all_topics_and_types)
-  .def("set_filter", &rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>::set_filter)
-  .def("reset_filter", &rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>::reset_filter)
-  .def("seek", &rosbag2_py::Reader<rosbag2_cpp::readers::SequentialReader>::seek);
+  .def("open", &PyReader::open)
+  .def("read_next", &PyReader::read_next)
+  .def("has_next", &PyReader::has_next)
+  .def("get_metadata", &PyReader::get_metadata)
+  .def("get_all_topics_and_types", &PyReader::get_all_topics_and_types)
+  .def("set_filter", &PyReader::set_filter)
+  .def("reset_filter", &PyReader::reset_filter)
+  .def("seek", &PyReader::seek);
 
-  pybind11::class_<rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>>(
-    m, "SequentialCompressionReader")
+  pybind11::class_<PyCompressionReader>(m, "SequentialCompressionReader")
   .def(pybind11::init())
-  .def("open", &rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>::open)
-  .def(
-    "read_next", &rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>::read_next)
-  .def("has_next", &rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>::has_next)
-  .def(
-    "get_all_topics_and_types",
-    &rosbag2_py::Reader<
-      rosbag2_compression::SequentialCompressionReader
-    >::get_all_topics_and_types)
-  .def(
-    "set_filter",
-    &rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>::set_filter)
-  .def(
-    "reset_filter",
-    &rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>::reset_filter)
-  .def(
-    "seek",
-    &rosbag2_py::Reader<rosbag2_compression::SequentialCompressionReader>::seek);
-
+  .def("open", &PyCompressionReader::open)
+  .def("read_next", &PyCompressionReader::read_next)
+  .def("has_next", &PyCompressionReader::has_next)
+  .def("get_metadata", &PyCompressionReader::get_metadata)
+  .def("get_all_topics_and_types", &PyCompressionReader::get_all_topics_and_types)
+  .def("set_filter", &PyCompressionReader::set_filter)
+  .def("reset_filter", &PyCompressionReader::reset_filter)
+  .def("seek", &PyCompressionReader::seek);
   m.def(
     "get_registered_readers",
     &rosbag2_py::get_registered_readers,

rosbag2_py/CMakeLists.txt
@@ -118,12 +118,13 @@ if(BUILD_TESTING)
     set(set_env_vars "${set_env_vars} ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL=True")
   endif()
 
-  # message(FATAL_ERROR "HELP I AM THE PATH ${CMAKE_CURRENT_SOURCE_DIR}")
-  ament_add_pytest_test(test_sequential_reader_py "test/test_sequential_reader.py"
+  ament_add_pytest_test(test_sequential_reader_py
+    "test/test_sequential_reader.py"
     APPEND_ENV "${append_env_vars}"
     ENV "${set_env_vars}"
   )
-  ament_add_pytest_test(test_sequential_reader_multiple_files_py "test/test_sequential_reader_multiple_files.py"
+  ament_add_pytest_test(test_sequential_reader_multiple_files_py
+    "test/test_sequential_reader_multiple_files.py"
     APPEND_ENV "${append_env_vars}"
     ENV "${set_env_vars}"
   )
@@ -144,6 +145,12 @@ if(BUILD_TESTING)
   )
   ament_add_pytest_test(test_convert_py
     "test/test_convert.py"
+    APPEND_ENV "${append_env_vars}"
+    ENV "${set_env_vars}"
+  )
+
+  ament_add_pytest_test(test_storage_py
+    "test/test_storage.py"
     APPEND_ENV "${append_env_vars}"
     ENV "${set_env_vars}"
   )

rosbag2_py/rosbag2_py/__init__.py
@@ -25,6 +25,7 @@ with add_dll_directories_from_env('PATH'):
     )
     from rosbag2_py._storage import (
         ConverterOptions,
+        FileInformation,
         StorageFilter,
         StorageOptions,
         TopicMetadata,
@@ -56,6 +57,7 @@ with add_dll_directories_from_env('PATH'):
 __all__ = [
     'bag_rewrite',
     'ConverterOptions',
+    'FileInformation',
     'get_default_storage_id',
     'get_registered_readers',
     'get_registered_writers',

rosbag2_py/src/rosbag2_py/_storage.cpp
@@ -25,14 +25,50 @@
 
 #include "./pybind11.hpp"
 
+namespace
+{
+
+using pybind11::literals::operator""_a;
+
+pybind11::object to_rclpy_duration(std::chrono::nanoseconds duration)
+{
+  pybind11::object Duration = pybind11::module::import("rclpy.duration").attr("Duration");
+  return Duration("nanoseconds"_a = duration.count());
+}
+
+std::chrono::nanoseconds from_rclpy_duration(const pybind11::object & duration)
+{
+  pybind11::int_ nanos = duration.attr("nanoseconds");
+  return std::chrono::nanoseconds(nanos);
+}
+
+template<typename T>
+pybind11::object to_rclpy_time(T time)
+{
+  pybind11::object Time = pybind11::module::import("rclpy.time").attr("Time");
+  return Time(
+    "nanoseconds"_a = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      time.time_since_epoch()).count());
+}
+
+std::chrono::time_point<std::chrono::high_resolution_clock> from_rclpy_time(
+  const pybind11::object & time)
+{
+  pybind11::int_ nanos = time.attr("nanoseconds");
+  return std::chrono::time_point<std::chrono::high_resolution_clock>(
+    std::chrono::nanoseconds(nanos));
+}
+
+}  // namespace
+
 PYBIND11_MODULE(_storage, m) {
   m.doc() = "Python wrapper of the rosbag2 utilities API";
 
   pybind11::class_<rosbag2_cpp::ConverterOptions>(m, "ConverterOptions")
   .def(
     pybind11::init<std::string, std::string>(),
-    pybind11::arg("input_serialization_format"),
-    pybind11::arg("output_serialization_format"))
+    pybind11::arg("input_serialization_format") = "",
+    pybind11::arg("output_serialization_format") = "")
   .def_readwrite(
     "input_serialization_format",
     &rosbag2_cpp::ConverterOptions::input_serialization_format)
@@ -76,7 +112,7 @@ PYBIND11_MODULE(_storage, m) {
   pybind11::class_<rosbag2_storage::StorageFilter>(m, "StorageFilter")
   .def(
     pybind11::init<std::vector<std::string>>(),
-    pybind11::arg("topics"))
+    pybind11::arg("topics") = std::vector<std::string>())
   .def_readwrite("topics", &rosbag2_storage::StorageFilter::topics);
 
   pybind11::class_<rosbag2_storage::TopicMetadata>(m, "TopicMetadata")
@@ -106,51 +142,108 @@ PYBIND11_MODULE(_storage, m) {
 
   pybind11::class_<rosbag2_storage::FileInformation>(m, "FileInformation")
   .def(
-    pybind11::init<std::string,
-    std::chrono::time_point<std::chrono::high_resolution_clock>,
-    std::chrono::nanoseconds,
-    size_t>(),
+    pybind11::init(
+      [](
+        std::string path,
+        pybind11::object starting_time,
+        pybind11::object duration,
+        size_t message_count)
+      {
+        return rosbag2_storage::FileInformation{
+          path,
+          from_rclpy_time(starting_time),
+          from_rclpy_duration(duration),
+          message_count
+        };
+      }),
     pybind11::arg("path"),
     pybind11::arg("starting_time"),
     pybind11::arg("duration"),
-    pybind11::arg("message_count"))
+    pybind11::arg("message_count")
+  )
   .def_readwrite("path", &rosbag2_storage::FileInformation::path)
-  .def_readwrite("starting_time", &rosbag2_storage::FileInformation::starting_time)
+  .def_property(
+    "starting_time",
+    [](const rosbag2_storage::FileInformation & self) {
+      return to_rclpy_time(self.starting_time);
+    },
+    [](rosbag2_storage::FileInformation & self, const pybind11::object & value) {
+      self.starting_time = from_rclpy_time(value);
+    })
+  .def_property(
+    "duration",
+    [](const rosbag2_storage::FileInformation & self) {
+      return to_rclpy_duration(self.duration);
+    },
+    [](rosbag2_storage::FileInformation & self, const pybind11::object & value) {
+      self.duration = from_rclpy_duration(value);
+    })
   .def_readwrite("duration", &rosbag2_storage::FileInformation::duration)
   .def_readwrite("message_count", &rosbag2_storage::FileInformation::message_count);
 
   pybind11::class_<rosbag2_storage::BagMetadata>(m, "BagMetadata")
   .def(
-    pybind11::init<
-      int,
-      uint64_t,
-      std::string,
-      std::vector<std::string>,
-      std::vector<rosbag2_storage::FileInformation>,
-      std::chrono::nanoseconds,
-      std::chrono::time_point<std::chrono::high_resolution_clock>,
-      uint64_t,
-      std::vector<rosbag2_storage::TopicInformation>,
-      std::string,
-      std::string>(),
-    pybind11::arg("version"),
-    pybind11::arg("bag_size"),
-    pybind11::arg("storage_identifier"),
-    pybind11::arg("relative_file_paths"),
-    pybind11::arg("files"),
-    pybind11::arg("duration"),
-    pybind11::arg("starting_time"),
-    pybind11::arg("message_count"),
-    pybind11::arg("topics_with_message_count"),
-    pybind11::arg("compression_format"),
-    pybind11::arg("compression_mode"))
+    pybind11::init(
+      [](
+        int version,
+        uint64_t bag_size,
+        std::string storage_identifier,
+        std::vector<std::string> relative_file_paths,
+        std::vector<rosbag2_storage::FileInformation> files,
+        pybind11::object duration,
+        pybind11::object starting_time,
+        uint64_t message_count,
+        std::vector<rosbag2_storage::TopicInformation> topics_with_message_count,
+        std::string compression_format,
+        std::string compression_mode)
+      {
+        return rosbag2_storage::BagMetadata{
+          version,
+          bag_size,
+          storage_identifier,
+          relative_file_paths,
+          files,
+          from_rclpy_duration(duration),
+          from_rclpy_time(starting_time),
+          message_count,
+          topics_with_message_count,
+          compression_format,
+          compression_mode
+        };
+      }),
+    pybind11::arg("version") = 6,
+    pybind11::arg("bag_size") = 0,
+    pybind11::arg("storage_identifier") = "",
+    pybind11::arg("relative_file_paths") = std::vector<std::string>(),
+    pybind11::arg("files") = std::vector<rosbag2_storage::FileInformation>(),
+    pybind11::arg("duration") = to_rclpy_duration(std::chrono::nanoseconds{0}),
+    pybind11::arg("starting_time") = to_rclpy_time(
+      std::chrono::time_point<std::chrono::high_resolution_clock>(std::chrono::nanoseconds{0})),
+    pybind11::arg("message_count") = 0,
+    pybind11::arg("topics_with_message_count") = std::vector<rosbag2_storage::TopicInformation>(),
+    pybind11::arg("compression_format") = "",
+    pybind11::arg("compression_mode") = "")
   .def_readwrite("version", &rosbag2_storage::BagMetadata::version)
   .def_readwrite("bag_size", &rosbag2_storage::BagMetadata::bag_size)
   .def_readwrite("storage_identifier", &rosbag2_storage::BagMetadata::storage_identifier)
   .def_readwrite("relative_file_paths", &rosbag2_storage::BagMetadata::relative_file_paths)
   .def_readwrite("files", &rosbag2_storage::BagMetadata::files)
-  .def_readwrite("duration", &rosbag2_storage::BagMetadata::duration)
-  .def_readwrite("starting_time", &rosbag2_storage::BagMetadata::starting_time)
+  .def_property(
+    "duration",
+    [](const rosbag2_storage::BagMetadata & self) {
+      return to_rclpy_duration(self.duration);
+    },
+    [](rosbag2_storage::BagMetadata & self, const pybind11::object & value) {
+      self.duration = from_rclpy_duration(value);
+    })
+  .def_property(
+    "starting_time",
+    [](const rosbag2_storage::BagMetadata & self) {
+      return to_rclpy_time(self.starting_time);
+    },
+    [](rosbag2_storage::BagMetadata & self, const pybind11::object & value) {
+      self.starting_time = from_rclpy_time(value);
+    })
   .def_readwrite("message_count", &rosbag2_storage::BagMetadata::message_count)
   .def_readwrite(
     "topics_with_message_count",

rosbag2_py/test/common.py
@@ -12,12 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import sys
 import time
 from typing import Callable
 
-from rclpy.clock import Clock, ClockType
-from rclpy.duration import Duration
-import rosbag2_py
+if os.environ.get('ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL', None) is not None:
+    # This is needed on Linux when compiling with clang/libc++.
+    # TL;DR This makes class_loader work when using a python extension compiled with libc++.
+    #
+    # For the fun RTTI ABI details, see https://whatofhow.wordpress.com/2015/03/17/odr-rtti-dso/.
+    sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
+
+from rclpy.clock import Clock, ClockType  # noqa
+from rclpy.duration import Duration  # noqa
+import rosbag2_py  # noqa
 
 
 def get_rosbag_options(path, serialization_format='cdr'):

rosbag2_py/test/test_convert.py
@@ -14,24 +14,15 @@
 
 import os
 from pathlib import Path
-import sys
 import tempfile
 import unittest
 
-if os.environ.get('ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL', None) is not None:
-    # This is needed on Linux when compiling with clang/libc++.
-    # TL;DR This makes class_loader work when using a python extension compiled with libc++.
-    #
-    # For the fun RTTI ABI details, see https://whatofhow.wordpress.com/2015/03/17/odr-rtti-dso/.
-    sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
-
-from common import get_rosbag_options  # noqa
-import rosbag2_py  # noqa
+import common  # noqa
 from rosbag2_py import (
     bag_rewrite,
     get_default_storage_id,
     StorageOptions,
-)  # noqa
+)
 
 RESOURCES_PATH = Path(os.environ['ROSBAG2_PY_TEST_RESOURCES_DIR'])
 

rosbag2_py/test/test_reindexer.py
@@ -22,17 +22,9 @@
 
 import os
 from pathlib import Path
-import sys
 
-if os.environ.get('ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL', None) is not None:
-    # This is needed on Linux when compiling with clang/libc++.
-    # TL;DR This makes class_loader work when using a python extension compiled with libc++.
-    #
-    # For the fun RTTI ABI details, see https://whatofhow.wordpress.com/2015/03/17/odr-rtti-dso/.
-    sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
-
-from common import get_rosbag_options  # noqa
-import rosbag2_py  # noqa
+from common import get_rosbag_options
+import rosbag2_py
 
 RESOURCES_PATH = Path(os.environ['ROSBAG2_PY_TEST_RESOURCES_DIR'])
 

rosbag2_py/test/test_sequential_reader.py
@@ -14,22 +14,15 @@
 
 import os
 from pathlib import Path
-import sys
+
+from common import get_rosbag_options
 
 from rcl_interfaces.msg import Log
 from rclpy.serialization import deserialize_message
+import rosbag2_py
 from rosidl_runtime_py.utilities import get_message
 from std_msgs.msg import String
 
-if os.environ.get('ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL', None) is not None:
-    # This is needed on Linux when compiling with clang/libc++.
-    # TL;DR This makes class_loader work when using a python extension compiled with libc++.
-    #
-    # For the fun RTTI ABI details, see https://whatofhow.wordpress.com/2015/03/17/odr-rtti-dso/.
-    sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
-
-from common import get_rosbag_options  # noqa
-import rosbag2_py  # noqa
 
 RESOURCES_PATH = Path(os.environ['ROSBAG2_PY_TEST_RESOURCES_DIR'])
 

commit 10ffd78fe354f698aaddfdc7e69705fba73f409d

Fixes clang compiler error introduced by #1404.
```
rosbag2_py/src/rosbag2_py/_reader.cpp:48:37: error: non-const lvalue reference to type 'rosbag2_cpp::ConverterOptions' cannot bind to a temporary of type 'rosbag2_cpp::ConverterOptions'
    rosbag2_cpp::ConverterOptions & converter_options = rosbag2_cpp::ConverterOptions())
                                    ^                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
```

A non-const lvalue reference cannot be bound to a temporary.

Signed-off-by: Halvor Platou <halvor.platou@zeabuz.com>

rosbag2_py/src/rosbag2_py/_reader.cpp
@@ -45,7 +45,7 @@ public:
 
   void open(
     rosbag2_storage::StorageOptions & storage_options,
-    rosbag2_cpp::ConverterOptions & converter_options = rosbag2_cpp::ConverterOptions())
+    const rosbag2_cpp::ConverterOptions & converter_options = rosbag2_cpp::ConverterOptions())
   {
     reader_->open(storage_options, converter_options);
   }