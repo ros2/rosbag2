d6fd40fc6211765809ee77a980fb5266e7bb3139
Chris Lalancette <clalancette@gmail.com>
2024-08-23 14:53:59 -0400

    Small cleanups to the rosbag2 tests. (#1792)
    * Small cleanups to the rosbag2 tests.
    
    1.  Rename "wait_for_srvice_to_be_ready" to "wait_for_service_to_be_ready".
    2.  Make some of the constants constexpr, so we no longer have to capture them.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>
    (cherry picked from commit 604cebcf11775151efa94f7c30ba1aea68e90c5c)

--- rosbag2_transport/test/rosbag2_transport/test_record_all_ignore_leaf_topics.cpp
+++ rosbag2_transport/test/rosbag2_transport/test_record_all_ignore_leaf_topics.cpp
@@ -68,7 +68,7 @@ TEST_F(RecordIntegrationTestFixture, published_messages_from_two_topics_ignore_l
   MockSequentialWriter & mock_writer =
     static_cast<MockSequentialWriter &>(writer.get_implementation_handle());
 
-  size_t expected_messages = 2;
+  constexpr size_t expected_messages = 2;
   auto ret = rosbag2_test_common::wait_until_shutdown(
     std::chrono::seconds(5),
     [&mock_writer, &expected_messages]() {
