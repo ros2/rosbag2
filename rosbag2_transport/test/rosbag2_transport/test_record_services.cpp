afdefb51555681c820b786ee78711f474da5a586
Sharmin Ramli <sharmin.ramli@enway.ai>
2024-10-17 18:01:18 +0200

    Add recorder services tests
    Signed-off-by: Sharmin Ramli <sharmin.ramli@enway.ai>

--- rosbag2_transport/test/rosbag2_transport/test_record_services.cpp
+++ rosbag2_transport/test/rosbag2_transport/test_record_services.cpp
@@ -21,11 +21,16 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include "rosbag2_interfaces/srv/is_discovery_stopped.hpp"
 #include "rosbag2_interfaces/srv/is_paused.hpp"
 #include "rosbag2_interfaces/srv/pause.hpp"
+#include "rosbag2_interfaces/srv/record.hpp"
 #include "rosbag2_interfaces/srv/resume.hpp"
 #include "rosbag2_interfaces/srv/snapshot.hpp"
 #include "rosbag2_interfaces/srv/split_bagfile.hpp"
+#include "rosbag2_interfaces/srv/start_discovery.hpp"
+#include "rosbag2_interfaces/srv/stop.hpp"
+#include "rosbag2_interfaces/srv/stop_discovery.hpp"
 #include "rosbag2_transport/recorder.hpp"
 
 #include "rosbag2_test_common/publication_manager.hpp"
@@ -39,11 +44,16 @@ using namespace ::testing;  // NOLINT
 class RecordSrvsTest : public RecordIntegrationTestFixture
 {
 public:
+  using IsDiscoveryStopped = rosbag2_interfaces::srv::IsDiscoveryStopped;
   using IsPaused = rosbag2_interfaces::srv::IsPaused;
   using Pause = rosbag2_interfaces::srv::Pause;
+  using Record = rosbag2_interfaces::srv::Record;
   using Resume = rosbag2_interfaces::srv::Resume;
   using Snapshot = rosbag2_interfaces::srv::Snapshot;
   using SplitBagfile = rosbag2_interfaces::srv::SplitBagfile;
+  using StartDiscovery = rosbag2_interfaces::srv::StartDiscovery;
+  using Stop = rosbag2_interfaces::srv::Stop;
+  using StopDiscovery = rosbag2_interfaces::srv::StopDiscovery;
 
   explicit RecordSrvsTest(bool snapshot_mode = false)
   : RecordIntegrationTestFixture(),
@@ -80,11 +90,16 @@ public:
     pub_manager.setup_publisher(test_topic_, string_message, 10);
 
     const std::string ns = "/" + recorder_name_;
+    cli_is_discovery_stopped_ = client_node_->create_client<IsDiscoveryStopped>(ns + "/is_discovery_stopped");
     cli_is_paused_ = client_node_->create_client<IsPaused>(ns + "/is_paused");
     cli_pause_ = client_node_->create_client<Pause>(ns + "/pause");
+    cli_record = client_node_->create_client<Record>(ns + "/record");
     cli_resume_ = client_node_->create_client<Resume>(ns + "/resume");
     cli_snapshot_ = client_node_->create_client<Snapshot>(ns + "/snapshot");
     cli_split_bagfile_ = client_node_->create_client<SplitBagfile>(ns + "/split_bagfile");
+    cli_start_discovery = client_node_->create_client<StartDiscovery>(ns + "/start_discovery");
+    cli_stop = client_node_->create_client<Stop>(ns + "/stop");
+    cli_stop_discovery = client_node_->create_client<StopDiscovery>(ns + "/stop_discovery");
     exec_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
 
     exec_->add_node(recorder_);
@@ -141,11 +156,16 @@ public:
 
   // Service clients
   rclcpp::Node::SharedPtr client_node_;
+  rclcpp::Client<IsDiscoveryStopped>::SharedPtr cli_is_discovery_stopped_;
   rclcpp::Client<IsPaused>::SharedPtr cli_is_paused_;
   rclcpp::Client<Pause>::SharedPtr cli_pause_;
+  rclcpp::Client<Record>::SharedPtr cli_record_;
   rclcpp::Client<Resume>::SharedPtr cli_resume_;
   rclcpp::Client<Snapshot>::SharedPtr cli_snapshot_;
   rclcpp::Client<SplitBagfile>::SharedPtr cli_split_bagfile_;
+  rclcpp::Client<StartDiscovery>::SharedPtr cli_start_discovery_;
+  rclcpp::Client<Stop>::SharedPtr cli_stop_;
+  rclcpp::Client<StopDiscovery>::SharedPtr cli_stop_discovery_;
 
   bool snapshot_mode_;
 };
@@ -225,4 +245,35 @@ TEST_F(RecordSrvsTest, pause_resume)
   EXPECT_FALSE(recorder_->is_paused());
   is_paused_response = successful_service_request<IsPaused>(cli_is_paused_);
   EXPECT_FALSE(is_paused_response->paused);
+}
+
+TEST_F(RecordSrvsTest, stop_start_discovery)
+{
+  EXPECT_FALSE(recorder_->is_discovery_stopped());
+  auto is_discovery_stopped_response = successful_service_request<IsDiscoveryStopped>(cli_is_discovery_stopped_);
+  EXPECT_FALSE(is_discovery_stopped_response->stopped);
+
+  successful_service_request<StopDiscovery>(cli_stop_discovery);
+  EXPECT_TRUE(recorder_->is_discovery_stopped());
+  is_discovery_stopped_response = successful_service_request<IsDiscoveryStopped>(cli_is_discovery_stopped_);
+  EXPECT_TRUE(is_discovery_stopped_response->stopped);
+
+  successful_service_request<StartDiscovery>(cli_start_discovery);
+  EXPECT_FALSE(recorder_->is_discovery_stopped());
+  auto is_discovery_stopped_response = successful_service_request<IsDiscoveryStopped>(cli_is_discovery_stopped_);
+  EXPECT_FALSE(is_discovery_stopped_response->stopped);
+}
+
+TEST_F(RecordSrvsTest, record_stop)
+{
+  auto & writer = recorder_->get_writer_handle();
+  auto & mock_writer = dynamic_cast<MockSequentialWriter &>(writer.get_implementation_handle());
+
+  EXPECT_FALSE(mock_writer.closed_was_called());
+
+  successful_service_request<Stop>(cli_stop);
+  EXPECT_TRUE(mock_writer.closed_was_called());
+
+  successful_service_request<Record>(cli_record);
+  EXPECT_FALSE(mock_writer.closed_was_called());
 }
