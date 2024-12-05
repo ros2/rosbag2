e67a376c3f02b8af3bf4cb86a170b8edf5525f12
Nicola Loi <nicolaloi@outlook.com>
2024-12-05 22:52:29 +0000

    review
    Signed-off-by: Nicola Loi <nicolaloi@outlook.com>

--- rosbag2_transport/src/rosbag2_transport/player.cpp
+++ rosbag2_transport/src/rosbag2_transport/player.cpp
@@ -529,13 +529,6 @@ bool PlayerImpl::play()
           std::chrono::nanoseconds delay_duration(delay.nanoseconds());
           std::this_thread::sleep_for(delay_duration);
         }
-        {
-          std::lock_guard<std::mutex> lk(reader_mutex_);
-          clock_->jump(starting_time_);
-        }
-        if (clock_publish_timer_ != nullptr) {
-          clock_publish_timer_->reset();
-        }
         do {
           {
             std::lock_guard<std::mutex> lk(reader_mutex_);
@@ -543,6 +536,9 @@ bool PlayerImpl::play()
               reader->seek(starting_time_);
             }
             clock_->jump(starting_time_);
+          }
+          if (clock_publish_timer_ != nullptr) {
+            clock_publish_timer_->reset();
           }
           load_storage_content_ = true;
           storage_loading_future_ = std::async(
