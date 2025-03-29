6bb2be1540966d304594b62768b2335a794620f3
Michael Orlov <morlovmr@gmail.com>
2025-03-29 00:46:04 -0700

    Add Doxygen comments to the action_utils.hpp
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>

--- rosbag2_cpp/include/rosbag2_cpp/action_utils.hpp
+++ rosbag2_cpp/include/rosbag2_cpp/action_utils.hpp
@@ -22,6 +22,7 @@
 
 namespace rosbag2_cpp
 {
+/// \brief Enum class for action introspection interface types.
 enum class ActionInterfaceType
 {
   SendGoalEvent,
@@ -32,31 +33,56 @@ enum class ActionInterfaceType
   Unknown
 };
 
+/// \brief Check if the topic belong to the action introspection interface.
+/// \param topic_name Topic name.
+/// \param topic_type Topic type.
+/// \return Boolean value indicating if the topic belongs to the action introspection interface.
 ROSBAG2_CPP_PUBLIC
 bool
 is_topic_belong_to_action(const std::string & topic_name, const std::string & topic_type);
 
+/// \brief Check if the topic belong to the action introspection interface.
+/// \param action_interface_type Action interface type given from the
+/// get_action_interface_type(const std::string & topic_name) function call.
+/// \param topic_type
+/// \return Boolean value indicating if the topic belongs to the action introspection interface.
 ROSBAG2_CPP_PUBLIC
 bool
 is_topic_belong_to_action(
   ActionInterfaceType action_interface_type, const std::string & topic_type);
 
-// Call this function after is_topic_belong_to_action() return true
+/// \brief Transform the action's introspection topic name to the short action name.
+/// \param topic_name Topic name.
+/// \note Call this function after is_topic_belong_to_action() returns true
+/// \return String with short action name.
+/// \example if topic_name is "/fibonacci/_action/send_goal/_service_event" the function will
+/// return the short action name "/fibonacci"
 ROSBAG2_CPP_PUBLIC
 std::string
 action_interface_name_to_action_name(const std::string & topic_name);
 
-// Get the action type from action interface type for display info
-// Call this function after is_topic_belong_to_action() return true
-// Note that cancel_goal event topic and status topic return ""
+/// \brief Extract the original action type from the action's introspection type.
+/// \note Call this function after is_topic_belong_to_action() returns true
+/// \param topic_type Topic type name.
+/// \return Original action type name aka "example_interfaces/action/Fibonacci" if action interface
+//// type is SendGoalEvent, GetResultEvent or Feedback message, if action interface type is
+/// CancelGoalEvent or Status, return empty string.
+/// \throw std::runtime_error for unknown action interface type
 ROSBAG2_CPP_PUBLIC
 std::string
-get_action_type_for_info(const std::string & interface_type);
+get_action_type_for_info(const std::string & topic_type);
 
+/// \brief Extract the action interface type from the action's introspection topic name.
+/// \param topic_name Topic name.
+/// \return ActionInterfaceType enum value.
 ROSBAG2_CPP_PUBLIC
 ActionInterfaceType
 get_action_interface_type(const std::string & topic_name);
 
+/// \brief Transform the action name returned from the `action_interface_name_to_action_name(..)
+/// to the list of the action introspection topics.
+/// \param action_name Action name returned from the `action_interface_name_to_action_name(..)`.
+/// \return Vector of action introspection topics corresponding to the action name.
 ROSBAG2_CPP_PUBLIC
 std::vector<std::string>
 action_name_to_action_interface_names(const std::string & action_name);
