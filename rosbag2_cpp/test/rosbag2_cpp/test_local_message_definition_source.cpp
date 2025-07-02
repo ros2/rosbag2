4313c9bfcb62ea30e3f1a9e871d3d2e445a1e8fe
Michael Orlov <morlovmr@gmail.com>
2025-07-01 22:40:10 -0700

    Add support for searching message definitions in nested subdirectories (#2055)
    * Add support for searching message definitions in subfolders
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Attempt to address windows build errors
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    ---------
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    (cherry picked from commit 943993a9b4cc084ea6c3ca6742baafc7fbe7c545)
    
    # Conflicts:
    #	rosbag2_cpp/src/rosbag2_cpp/message_definitions/local_message_definition_source.cpp
    #	rosbag2_cpp/test/rosbag2_cpp/test_local_message_definition_source.cpp
    #	rosbag2_test_msgdefs/CMakeLists.txt

--- rosbag2_cpp/test/rosbag2_cpp/test_local_message_definition_source.cpp
+++ rosbag2_cpp/test/rosbag2_cpp/test_local_message_definition_source.cpp
@@ -62,6 +62,36 @@ TEST(test_local_message_definition_source, can_find_msg_deps)
     "float32 c\n");
 }
 
+TEST(test_local_message_definition_source, can_find_msg_definition_in_nested_subfolder)
+{
+  LocalMessageDefinitionSource source;
+  auto result =
+    source.get_full_text_ext("rosbag2_test_msgdefs/msg/nested_sub_dir/AnotherBasicMsg",
+                             "/basic_msg_topic");
+  ASSERT_EQ(result.encoding, "ros2msg");
+  // Note: By design The top-level message definition for MSG format is present first, with no
+  // delimiter. All dependent .msg definitions are preceded by a two-line delimiter:
+  ASSERT_EQ(result.encoded_message_definition, "float32 c\n");
+}
+
+TEST(test_local_message_definition_source, can_find_action_definition_in_nested_subfolder)
+{
+  LocalMessageDefinitionSource source;
+  auto result =
+    source.get_full_text_ext("rosbag2_test_msgdefs/nested_sub_dir/action/BasicMsg",
+                             "/basic_action_msg/_action/send_goal");
+  ASSERT_EQ(result.encoding, "ros2msg");
+  ASSERT_EQ(result.encoded_message_definition,
+    "================================================================================\n"
+    "ACTION: rosbag2_test_msgdefs/nested_sub_dir/action/BasicMsg\n"
+    "string goal\n"
+    "---\n"
+    "string result\n"
+    "---\n"
+    "string feedback\n"
+  );
+}
+
 TEST(test_local_message_definition_source, can_find_srv_deps_in_msg)
 {
   LocalMessageDefinitionSource source;
@@ -175,9 +205,44 @@ TEST(test_local_message_definition_source, no_crash_on_bad_name)
 {
   LocalMessageDefinitionSource source;
   rosbag2_storage::MessageDefinition result;
+
+  // The following type names are not valid, but it should not crash
   ASSERT_NO_THROW(
   {
+<<<<<<< HEAD
     result = source.get_full_text("rosbag2_test_msgdefs/idl/BasicSrv_Request");
+=======
+    // The typename without preceding package name
+    result = source.get_full_text_ext("/msg/String", "/msg_topic");
+  });
+  ASSERT_EQ(result.encoding, "unknown");
+
+  ASSERT_NO_THROW(
+  {
+    // Missing the actual type name after format specifier
+    result = source.get_full_text_ext("std_msgs/msg/", "/msg_topic");
+  });
+  ASSERT_EQ(result.encoding, "unknown");
+
+  ASSERT_NO_THROW(
+  {
+    // Missing package name before the first slash
+    result = source.get_full_text_ext("/String", "/msg_topic");
+  });
+  ASSERT_EQ(result.encoding, "unknown");
+
+  ASSERT_NO_THROW(
+  {
+    // Hyphens are not allowed in package names
+    result = source.get_full_text_ext("std-msgs/String", "/msg_topic");
+  });
+  ASSERT_EQ(result.encoding, "unknown");
+
+  ASSERT_NO_THROW(
+  {
+    // File extensions are not allowed
+    result = source.get_full_text_ext("std_msgs/String.msg", "/msg_topic");
+>>>>>>> 943993a (Add support for searching message definitions in nested subdirectories (#2055))
   });
   ASSERT_EQ(result.encoding, "unknown");
 }
