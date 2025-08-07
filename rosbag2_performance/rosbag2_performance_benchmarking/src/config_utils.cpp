678ac2fa6cb117c65166805f6d0df311583d7ba3
Cristóbal Arroyo <cristobal.arroyo@ekumenlabs.com>
2025-08-06 22:15:31 -0500

    Fix warning: initialize number_of_threads (#2121)
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>
    (cherry picked from commit 24fb1a9fe5457c5d74bf10f0b5e81fcec84eeb35)
    
    # Conflicts:
    #	rosbag2_performance/rosbag2_performance_benchmarking/src/config_utils.cpp

--- rosbag2_performance/rosbag2_performance_benchmarking/src/config_utils.cpp
+++ rosbag2_performance/rosbag2_performance_benchmarking/src/config_utils.cpp
@@ -58,6 +58,18 @@ bool wait_for_subscriptions_from_node_parameters(rclcpp::Node & node)
   return wait_for_subscriptions;
 }
 
+<<<<<<< HEAD
+=======
+size_t get_number_of_threads_from_node_parameters(rclcpp::Node & node)
+{
+  const std::string parameters_ns = "publishers";
+  node.declare_parameter<int>(parameters_ns + ".number_of_threads", 0);
+  size_t number_of_threads = 0;
+  node.get_parameter(parameters_ns + ".number_of_threads", number_of_threads);
+  return number_of_threads;
+}
+
+>>>>>>> 24fb1a9 (Fix warning: initialize number_of_threads (#2121))
 std::vector<PublisherGroupConfig> publisher_groups_from_node_parameters(
   rclcpp::Node & node)
 {
