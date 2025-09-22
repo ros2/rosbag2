60ca24d43add7d8ae91b04f7270d2f467032dd47
Michael Orlov <morlovmr@gmail.com>
2025-09-22 13:44:01 -0700

    Add TODO to not forget to remove deprecated method per review request
    Signed-off-by: Michael Orlov <michael.orlov@apex.ai>

--- rosbag2_transport/include/rosbag2_transport/player.hpp
+++ rosbag2_transport/include/rosbag2_transport/player.hpp
@@ -381,6 +381,7 @@ protected:
 
   /// \brief Getter for the first of the currently stored storage options
   /// \return Copy of the first item in the StorageOptions vector
+  // TODO(morlov): Remove this method in Rolling after Lyrical release
   [[deprecated("Use rosabg2_transport::Player::get_all_storage_options() instead")]]
   ROSBAG2_TRANSPORT_PUBLIC
   rosbag2_storage::StorageOptions get_storage_options();
