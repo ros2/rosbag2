e66a1d60e29e6a2966a61b108254d5f0b4c2a641
Dhruv Patel <dhruvpatel2991998@gmail.com>
2025-11-23 12:58:59 +0100

    comment : no-op
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>

--- rosbag2_transport/src/rosbag2_transport/locked_priority_queue.hpp
+++ rosbag2_transport/src/rosbag2_transport/locked_priority_queue.hpp
@@ -26,6 +26,7 @@
 #include "rcpputils/unique_lock.hpp"
 
 #ifdef __APPLE__
+// no-op: Clang on macOS does not support thread-safety annotations
 // On macOS, thread-safety annotations break compilation; make them no-op
 #pragma push_macro("RCPPUTILS_TSA_GUARDED_BY")
 #undef RCPPUTILS_TSA_GUARDED_BY
