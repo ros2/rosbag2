b2d1b13ab4912858879026eba5ab05bc3e00fdce
Michael Orlov <morlovmr@gmail.com>
2026-01-06 08:57:28 -0800

    Upstream minor fixes from Apex.AI (#2240)
    * Adjust warnings in recorder
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Address thread sanitizer warning in tests
    
    - Address clang16 warning in test
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Add EventQoS for event notifier publishers
    
    - Follow-up on ros2/rosbag2#2150
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Fix: rosbag_py Player not init global exit flag before starting to play
    
    The rosbag_py Player does not reset its global static exit_ flag upon
     start playing. This leads to the situation when player instances
     created afterward can not start playing and stop immediately.
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Add check for nullptr in check_against_sorted to avoid crashes in tests
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Sort includes alphabetically in the test_recorder_event_notifier.cpp
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Address Windows build warnings (explicit capturing constant in lambda)
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    ---------
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    (cherry picked from commit 0cc434079af6b86ee4be10d023c8e4ac7a1b72bb)
    
    # Conflicts:
    #	rosbag2_transport/src/rosbag2_transport/recorder.cpp
    #	rosbag2_transport/src/rosbag2_transport/recorder_event_notifier_impl.hpp
    #	rosbag2_transport/test/rosbag2_transport/test_recorder_event_notifier.cpp

--- rosbag2_transport/src/rosbag2_transport/recorder.cpp
+++ rosbag2_transport/src/rosbag2_transport/recorder.cpp
@@ -331,18 +331,24 @@ void RecorderImpl::record()
     };
   writer_->add_event_callbacks(callbacks);
 
+<<<<<<< HEAD
   serialization_format_ = record_options_.rmw_serialization_format;
   RCLCPP_INFO(node->get_logger(), "Listening for topics...");
+=======
+>>>>>>> 0cc4340 (Upstream minor fixes from Apex.AI (#2240))
   if (!record_options_.use_sim_time) {
     subscribe_topics(get_requested_or_available_topics());
   }
   if (!record_options_.is_discovery_disabled) {
     start_discovery();
+    RCLCPP_INFO(node->get_logger(), "Listening for topics...");
   }
   if (record_options_.start_paused) {
-    RCLCPP_INFO(
-      node->get_logger(), "Wait for recording: Press %s to start.",
-      enum_key_code_to_str(Recorder::kPauseResumeToggleKey).c_str());
+    if (!record_options_.disable_keyboard_controls) {
+      RCLCPP_INFO(
+        node->get_logger(), "Wait for recording: Press %s to start.",
+        enum_key_code_to_str(Recorder::kPauseResumeToggleKey).c_str());
+    }
   } else {
     RCLCPP_INFO(node->get_logger(), "Recording...");
   }
