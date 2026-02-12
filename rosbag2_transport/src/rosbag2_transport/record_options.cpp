c87a5578183b318d1e5af1f20994de3214884b85
baranbologur <69406119+Baranbologur@users.noreply.github.com>
2026-02-12 09:46:30 +0300

    Add missing "RecordOptions" fields to the encode/decode functions (#2334)
    * Fix YAML decode/encode for ignore_leaf_topics, start_paused and use_sim_time in RecordOptions
    
    Signed-off-by: Baran Bologur <baranbologur@hotmail.com>
    
    * Add missing RecordOptions fields to the encode/decode functions
    
    - Also added a safeguard aka compile time error if someone will
     add or remove a field to the `RecordOption` and forgotten to add it
     to the encode/decode function in future.
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    * Reorder YAML encode/decode assignments to match the RecordOptions struct field order
    
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    
    ---------
    
    Signed-off-by: Baran Bologur <baranbologur@hotmail.com>
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>
    Co-authored-by: Michael Orlov <morlovmr@gmail.com>
    (cherry picked from commit 1a684e2b854a40c12c09d01892d87322f9dde252)
    
    # Conflicts:
    #	rosbag2_transport/include/rosbag2_transport/record_options.hpp
    #	rosbag2_transport/src/rosbag2_transport/record_options.cpp
    #	rosbag2_transport/test/rosbag2_transport/test_record_options.cpp

--- rosbag2_transport/src/rosbag2_transport/record_options.cpp
+++ rosbag2_transport/src/rosbag2_transport/record_options.cpp
@@ -26,7 +26,22 @@ namespace YAML
 Node convert<rosbag2_transport::RecordOptions>::encode(
   const rosbag2_transport::RecordOptions & record_options)
 {
+  // Structured binding check - this line will fail to compile if fields are added/removed to
+  // RecordOptions without updating this function.
+  // Note: Please don't forget to update the test case `test_yaml_serialization_deserialization`
+  // in `test_record_options.cpp` when updating the fields in RecordOptions.
+  auto & [all_topics, all_services, all_actions, is_discovery_disabled,
+    topics, topic_types, services, actions,
+    exclude_topics, exclude_topic_types, exclude_service_events, exclude_actions,
+    rmw_serialization_format, input_serialization_format, output_serialization_format,
+    topic_polling_interval, regex, exclude_regex, node_prefix,
+    compression_mode, compression_format, compression_queue_size, compression_threads,
+    compression_threads_priority, topic_qos_profile_overrides,
+    include_hidden_topics, include_unpublished_topics, ignore_leaf_topics,
+    start_paused, use_sim_time, static_topics_uri, disable_keyboard_controls,
+    statistics_max_publishing_rate] = record_options;
   Node node;
+<<<<<<< HEAD
   node["all_topics"] = record_options.all_topics;
   node["all_services"] = record_options.all_services;
   node["all_actions"] = record_options.all_actions;
@@ -49,75 +64,123 @@ Node convert<rosbag2_transport::RecordOptions>::encode(
   node["compression_queue_size"] = record_options.compression_queue_size;
   node["compression_threads"] = record_options.compression_threads;
   node["compression_threads_priority"] = record_options.compression_threads_priority;
+=======
+  node["all_topics"] = all_topics;
+  node["all_services"] = all_services;
+  node["all_actions"] = all_actions;
+  node["is_discovery_disabled"] = is_discovery_disabled;
+  node["topics"] = topics;
+  node["topic_types"] = topic_types;
+  node["services"] = services;
+  node["actions"] = actions;
+  node["exclude_topics"] = exclude_topics;
+  node["exclude_topic_types"] = exclude_topic_types;
+  node["exclude_services"] = exclude_service_events;
+  node["exclude_actions"] = exclude_actions;
+  node["rmw_serialization_format"] = rmw_serialization_format;
+  node["input_serialization_format"] = input_serialization_format;
+  node["output_serialization_format"] = output_serialization_format;
+  node["topic_polling_interval"] = topic_polling_interval;
+  node["regex"] = regex;
+  node["exclude_regex"] = exclude_regex;
+  node["node_prefix"] = node_prefix;
+  node["compression_mode"] = compression_mode;
+  node["compression_format"] = compression_format;
+  node["compression_queue_size"] = compression_queue_size;
+  node["compression_threads"] = compression_threads;
+  node["compression_threads_priority"] = compression_threads_priority;
+>>>>>>> 1a684e2 (Add missing "RecordOptions" fields to the encode/decode functions (#2334))
   node["topic_qos_profile_overrides"] =
-    convert<std::unordered_map<std::string, rclcpp::QoS>>::encode(
-    record_options.topic_qos_profile_overrides);
-  node["include_hidden_topics"] = record_options.include_hidden_topics;
-  node["include_unpublished_topics"] = record_options.include_unpublished_topics;
-  node["disable_keyboard_controls"] = record_options.disable_keyboard_controls;
+    convert<std::unordered_map<std::string, rclcpp::QoS>>::encode(topic_qos_profile_overrides);
+  node["include_hidden_topics"] = include_hidden_topics;
+  node["include_unpublished_topics"] = include_unpublished_topics;
+  node["ignore_leaf_topics"] = ignore_leaf_topics;
+  node["start_paused"] = start_paused;
+  node["use_sim_time"] = use_sim_time;
+  node["static_topics_uri"] = static_topics_uri;
+  node["disable_keyboard_controls"] = disable_keyboard_controls;
+  node["statistics_max_publishing_rate"] = statistics_max_publishing_rate;
   return node;
 }
 
 bool convert<rosbag2_transport::RecordOptions>::decode(
   const Node & node, rosbag2_transport::RecordOptions & record_options, int version)
 {
-  optional_assign<bool>(node, "all_topics", record_options.all_topics);
-  optional_assign<bool>(node, "all_services", record_options.all_services);
-  optional_assign<bool>(node, "all_actions", record_options.all_actions);
+  // Structured binding check - this line will fail to compile if fields are added/removed to
+  // RecordOptions without updating this function.
+  // Note: Please don't forget to update the test case `test_yaml_serialization_deserialization`
+  // in `test_record_options.cpp` when updating the fields in RecordOptions.
+  auto & [all_topics, all_services, all_actions, is_discovery_disabled,
+    topics, topic_types, services, actions,
+    exclude_topics, exclude_topic_types, exclude_service_events, exclude_actions,
+    rmw_serialization_format, input_serialization_format, output_serialization_format,
+    topic_polling_interval, regex, exclude_regex, node_prefix,
+    compression_mode, compression_format, compression_queue_size, compression_threads,
+    compression_threads_priority, topic_qos_profile_overrides,
+    include_hidden_topics, include_unpublished_topics, ignore_leaf_topics,
+    start_paused, use_sim_time, static_topics_uri, disable_keyboard_controls,
+    statistics_max_publishing_rate] = record_options;
+
+  optional_assign<bool>(node, "all_topics", all_topics);
+  optional_assign<bool>(node, "all_services", all_services);
+  optional_assign<bool>(node, "all_actions", all_actions);
   bool record_options_all{false};  // Parse `all` for backward compatability and convenient usage
   optional_assign<bool>(node, "all", record_options_all);
   if (record_options_all) {
-    record_options.all_topics = true;
-    record_options.all_services = true;
-    record_options.all_actions = true;
+    all_topics = true;
+    all_services = true;
+    all_actions = true;
   }
 
-  optional_assign<bool>(node, "is_discovery_disabled", record_options.is_discovery_disabled);
-  optional_assign<std::vector<std::string>>(node, "topics", record_options.topics);
-  optional_assign<std::vector<std::string>>(node, "topic_types", record_options.topic_types);
-  optional_assign<std::vector<std::string>>(node, "services", record_options.services);
-  optional_assign<std::vector<std::string>>(node, "actions", record_options.actions);
+  optional_assign<bool>(node, "is_discovery_disabled", is_discovery_disabled);
+  optional_assign<std::vector<std::string>>(node, "topics", topics);
+  optional_assign<std::vector<std::string>>(node, "topic_types", topic_types);
+  optional_assign<std::vector<std::string>>(node, "services", services);
+  optional_assign<std::vector<std::string>>(node, "actions", actions);
+  optional_assign<std::vector<std::string>>(node, "exclude_topics", exclude_topics);
+  optional_assign<std::vector<std::string>>(node, "exclude_topic_types", exclude_topic_types);
+  optional_assign<std::vector<std::string>>(node, "exclude_services", exclude_service_events);
+  optional_assign<std::vector<std::string>>(node, "exclude_actions", exclude_actions);
   optional_assign<std::string>(
+<<<<<<< HEAD
     node, "rmw_serialization_format", record_options.rmw_serialization_format);
+=======
+    node, "rmw_serialization_format", rmw_serialization_format);
+  optional_assign<std::string>(
+    node, "input_serialization_format", input_serialization_format);
+  optional_assign<std::string>(
+    node, "output_serialization_format", output_serialization_format);
+>>>>>>> 1a684e2 (Add missing "RecordOptions" fields to the encode/decode functions (#2334))
 
   optional_assign<std::chrono::milliseconds>(
-    node, "topic_polling_interval", record_options.topic_polling_interval);
+    node, "topic_polling_interval", topic_polling_interval);
 
-  optional_assign<std::string>(node, "regex", record_options.regex);
+  optional_assign<std::string>(node, "regex", regex);
   // Map exclude to the "exclude_regex" for backward compatability.
-  optional_assign<std::string>(node, "exclude", record_options.exclude_regex);
-  optional_assign<std::string>(node, "exclude_regex", record_options.exclude_regex);
-  optional_assign<std::vector<std::string>>(node, "exclude_topics", record_options.exclude_topics);
-  optional_assign<std::vector<std::string>>(
-    node, "exclude_topic_types",
-    record_options.exclude_topic_types);
-  optional_assign<std::vector<std::string>>(
-    node, "exclude_services", record_options.exclude_service_events);
-  optional_assign<std::vector<std::string>>(
-    node, "exclude_actions", record_options.exclude_actions);
-  optional_assign<std::string>(node, "node_prefix", record_options.node_prefix);
-  optional_assign<std::string>(node, "compression_mode", record_options.compression_mode);
-  optional_assign<std::string>(node, "compression_format", record_options.compression_format);
-  optional_assign<uint64_t>(node, "compression_queue_size", record_options.compression_queue_size);
-  optional_assign<uint64_t>(node, "compression_threads", record_options.compression_threads);
-  optional_assign<int32_t>(
-    node, "compression_threads_priority",
-    record_options.compression_threads_priority);
+  optional_assign<std::string>(node, "exclude", exclude_regex);
+  optional_assign<std::string>(node, "exclude_regex", exclude_regex);
+  optional_assign<std::string>(node, "node_prefix", node_prefix);
+  optional_assign<std::string>(node, "compression_mode", compression_mode);
+  optional_assign<std::string>(node, "compression_format", compression_format);
+  optional_assign<uint64_t>(node, "compression_queue_size", compression_queue_size);
+  optional_assign<uint64_t>(node, "compression_threads", compression_threads);
+  optional_assign<int32_t>(node, "compression_threads_priority", compression_threads_priority);
 
   std::unordered_map<std::string, rclcpp::QoS> qos_overrides;
   if (node["topic_qos_profile_overrides"]) {
     qos_overrides = YAML::decode_for_version<std::unordered_map<std::string, rclcpp::QoS>>(
       node["topic_qos_profile_overrides"], version);
   }
-  record_options.topic_qos_profile_overrides = qos_overrides;
+  topic_qos_profile_overrides = qos_overrides;
 
-  optional_assign<bool>(node, "include_hidden_topics", record_options.include_hidden_topics);
-  optional_assign<bool>(
-    node, "include_unpublished_topics",
-    record_options.include_unpublished_topics);
-  optional_assign<bool>(
-    node, "disable_keyboard_controls",
-    record_options.disable_keyboard_controls);
+  optional_assign<bool>(node, "include_hidden_topics", include_hidden_topics);
+  optional_assign<bool>(node, "include_unpublished_topics", include_unpublished_topics);
+  optional_assign<bool>(node, "ignore_leaf_topics", ignore_leaf_topics);
+  optional_assign<bool>(node, "start_paused", start_paused);
+  optional_assign<bool>(node, "use_sim_time", use_sim_time);
+  optional_assign<std::string>(node, "static_topics_uri", static_topics_uri);
+  optional_assign<bool>(node, "disable_keyboard_controls", disable_keyboard_controls);
+  optional_assign<float>(node, "statistics_max_publishing_rate", statistics_max_publishing_rate);
   return true;
 }
 
