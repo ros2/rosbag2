746676bc44f799629ada7c3d31fa11d09e311bdb
Fleury <m.fleury@nct-dresden.de>
2026-03-09 16:24:33 +0100

    Add simple bag recorder example (#2347)
    Signed-off-by: Fleury <m.fleury@nct-dresden.de>

---
+++ rosbag2_examples/rosbag2_examples_py/rosbag2_examples_py/simple_bag_recorder.py
@@ -0,0 +1,46 @@
+# Copyright 2026 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import rclpy
+from rclpy.executors import ExternalShutdownException
+import rosbag2_py
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    storage_options = rosbag2_py.StorageOptions(uri='my_bag')
+
+    record_options = rosbag2_py.RecordOptions()
+    record_options.all_topics = True
+    record_options.is_discovery_disabled = False
+    record_options.rmw_serialization_format = 'cdr'
+
+    recorder = rosbag2_py.Recorder(
+        storage_options,
+        record_options,
+        'info',
+        'simple_recorder_demo')
+
+    recorder.start_spin()
+    recorder.record()
+
+    try:
+        while rclpy.ok():
+            pass
+    except (KeyboardInterrupt, ExternalShutdownException):
+        recorder.stop_spin()
+
+
+if __name__ == '__main__':
+    main()
--- rosbag2_examples/rosbag2_examples_py/setup.py
+++ rosbag2_examples/rosbag2_examples_py/setup.py
@@ -27,6 +27,7 @@ setup(
             'rosbag2csv = rosbag2_examples_py.rosbag2csv:main',
             'simple_bag_writer = rosbag2_examples_py.simple_bag_writer:main',
             'simple_bag_reader = rosbag2_examples_py.simple_bag_reader:main',
+            'simple_bag_recorder = rosbag2_examples_py.simple_bag_recorder:main',
             'data_generator_node = rosbag2_examples_py.data_generator_node:main',
             'data_generator_executable = rosbag2_examples_py.data_generator_executable:main',
             'compressed_bag_writer = rosbag2_examples_py.compressed_bag_writer:main',
