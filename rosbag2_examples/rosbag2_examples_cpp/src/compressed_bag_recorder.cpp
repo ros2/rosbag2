172ba285f5b166a0a160d56941fc9573e03bf091
Fleury <m.fleury@nct-dresden.de>
2026-03-10 11:47:42 +0100

    Add compressed bag recorder example (#2347)
    Signed-off-by: Fleury <m.fleury@nct-dresden.de>

---
+++ rosbag2_examples/rosbag2_examples_cpp/src/compressed_bag_recorder.cpp
@@ -0,0 +1,42 @@
+// Copyright 2026 Open Source Robotics Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rosbag2_transport/reader_writer_factory.hpp"
+#include "rosbag2_transport/recorder.hpp"
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+
+  rosbag2_storage::StorageOptions storage_options;
+  storage_options.uri = "my_bag";
+
+  rosbag2_transport::RecordOptions record_options;
+  record_options.all_topics = true;
+  record_options.rmw_serialization_format = "cdr";
+  record_options.compression_format = "zstd";
+  record_options.compression_mode = "file";
+
+  // Calling ReaderWriterFactory creates a writer with compression parameters
+  auto writer = rosbag2_transport::ReaderWriterFactory::make_writer(record_options);
+  auto recorder = std::make_shared<rosbag2_transport::Recorder>(
+    std::move(writer), storage_options, record_options);
+
+  recorder->record();
+  rclcpp::spin(recorder);
+
+  recorder->stop();
+  rclcpp::shutdown();
+  return 0;
+}
