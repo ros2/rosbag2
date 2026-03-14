c9103d2fee040e2909876160f0c2f8f6e0d7af0d
Michael Orlov <morlovmr@gmail.com>
2026-03-14 11:57:51 -0700

    Address linter warning
    Signed-off-by: Michael Orlov <morlovmr@gmail.com>

--- rosbag2_transport/src/rosbag2_transport/recorder.cpp
+++ rosbag2_transport/src/rosbag2_transport/recorder.cpp
@@ -630,7 +630,7 @@ void RecorderImpl::stop()
     std::lock_guard<std::mutex> lock(pending_resume_request_mutex_);
     pending_resume_request_.reset();
   }
-  
+
   last_seen_timestamps_.reset();  // Clear last seen timestamps
 
   in_recording_ = false;
